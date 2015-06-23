71e9633ccee427c9d761f246817d02fd5d377250
Jeremia Kimelman <jbkimelman@gmail.com>
2015-06-23 17:37:57 -0400

    Get at actual target from longer URL for scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,9 +7,9 @@ $(function(){
 
 $('.sidenav nav a').click(function(e) {
   e.preventDefault();
-  var hashLocation = $(this).attr('href');
+  var hashLocation = $(this).attr('href').split('#')[1]; // long url splitting
   $('.main-content').animate({
-    scrollTop: $(hashLocation).position().top
+    scrollTop: $('#' + hashLocation).position().top
   }, 200);
   window.location.hash = hashLocation;
 });
