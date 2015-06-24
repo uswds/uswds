4a904e861c612211195993ca3a8d354b84ddce1b
Maya Benari <hello@maya-benari.com>
2015-06-24 15:13:43 -0700

    Adjust scroll to placement

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ $('.secondary-sidenav-link').click(function(e) {
   e.preventDefault();
   var hashLocation = $(this).attr('href').split('#')[1]; // long url splitting
   $('.main-content').animate({
-    scrollTop: $('#' + hashLocation).position().top
+    scrollTop: $('#' + hashLocation).position().top +100
   }, 200);
   window.location.hash = hashLocation;
 });
