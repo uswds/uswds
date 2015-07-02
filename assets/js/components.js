86276df266ebbfe9b1105de757447db3420c2d60
Marco Segreto <marco.segreto@gsa.gov>
2015-07-02 16:11:25 -0700

    Improvements to accessibility code
    - Made accordion a constructor with methods to reduce namespace
    collisions.
    - Simplified JS code by using initializer.
    - Moved aria hidden css to base css.

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,30 +1,64 @@
 'use strict';
 
-function hide($el) {
-  var selector = $el.attr('aria-controls'),
-      $content = $('#' + selector);
+/* globals $: false */
 
-  $el.attr('aria-expanded', false);
-  $content.attr('aria-hidden', true);
-}
-
-function show($el) {
-  var selector = $el.attr('aria-controls'),
-      $content = $('#' + selector);
-
-  $el.attr('aria-expanded', true);
-  $content.attr('aria-hidden', false);
-}
-
-function hideAll($container) {
-  $container.find('button').each(function() {
-    hide($(this));
+/**
+ * Accordion
+ *
+ * An accordion component.
+ *
+ * @param {jQuery} $el A jQuery html element to turn into an accordion.
+ */
+function Accordion($el) {
+  var self = this;
+  this.$root = $el;
+  this.$root.on('click', 'button', function(ev) {
+    ev.preventDefault();
+    self.hideAll();
+    self.show($(this));
   });
 }
 
+Accordion.prototype.$ = function(selector) {
+  return this.$root.find(selector);
+}
+
+Accordion.prototype.hide = function($button) {
+  var selector = $button.attr('aria-controls'),
+      $content = this.$('#' + selector);
+
+  $button.attr('aria-expanded', false);
+  $content.attr('aria-hidden', true);
+};
+
+Accordion.prototype.show = function($button) {
+  var selector = $button.attr('aria-controls'),
+      $content = this.$('#' + selector);
+
+  $button.attr('aria-expanded', true);
+  $content.attr('aria-hidden', false);
+};
+
+Accordion.prototype.hideAll = function() {
+  var self = this;
+  this.$('button').each(function() {
+    self.hide($(this));
+  });
+};
+
+/**
+ * accordion
+ *
+ * Initialize a new Accordion component.
+ *
+ * @param {jQuery} $el A jQuery html element to turn into an accordion.
+ */
+function accordion($el) {
+  return new Accordion($el);
+}
+
 $(function() {
-  $('.usa-accordion').on('click', 'button', function() {
-    hideAll($('.usa-accordion'));
-    show($(this));
+  $('.usa-accordion').each(function() {
+    accordion($(this));
   });
 });
