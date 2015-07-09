92c49973638aa5f4d18ad40d95e060c5a209b944
Julia Elman <julia.elman@gsa.gov>
2015-07-09 12:34:41 -0400

    Adding accordion functionality to the footer.

--- assets/js/components.js
+++ assets/js/components.js
@@ -3,4 +3,19 @@ $(function() {
    $(this).parent().removeClass('hidden')
     .siblings().addClass('hidden');
   });
+
+  $(window).resize(function() {
+    if (window.innerWidth < 600) {
+
+      $('.usa-footer-big nav ul').addClass('hidden');      
+
+      $('.usa-footer-big nav').on('click', 'h3', function() {
+        $(this).parent().removeClass('hidden')
+        .siblings().addClass('hidden');
+      });
+    } else {
+      $('.usa-footer-big nav ul').removeClass('hidden');  
+    }
+  });
+
 });
