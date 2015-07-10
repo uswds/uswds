7ca4611e5d0f53aab1fc24f7a6bac1137195a4e6
Julia Elman <julia.elman@gsa.gov>
2015-07-10 13:55:56 -0400

    Initialize accordion functionality on page load instead of on window resize.

--- assets/js/components.js
+++ assets/js/components.js
@@ -4,18 +4,24 @@ $(function() {
     .siblings().addClass('hidden');
   });
 
-  $(window).resize(function() {
+
+  var footerAccordion = function() {
     if (window.innerWidth < 600) {
+        $('.usa-footer-big nav ul').addClass('hidden');
 
-      $('.usa-footer-big nav ul').addClass('hidden');      
-
-      $('.usa-footer-big nav').on('click', 'h3', function() {
-        $(this).parent().removeClass('hidden')
-        .siblings().addClass('hidden');
-      });
+        $('.usa-footer-big nav').on('click', 'h3', function() {
+          $(this).parent().removeClass('hidden')
+          .siblings().addClass('hidden');
+        });
     } else {
       $('.usa-footer-big nav ul').removeClass('hidden');  
     }
+  };
+
+  footerAccordion();
+
+  $(window).resize(function() {
+    footerAccordion();
   });
 
 });
