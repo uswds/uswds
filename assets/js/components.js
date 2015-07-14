c73063fc69176a06c8c245e7de9c5f079b19ceb3
Julia Elman <julia.elman@gsa.gov>
2015-07-14 10:01:37 -0400

    Fix for disabling click events when going from smaller to larger window sizes.

--- assets/js/components.js
+++ assets/js/components.js
@@ -62,17 +62,21 @@ $(function() {
     accordion($(this));
   });
 
-
   var footerAccordion = function() {
     if (window.innerWidth < 600) {
-        $('.usa-footer-big nav ul').addClass('hidden');
 
-        $('.usa-footer-big nav').on('click', 'h3', function() {
-          $(this).parent().removeClass('hidden')
-          .siblings().addClass('hidden');
-        });
+      $('.usa-footer-big nav ul').addClass('hidden');
+
+      $('.usa-footer-big nav h3').bind('click', function() {
+        $(this).parent().removeClass('hidden')
+        .siblings().addClass('hidden');
+      });
+      
     } else {
-      $('.usa-footer-big nav ul').removeClass('hidden');  
+
+      $('.usa-footer-big nav ul').removeClass('hidden');
+
+      $('.usa-footer-big nav h3').unbind('click');
     }
   };
 
