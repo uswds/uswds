85d7814432935de4a02632d966d5233db879c5b8
Maya Benari <hello@maya-benari.com>
2015-07-15 15:57:44 -0700

    Fix scrollTop location

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ $('.secondary-sidenav-link').click(function(e) {
   e.preventDefault();
   var hashLocation = $(this).attr('href').split('#')[1]; // long url splitting
   $('.main-content').animate({
-    scrollTop: $('#' + hashLocation).position().top +100
+    scrollTop: $('#' + hashLocation).position().top +35
   }, 200);
   window.location.hash = hashLocation;
 });
