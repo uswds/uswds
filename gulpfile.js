0bead731208375516e415ba7a2a7be64ad099726
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2016-02-02 16:45:09 -0500

    Add in gulpfile entry point

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+require( './config/gulp/stylesheet' );
+require( './config/gulp/website' );
+
+var gulp = require( 'gulp' );
+
+gulp.task( 'default', function ( done ) {
+
+  console.log( 'gulp is working' );
+
+  done();
+
+} );
