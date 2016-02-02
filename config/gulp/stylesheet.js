32bf12da21b4c75284bb6cbd66c5bb8cf29fae79
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2016-02-02 16:39:38 -0500

    Add in gulp workflows;
    This patch adds workflows for the stylesheet and website gulp tasks.

---
+++ config/gulp/stylesheet.js
@@ -0,0 +1,35 @@
+var gulp = require( 'gulp' );
+var sass = require( 'gulp-sass' );
+var sourcemaps = require( 'gulp-sourcemaps' );
+var rename = require( 'gulp-rename' );
+
+var files = [
+
+  // README: These files do not need to be explicitly brought into the `sass` task.
+  //'stylesheets/_scss/lib/bourbon/**/*.scss',
+  //'stylesheets/_scss/lib/neat/**/*.scss',
+  //'stylesheets/_scss/core/*.scss',
+  //'stylesheets/_scss/components/*.scss',
+  //'stylesheets/_scss/elements/*.scss',
+  'stylesheets/_scss/all.scss',
+
+];
+
+var options = {
+
+  outputStyle: 'expanded',
+
+};
+
+gulp.task( 'sass', function ( done ) {
+
+  sass.on( 'error', sass.logError );
+
+  return gulp.src( files )
+    .pipe( sourcemaps.init() )
+    .pipe( sass( options ) )
+    .pipe( sourcemaps.write() )
+    .pipe( rename( { basename: 'uswds' } ) )
+    .pipe( gulp.dest( 'dist/css' ) );
+
+} );
---
+++ config/gulp/website.js
@@ -0,0 +1,103 @@
+var gulp = require( 'gulp' );
+var gutil = require( 'gulp-util' );
+var spawn = require( 'child_process' ).spawn;
+
+gulp.task( 'bundle-gems', function ( done ) {
+
+  var bundle = spawn( 'bundle' );
+
+  bundle.stdout.on( 'data', function ( data ) {
+
+    gutil.log(
+      gutil.colors.cyan( 'bundle-gems' ),
+      gutil.colors.yellow( '\n' + data )
+    );
+
+  } );
+
+  bundle.on( 'error', function ( error ) { done( error ); } );
+
+  bundle.on( 'close', function ( code ) { if ( 0 === code ) { done(); } } );
+
+} );
+
+gulp.task( 'website', function ( done ) {
+
+  gutil.log(
+    '$',
+    gutil.colors.cyan( 'gulp website' ),
+    gutil.colors.yellow( 'This is the default website task. Please review the available commands.' )
+  );
+  gutil.log(
+    '$',
+    gutil.colors.cyan( 'gulp website:build' ),
+    gutil.colors.magenta( 'Build the website.' )
+  );
+  gutil.log(
+    '$',
+    gutil.colors.cyan( 'gulp website:watch' ),
+    gutil.colors.magenta( 'Preview the website locally and rebuild it when files change.' )
+  );
+
+  done();
+
+} );
+
+// TODO: Review that this should be using the `go` script
+gulp.task( 'website:watch', function ( done ) {
+
+  var jekyll = spawn( 'jekyll', [ 'serve', '-w' ] );
+
+  jekyll.stdout.on( 'data', function ( data ) {
+
+    if ( /[\w\d]+/.test( data ) ) {
+
+      data += '';
+      data = data.replace( /[\s]+/g, ' ' );
+      if ( /done|regen/i.test( data ) ) {
+        gutil.log(
+          gutil.colors.cyan( 'website:watch' ),
+          gutil.colors.green( data )
+        );
+      } else {
+        gutil.log(
+          gutil.colors.cyan( 'website:watch' ),
+          gutil.colors.yellow( data )
+        );
+      }
+
+    }
+
+  } );
+
+  jekyll.on( 'error', function ( error ) { done( error ); } );
+
+  jekyll.on( 'close', function ( code ) { if ( 0 === code ) { done(); } } );
+
+
+} );
+
+gulp.task( 'website:build', [ 'bundle-gems' ], function ( done ) {
+
+  var jekyll = spawn( 'jekyll', [ 'build' ] );
+
+  jekyll.stdout.on( 'data', function ( data ) {
+
+    if ( /[\w\d]+/.test( data ) ) {
+
+      data += '';
+      data = data.replace( /[\s]+/g, ' ' );
+      gutil.log(
+        gutil.colors.cyan( 'website:build' ),
+        gutil.colors.yellow( data )
+      );
+
+    }
+
+  } );
+
+  jekyll.on( 'error', function ( error ) { done( error ); } );
+
+  jekyll.on( 'close', function ( code ) { if ( 0 === code ) { done(); } } );
+
+} );
