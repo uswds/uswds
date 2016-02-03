53465009ea60600b1024d17f8bee1130ea5a7c75
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2016-02-03 13:23:10 -0500

    Remove comments around file globbing for Sass

--- config/gulp/sass.js
+++ config/gulp/sass.js
@@ -5,12 +5,6 @@ var rename = require( 'gulp-rename' );
 
 var files = [
 
-  // README: These files do not need to be explicitly brought into the `sass` task.
-  //'stylesheets/_scss/lib/bourbon/**/*.scss',
-  //'stylesheets/_scss/lib/neat/**/*.scss',
-  //'stylesheets/_scss/core/*.scss',
-  //'stylesheets/_scss/components/*.scss',
-  //'stylesheets/_scss/elements/*.scss',
   'src/stylesheets/_scss/all.scss',
 
 ];
