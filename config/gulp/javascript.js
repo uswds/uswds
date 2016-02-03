f9c248903215ea0857171681afde000b42a6641f
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2016-02-03 16:15:07 -0500

    Update javascript task to use browserify;
    This patch removes the deprecated `gulp-browserify` library and instead
    uses `browserify` directly.

--- config/gulp/javascript.js
+++ config/gulp/javascript.js
@@ -1,27 +1,31 @@
 var gulp = require( 'gulp' );
-var browserify = require( 'gulp-browserify' );
+var gutil = require( 'gulp-util' );
+var dutil = require( './doc-util' );
+var browserify = require( 'browserify' );
+var buffer = require( 'vinyl-buffer' );
+var source = require( 'vinyl-source-stream' );
+var uglify = require( 'gulp-uglify' );
 var sourcemaps = require( 'gulp-sourcemaps' );
 var rename = require( 'gulp-rename' );
 
-var files = [
-
-  'src/js/start.js',
-
-];
-
-var options = {
-
-  // TODO
-
-};
-
 gulp.task( 'javascript', function ( done ) {
 
-  return gulp.src( files )
-    //.pipe( sourcemaps.init() )
-    .pipe( browserify( options ) )
-    //.pipe( sourcemaps.write() )
-    .pipe( rename( { basename: 'components' } ) )
+  var bundleStream = browserify( {
+
+    entries: 'src/js/start.js',
+    debug: true,
+
+  } );
+
+  return bundleStream.bundle()
+    .pipe( source( 'components.js' ) )
+    .pipe( buffer() )
+    .pipe( sourcemaps.init( { loadMaps: true } ) )
+    //.pipe( uglify() )
+    .on( 'error', gutil.log )
+    .pipe( sourcemaps.write() )
+    // TODO: Renaming the components.js file to the package name TBD
+    //.pipe( rename( { basename: dutil.pkg.name } ) )
     .pipe( gulp.dest( 'dist/js' ) );
 
 } );
