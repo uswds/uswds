d4ba7e9532e16e5b1e3b9c9adaab2796cd155d5b
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2016-02-09 11:44:13 -0500

    Add `clean-dist` task that runs by default

--- config/gulp/build.js
+++ config/gulp/build.js
@@ -1,13 +1,42 @@
 var gulp = require('gulp');
 var dutil = require('./doc-util');
+var clean = require('gulp-clean');
+var runSequence = require( 'run-sequence' );
 
-gulp.task('build', [ 'sass', 'javascript', 'images', 'fonts' ], function (done) {
+gulp.task('clean-dist', function (done) {
+
+  if (!cFlags.cleanup) {
+    dutil.logMessage(
+      'clean-dist',
+      'Skipping cleaning up the distrubtion directories.'
+    );
+    return done();
+  }
+
+  dutil.logMessage('clean-dist', 'Removing distrubtion directories.');
+
+  return gulp.src([ 'dist', 'dist-gem/*' ], { read: false })
+    .pipe(clean());
+
+});
+
+gulp.task('build', function (done) {
 
   dutil.logIntroduction();
   dutil.logMessage(
     'build',
     'The build task has created distrubtion directories.'
   );
-  done();
+
+  runSequence(
+    'clean-dist',
+    [
+      'sass',
+      'javascript',
+      'images',
+      'fonts',
+    ],
+    done
+  );
 
 });
