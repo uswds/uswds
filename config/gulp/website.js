196316a9a1ea1791697521be584bf128fe07d1af
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2016-03-09 09:36:04 -0500

    Remove the jekyll gem dependency;
    This patch removes the Jekyll gem dependency from the `docs/` site.
    There are a few moving parts to this process. Because we are no longer
    using the gem all files need to be copied over to their respective
    directories. A list of files and directories that are generated using
    `gulp website:serve` are in the `.gitignore` file. These files will be
    ignored from version control since they already exist in many different
    variations.

--- config/gulp/website.js
+++ config/gulp/website.js
@@ -2,7 +2,9 @@ var gulp = require('gulp');
 var dutil = require('./doc-util');
 var process = require('child_process');
 var spawn = process.spawn;
+var exec = process.exec;
 var runSequence = require('run-sequence');
+var del = require('del');
 var clean = require('gulp-clean');
 
 var task = /([\w\d-_]+)\.js$/.exec(__filename)[ 1 ];
@@ -92,9 +94,122 @@ gulp.task('local-bundle-config', function (done) {
 
 });
 
+gulp.task('clean-generated-assets', function (done) {
+  return del([
+    'docs/_scss',
+    'docs/assets/fonts/',
+    'docs/assets/js/vendor/' + dutil.fileName + '.js',
+  ]);
+});
+
+gulp.task('copy-source-sass', function (done) {
+
+  var copySourceSass = spawn('cp', [
+    '-rvf',
+    'src/stylesheets',
+    'docs/_scss',
+  ]);
+
+  copySourceSass.stdout.on('data', function (data) {
+
+    if (/[\w\d]+/.test(data)) {
+
+      dutil.logData('copy-source-sass', data);
+
+    }
+
+  });
+
+  copySourceSass.on('error', function (error) { done(error); });
+
+  copySourceSass.on('close', function (code) { if (0 === code) {
+    process.execSync(
+      'mv docs/_scss/all.scss ' +
+      'docs/_scss/_' + dutil.pkg.name + '.scss'
+    );
+     done();
+  } });
+
+});
+
+gulp.task('copy-bundled-javascript', function (done) {
+
+  var copyBundledJavaScript = spawn('cp', [
+    '-rvf',
+    'dist/js/' + dutil.fileName + '.js',
+    'docs/assets/js/vendor/',
+  ]);
+
+  copyBundledJavaScript.stdout.on('data', function (data) {
+
+    if (/[\w\d]+/.test(data)) {
+
+      dutil.logData('copy-bundled-javascript', data);
+
+    }
+
+  });
+
+  copyBundledJavaScript.on('error', function (error) { done(error); });
+
+  copyBundledJavaScript.on('close', function (code) { if (0 === code) { done(); } });
+
+});
+
+gulp.task('copy-fonts', function (done) {
+
+  var copyFonts = spawn('cp', [
+    '-rvf',
+    'dist/fonts',
+    'docs/assets/fonts',
+  ]);
+
+  copyFonts.stdout.on('data', function (data) {
+
+    if (/[\w\d]+/.test(data)) {
+
+      dutil.logData('copy-fonts', data);
+
+    }
+
+  });
+
+  copyFonts.on('error', function (error) { done(error); });
+
+  copyFonts.on('close', function (code) { if (0 === code) { done(); } });
+
+});
+
+gulp.task('copy-images', function (done) {
+
+  var copyImages = exec('cp -rvf dist/img/* docs/assets/img/', function (error, stdout, stderr) {
+
+    if (stdout && /[\w\d]+/.test(stdout)) {
+
+      dutil.logData('copy-images', stdout);
+
+    }
+
+    done();
+
+  });
+
+});
+
+gulp.task('copy-assets', [ 'build' ], function (done) {
+  runSequence(
+    'clean-generated-assets',
+    'copy-source-sass',
+    'copy-bundled-javascript',
+    'copy-fonts',
+    'copy-images',
+    done
+  );
+});
+
 // Wrapper task for `bundle install` which installs gems for the Jekyll site.
 //
-gulp.task('bundle-gems', [ 'local-bundle-config' ], function (done) {
+gulp.task('bundle-gems', [ 'copy-assets' ], function (done) {
 
   var bundle = spawn('bundle', [ 'update' ]);
 
