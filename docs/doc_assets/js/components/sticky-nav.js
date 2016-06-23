6b2450cfc619ff844f8e5c46587e71028d04e63c
Maya Benari <hello@maya-benari.com>
2016-06-22 18:19:09 -0700

    Update scroll location

--- docs/doc_assets/js/components/sticky-nav.js
+++ docs/doc_assets/js/components/sticky-nav.js
@@ -7,13 +7,14 @@ var STICKY_CLASS_NAME = 'is-scrolled';
 module.exports = function stickyNav (event) {
 
   var originalNavigationHeight = $nav.outerHeight(true);
-  var bannerHeight = $('.usa-site-header').outerHeight(true) - originalNavigationHeight * 0.5;
+  var bannerHeight = $('.usa-site-header').outerHeight(true) - originalNavigationHeight * 0.5 - 50;
   var scrollY = $(window).scrollTop();
   var scrollPositionY = scrollY - originalNavigationHeight * 0.5;
 
   if (scrollPositionY > bannerHeight) {
     $nav.addClass(STICKY_CLASS_NAME);
     $('.usa-site-header').addClass(STICKY_CLASS_NAME);
+    $('.site-navbar').addClass(STICKY_CLASS_NAME);
     $('.site-nav-secondary').addClass(STICKY_CLASS_NAME);
     $('.sidenav').addClass(STICKY_CLASS_NAME);
     $('.main-content').addClass(STICKY_CLASS_NAME);
@@ -21,6 +22,7 @@ module.exports = function stickyNav (event) {
   } else {
     $nav.removeClass(STICKY_CLASS_NAME);
     $('.usa-site-header').removeClass(STICKY_CLASS_NAME);
+    $('.site-navbar').removeClass(STICKY_CLASS_NAME);
     $('.site-nav-secondary').removeClass(STICKY_CLASS_NAME);
     $('.sidenav').removeClass(STICKY_CLASS_NAME);
     $('.main-content').removeClass(STICKY_CLASS_NAME);
