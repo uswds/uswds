be8b602a217ea623e500956a2ccaf1d82789572c
Maya Benari <hello@maya-benari.com>
2016-06-28 10:03:12 -0700

    Add overflow hidden to body when mobile sidenav is active

--- docs/doc_assets/js/start.js
+++ docs/doc_assets/js/start.js
@@ -7,6 +7,7 @@ var stickyNav = require('./components/sticky-nav');
 $(function (){
   $('.menu-btn, .overlay, .sliding-panel-close').on('click touchstart', function (e) {
     $('.sidenav-mobile, .overlay').toggleClass('is-visible');
+    $('body').toggleClass('mobile-sidenav-active');
     e.preventDefault();
   });
   require('./components/generate-code-snippets');
