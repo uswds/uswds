0937c7dde4315e20c375b7b500c36f596ea4f4de
Maya Benari <hello@maya-benari.com>
2016-08-01 12:17:40 -0700

    Add various CSS improvements

--- src/js/components/header/mobile.js
+++ src/js/components/header/mobile.js
@@ -1,7 +1,7 @@
 var $ = require('jquery');
 
-$('.usa-menu-btn, .usa-overlay, .usa-sliding-panel-close').on('click touchstart', function (e) {
+$('.usa-menu-btn, .usa-overlay, .usa-header-nav-close').on('click touchstart', function (e) {
   $('.usa-overlay, .usa-header-nav').toggleClass('is-visible');
-  $('body').toggleClass('mobile-sidenav-active');
+  $('body').toggleClass('usa-mobile_nav-active');
   e.preventDefault();
 });
