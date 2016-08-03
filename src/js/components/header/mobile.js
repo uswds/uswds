f1929fcd3fa59438971deff2616c67860bef6a51
Maya Benari <hello@maya-benari.com>
2016-08-03 09:15:02 -0700

    Add focus to close button when mobile menu is opened

--- src/js/components/header/mobile.js
+++ src/js/components/header/mobile.js
@@ -3,5 +3,6 @@ var $ = require('jquery');
 $('.usa-menu-btn, .usa-overlay, .usa-header-nav-close').on('click touchstart', function (e) {
   $('.usa-overlay, .usa-header-nav').toggleClass('is-visible');
   $('body').toggleClass('usa-mobile_nav-active');
+  $('.usa-header-nav-close').focus();
   e.preventDefault();
 });
