dcc5ae49ba6c6ca5971e0aa8e57d51670cf0e8ce
Yoz (Jeremy) Grahame <jeremy.grahame@gsa.gov>
2016-08-04 15:52:53 -0700

    Add tests for dispatch

--- spec/unit/utils/dispatch.spec.js
+++ spec/unit/utils/dispatch.spec.js
@@ -14,19 +14,45 @@ describe('dispatch', function () {
   });
 
   it('attaches an event to an element', function () {
-    var element = dispatch('#butts', 'click', function (e) {
-      e.preventDefault();
-
-    });
-
+    var element = document.body;
+    var flag = false;
+    var listener = function (e) { flag = true; };
+    var dispatcher = dispatch(element, 'click', listener);
+    click(element);
+    flag.should.equal(true);
+    dispatcher.off();
   });
 
-  it('detaches an event from an element', function () {});
+  describe('it returns an object which', function () {
+    it('contains an "off" method for detaching the listener', function () {
+      // confirm that the listener has been added
+      var element = document.body;
+      var flag = false;
+      var listener = function (e) { flag = true; };
+      var dispatcher = dispatch(element, 'click', listener);
+      click(element);
+      flag.should.equal(true);
+      // now detach it
+      flag = false;
+      dispatcher.off();
+      click(element);
+      flag.should.equal(false);
+    });
 
-  it('attaches an event to an element', function () {});
-
-  it('attaches an event to an element', function () {});
-
-  it('attaches an event to an element', function () {});
-
+    it('contains a "trigger" method for calling the listener', function () {
+      var element = document.body;
+      var flag = false;
+      var listener = function (e) { flag = true; };
+      var dispatcher = dispatch(element, 'click', listener);
+      dispatcher.trigger();
+      flag.should.equal(true);
+      dispatcher.off();      
+    });
+  });
 });
+
+function click(el) {
+  var evt = document.createEvent("HTMLEvents");
+  evt.initEvent("click", false, true);
+  el.dispatchEvent(evt);
+}
