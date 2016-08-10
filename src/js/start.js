b6b30a407f5d3232e406060fc7199d2b13bb4b0a
Yoz (Jeremy) Grahame <jeremy.grahame@gsa.gov>
2016-08-09 18:10:38 -0700

    Add polyfills for forEach() and bind()

--- src/js/start.js
+++ src/js/start.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+ * The 'polyfills' file defines key ECMAScript 5 methods that may be
+ * missing from older browsers, so must be loaded first.
+ */
+require('./initializers/polyfills');
+
 require('./initializers/accordions');
 require('./initializers/footer');
 require('./initializers/skip-nav');
