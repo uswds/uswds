d81092376929abb202d96ddffb21fa77c3a985ac
Marco Segreto <marco.segreto@gsa.gov>
2016-08-19 16:20:37 -0700

    Revert "Merge pull request #1360 from 18F/add-headers"
    This reverts commit 67dd274fbc33762d8f76b6570d9f8c1b6d037df2, reversing
    changes made to 08c3e8de82d92d17297ba4f8fafc0811641f8954.

--- src/js/start.js
+++ src/js/start.js
@@ -5,7 +5,7 @@
  * missing from older browsers, so must be loaded first.
  */
 require('./initializers/polyfills');
-require('./initializers/header');
+
 require('./initializers/accordions');
 require('./initializers/footer');
 require('./initializers/skip-nav');
