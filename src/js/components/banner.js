9d819378e12787bd763221655c20bafcd27cae5a
Maya Benari <hello@maya-benari.com>
2016-08-26 15:24:25 -0700

    Update class names on banner

--- src/js/components/banner.js
+++ src/js/components/banner.js
@@ -3,12 +3,12 @@ var addClass = require('../utils/add-class');
 var removeClass = require('../utils/remove-class');
 var dispatch = require('../utils/dispatch');
 
-var headers = select('.usa-banner-official');
+var headers = select('.usa-banner-header');
 headers.forEach(function (header) {
   var click = function (e) {
     var expanded = this.getAttribute('aria-expanded') === 'true';
     var toggleClass = expanded ? addClass : removeClass;
-    toggleClass(header, 'usa-banner-official-expanded');
+    toggleClass(header, 'usa-banner-header-expanded');
   };
   select('[aria-controls]').forEach(function (button) {
     dispatch(button, 'click', click.bind(button));
