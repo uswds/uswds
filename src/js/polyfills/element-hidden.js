56fe0bc024ef64de077ce4583dddaf991c756188
Shawn Allen <shawn.allen@gsa.gov>
2016-10-14 14:26:34 -0700

    nix stylize() utility

--- src/js/polyfills/element-hidden.js
+++ src/js/polyfills/element-hidden.js
@@ -1,16 +1,7 @@
-var stylize = require('../utils/stylize');
-
 var elproto = window.HTMLElement.prototype;
 var HIDDEN = 'hidden';
 
 if (!(HIDDEN in elproto)) {
-
-  // create a singleton <style> element that hides elements with
-  // [hidden] if it doesn't exist
-  stylize(
-    'element-hidden-css',
-    '[hidden] { display: none !important; }'
-  );
 
   Object.defineProperty(elproto, HIDDEN, {
     get: function () {
