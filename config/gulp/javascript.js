3030fd3655d9ab411145350b98f84f4a32ee6f99
Shawn Allen <shawn.allen@gsa.gov>
2016-12-08 14:42:46 -0800

    remove sourcemap options (addComment defaults to true)

--- config/gulp/javascript.js
+++ config/gulp/javascript.js
@@ -58,7 +58,7 @@ gulp.task(task, [ 'eslint' ], function (done) {
         basename: dutil.pkg.name,
         suffix: '.min',
       }))
-    .pipe(sourcemaps.write('.', { addComment: true }))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist/js'));
 
   return merge(defaultStream, minifiedStream);
