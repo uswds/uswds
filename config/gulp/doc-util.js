347df6649805462ede818898ce40f903f47a939e
Shawn Allen <shawn.allen@gsa.gov>
2017-02-07 13:20:46 -0800

    remove "Draft" from gulp task

--- config/gulp/doc-util.js
+++ config/gulp/doc-util.js
@@ -80,7 +80,7 @@ module.exports = {
 
   logIntroduction: function (message) {
 
-    message = message || 'Draft U.S. Web Design Standards';
+    message = message || 'U.S. Web Design Standards';
 
     gutil.log(
       chalk.yellow('v' + pkg.version),
