c3f7fb7db8dd4a0c252c6aa25e08813f61f4a231
Shawn Allen <shawn.allen@gsa.gov>
2017-03-02 17:23:28 -0800

    include package.json data in template context

--- config/gulp/html.js
+++ config/gulp/html.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var nunjucks = require('gulp-nunjucks');
 var beautify = require('gulp-html-beautify');
+var pkg = require('../../package.json');
 
 gulp.task('html', function (done) {
   return gulp.src([
@@ -8,9 +9,10 @@ gulp.task('html', function (done) {
     ])
     .pipe(nunjucks.compile({
       lang: 'en-US',
+      'package': pkg,
       uswds: {
         path: '..',
-      }
+      },
     }))
     .pipe(beautify({
       indent_char: ' ',
