2db6d781b2ae5b1a8f49154a80e269a094887445
Shawn Allen <shawn.allen@gsa.gov>
2017-03-03 16:01:45 -0800

    rename src/template => src/templates

--- config/gulp/html.js
+++ config/gulp/html.js
@@ -5,7 +5,7 @@ var pkg = require('../../package.json');
 
 gulp.task('html', function (done) {
   return gulp.src([
-      'src/template/*.html'
+      'src/templates/*.html'
     ])
     .pipe(nunjucks.compile({
       lang: 'en-US',
