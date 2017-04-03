af2490e99d29a696986ec91a66bd6fc8689d60fc
Shawn Allen <shawn.allen@gsa.gov>
2017-04-03 13:58:47 -0700

    set up HTML builds to use same Nunjucks paths

--- config/gulp/html.js
+++ config/gulp/html.js
@@ -1,9 +1,20 @@
 var gulp = require('gulp');
+var nj = require('nunjucks');
 var nunjucks = require('gulp-nunjucks');
 var beautify = require('gulp-html-beautify');
+var path = require('path');
 var pkg = require('../../package.json');
 
 gulp.task('html', function (done) {
+  var paths = [
+    'src/templates',
+    'src/components',
+  ].map(dir => path.join(__dirname, '../../', dir));
+
+  var options = {
+    env: new nj.Environment(new nj.FileSystemLoader(paths)),
+  };
+
   return gulp.src([
       'src/templates/**/*.html'
     ])
@@ -13,7 +24,7 @@ gulp.task('html', function (done) {
       uswds: {
         path: '..',
       },
-    }))
+    }, options))
     .pipe(beautify({
       indent_char: ' ',
       indent_size: 2,
