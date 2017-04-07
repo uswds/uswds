eec17f568d78543a81dabc5e9bac895561b7406d
Shawn Allen <shawn.allen@gsa.gov>
2017-04-07 00:53:45 -0700

    always attach behaviors to <body>

--- src/js/utils/behavior.js
+++ src/js/utils/behavior.js
@@ -1,6 +1,5 @@
 'use strict';
 const receptor = require('receptor');
-const defaultTarget = document.body;
 
 /**
  * @name behaviorFactory
@@ -9,21 +8,22 @@ const defaultTarget = document.body;
  * @return {receptor.behavior}
  */
 module.exports = function behaviorFactory (events, props) {
+  const target = document.body;
+
   const behavior = receptor.behavior(events, Object.assign({
-    on: target => {
-      target = target || defaultTarget;
+    on: () => {
       if (typeof behavior.init === 'function') {
         behavior.init(target);
       }
       behavior.add(target);
     },
-    off: target => {
-      target = target || defaultTarget;
+    off: () => {
       if (typeof behavior.teardown === 'function') {
         behavior.teardown(target);
       }
       behavior.remove(target);
     },
   }, props));
+
   return behavior;
 };
