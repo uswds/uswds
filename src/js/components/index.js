014a32afb817b5512c7bfe462bbc96b6d34de929
Shawn Allen <shawn.allen@gsa.gov>
2017-04-12 15:10:04 -0700

    add components entry point

---
+++ src/js/components/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  accordion:  require('./accordion'),
+  banner:     require('./banner'),
+  footer:     require('./footer'),
+  navigation: require('./navigation'),
+  password:   require('./password'),
+  search:     require('./search'),
+  skipnav:    require('./skipnav'),
+};
+
