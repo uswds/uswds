07a9b2790c0beb682f11802c09afbffb6ac1151d
Shawn Allen <shawn.allen@gsa.gov>
2017-04-12 15:13:36 -0700

    axe should

--- spec/unit/utils/select.spec.js
+++ spec/unit/utils/select.spec.js
@@ -1,9 +1,14 @@
 'use strict';
 const fs = require('fs');
-const should = require('should');
+const assert = require('assert');
 const select = require('../../../src/js/utils/select');
 
 const TEMPLATE = fs.readFileSync(__dirname + '/select.template.html');
+
+const assertArrayWithLength = (array, length) => {
+  assert(Array.isArray(array), 'not an array: ' + (typeof array));
+  assert.equal(array.length, length);
+};
 
 describe('select', function () {
 
@@ -16,17 +21,16 @@ describe('select', function () {
   });
 
   it('returns an empty array if given a non-string selector', function () {
-    select(undefined).should.be.Array().which.has.length(0);
+    assertArrayWithLength(select(undefined), 0);
   });
 
   it('returns an Array of selected DOM elements', function () {
-    select('#id1').should.be.Array().which.has.length(1);
-    select('.firstclass').should.be.Array().which.has.length(2);
+    assertArrayWithLength(select('#id1'), 1);
+    assertArrayWithLength(select('.firstclass'), 2);
   });
 
   it('returns an Array of selected DOM elements in a particular context', function () {
-    select('.secondclass', select('.firstclass'))
-      .should.be.Array().which.has.length(1);
+    assertArrayWithLength(select('.secondclass', select('.firstclass')), 1);
   });
 
 });
