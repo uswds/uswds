5f79d28963501e3bf0bc0479e51d2fd320475a7a
Shawn Allen <shawn.allen@gsa.gov>
2017-04-11 21:27:41 -0700

    whitespace

--- src/js/polyfills/index.js
+++ src/js/polyfills/index.js
@@ -3,14 +3,14 @@ require('./element-hidden');
 
 if (!Array.prototype.forEach) {
   const forEach = require('array-foreach');
-  Array.prototype.forEach = function(fn, thisArg) {
+  Array.prototype.forEach = function (fn, thisArg) {
     return forEach(this, fn, thisArg);
   };
 }
 
 if (!Array.prototype.filter) {
   const filter = require('array-filter');
-  Array.prototype.filter = function(fn, thisArg) {
+  Array.prototype.filter = function (fn, thisArg) {
     return filter(this, fn, thisArg);
   };
 }
