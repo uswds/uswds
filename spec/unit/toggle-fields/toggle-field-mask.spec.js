9626c40db232abfd1f0fc9427d9d8d6bb94b320a
Shawn Allen <shawn.allen@gsa.gov>
2017-04-12 15:42:27 -0700

    axe jquery

--- spec/unit/toggle-fields/toggle-field-mask.spec.js
+++ spec/unit/toggle-fields/toggle-field-mask.spec.js
@@ -1,21 +1,30 @@
 'use strict';
-const $ = require('jquery');
 const assert = require('assert');
 
 const toggleFieldMask = require('../../../src/js/utils/toggle-field-mask');
 
+const fromHTML = html => {
+  const el = document.createElement('div');
+  el.innerHTML = html;
+  return el.firstChild;
+};
+
 describe('toggleFieldMask()', function () {
-  const $text = $('<input type="text" autocapitalize="on" autocorrect="on">');
-  const $password = $('<input type="password" autocapitalize="on" autocorrect="on">');
+  const text = fromHTML(
+    '<input type="text" autocapitalize="on" autocorrect="on">'
+  );
+  const password = fromHTML(
+    '<input type="password" autocapitalize="on" autocorrect="on">'
+  );
 
   it('switches the type of an input from text to password when true', function () {
-    toggleFieldMask($text.get(0), true);
-    assert.equal($text.attr('type'), 'password');
+    toggleFieldMask(text, true);
+    assert.equal(text.getAttribute('type'), 'password');
   });
 
   it('switches the type of an input from password to text when false', function () {
-    toggleFieldMask($password.get(0), false);
-    assert.equal($password.attr('type'), 'text');
+    toggleFieldMask(password, false);
+    assert.equal(password.getAttribute('type'), 'text');
   });
 });
 
