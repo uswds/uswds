996b4ac8b8b8d45b9c76bfbac8d27c71a59515cc
Shawn Allen <shawn.allen@gsa.gov>
2017-04-17 16:30:19 -0700

    don't polyfill Array.prototype

--- src/js/polyfills/index.js
+++ src/js/polyfills/index.js
@@ -1,17 +1,3 @@
 'use strict';
 require('classlist-polyfill');
 require('./element-hidden');
-
-if (!Array.prototype.forEach) {
-  const forEach = require('array-foreach');
-  Array.prototype.forEach = function (fn, thisArg) {
-    return forEach(this, fn, thisArg);
-  };
-}
-
-if (!Array.prototype.filter) {
-  const filter = require('array-filter');
-  Array.prototype.filter = function (fn, thisArg) {
-    return filter(this, fn, thisArg);
-  };
-}
