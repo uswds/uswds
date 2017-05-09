2e3ee33967d76837df5e2b5f6a5ca8b4187eea8a
Shawn Allen <shawn.allen@gsa.gov>
2017-05-09 12:45:42 -0700

    create elements without innerHTML

--- spec/unit/toggle-fields/toggle-field-mask.spec.js
+++ spec/unit/toggle-fields/toggle-field-mask.spec.js
@@ -3,26 +3,32 @@ const assert = require('assert');
 
 const toggleFieldMask = require('../../../src/js/utils/toggle-field-mask');
 
-const fromHTML = html => {
-  const el = document.createElement('div');
-  el.innerHTML = html;
-  return el.querySelector('*');
+const create = (name, attrs) => {
+  const el = document.createElement(name);
+  for (const attr in attrs) {
+    el.setAttribute(attr, attrs[attr]);
+  }
+  return el;
 };
 
 describe('toggleFieldMask()', function () {
 
   it('switches the type of an input from text to password when true', function () {
-    const text = fromHTML(
-      '<input type="text" autocapitalize="on" autocorrect="on">'
-    );
+    const text = create('input', {
+      type: 'text',
+      autocapitalize: 'off',
+      autocorrect: 'off',
+    });
     toggleFieldMask(text, true);
     assert.equal(text.getAttribute('type'), 'password');
   });
 
   it('switches the type of an input from password to text when false', function () {
-    const password = fromHTML(
-      '<input type="password" autocapitalize="on" autocorrect="on">'
-    );
+    const password = create('input', {
+      type: 'password',
+      autocapitalize: 'off',
+      autocorrect: 'off',
+    });
     toggleFieldMask(password, false);
     assert.equal(password.getAttribute('type'), 'text');
   });
