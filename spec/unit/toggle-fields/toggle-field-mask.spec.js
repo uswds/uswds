cf4f8d86f2ffe130f29da2a9e374592597fa7ca8
Shawn Allen <shawn.allen@gsa.gov>
2017-05-09 12:40:55 -0700

    reorganize test vars

--- spec/unit/toggle-fields/toggle-field-mask.spec.js
+++ spec/unit/toggle-fields/toggle-field-mask.spec.js
@@ -6,23 +6,23 @@ const toggleFieldMask = require('../../../src/js/utils/toggle-field-mask');
 const fromHTML = html => {
   const el = document.createElement('div');
   el.innerHTML = html;
-  return el.firstChild;
+  return el.querySelector('*');
 };
 
 describe('toggleFieldMask()', function () {
-  const text = fromHTML(
-    '<input type="text" autocapitalize="on" autocorrect="on">'
-  );
-  const password = fromHTML(
-    '<input type="password" autocapitalize="on" autocorrect="on">'
-  );
 
   it('switches the type of an input from text to password when true', function () {
+    const text = fromHTML(
+      '<input type="text" autocapitalize="on" autocorrect="on">'
+    );
     toggleFieldMask(text, true);
     assert.equal(text.getAttribute('type'), 'password');
   });
 
   it('switches the type of an input from password to text when false', function () {
+    const password = fromHTML(
+      '<input type="password" autocapitalize="on" autocorrect="on">'
+    );
     toggleFieldMask(password, false);
     assert.equal(password.getAttribute('type'), 'text');
   });
