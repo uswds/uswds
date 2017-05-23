6058d2bea1ae8f9fbc542d32838e59bf3afebd80
Shawn Allen <shawn.allen@gsa.gov>
2017-05-22 17:10:43 -0700

    add browserify/js task

--- examples/grunt/gruntfile.js
+++ examples/grunt/gruntfile.js
@@ -3,24 +3,47 @@ module.exports = function(grunt) {
 
     pkg: grunt.file.readJSON('package.json'),
 
-    sass: {
-      options: {
-        style: 'compressed',
-        includePaths: [
-          'node_modules/uswds/src/stylesheets',
-        ],
-      },
+    browserify: {
       dist: {
-        files: {
-          'assets/css/style.css': 'src/css/style.scss'
+        src: 'src/js/main.js',
+        dest: './assets/js/main.js',
+        options: {
+          browserifyOptions: {
+            debug: true,
+            transform: [
+              [
+                'babelify',
+                {
+                  presets: ['es2015'],
+                },
+              ]
+            ],
+          },
+        },
+      },
+    },
+
+    sass: {
+      dist: {
+        src: 'src/css/style.scss',
+        dest: './assets/css/style.css',
+        options: {
+          style: 'nested',
+          includePaths: [
+            'node_modules/uswds/src/stylesheets',
+          ],
         },
       },
     },
 
     watch: {
+      js: {
+        files: 'src/js/**/*.js',
+        task: 'browserify',
+      },
       sass: {
         files: 'src/css/**/*.scss',
-        tasks: ['sass'],
+        task: 'sass',
       },
     },
 
@@ -28,8 +51,12 @@ module.exports = function(grunt) {
 
   require('load-grunt-tasks')(grunt);
 
+  // because `grunt js` is easier to type than `grunt browserify`
+  grunt.registerTask('js', ['browserify']);
+
   grunt.registerTask('default', [
+    'js',
     'sass',
-    // 'watch'
+    'watch',
   ]);
 };
