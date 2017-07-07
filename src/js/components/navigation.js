eefb76aa33f5d5d2ea9eb3cf78b1ea9fa4d8dc6a
Atul Varma <varmaa@gmail.com>
2017-07-07 12:02:55 -0400

    Collapse nav on link click. Fixes #1962.

--- src/js/components/navigation.js
+++ src/js/components/navigation.js
@@ -2,12 +2,14 @@
 const behavior = require('../utils/behavior');
 const forEach = require('array-foreach');
 const select = require('../utils/select');
+const accordion = require('./accordion');
 
 const CLICK = require('../events').CLICK;
 const PREFIX = require('../config').prefix;
 
 const CONTEXT = 'header';
 const NAV = `.${PREFIX}-nav`;
+const NAV_LINKS = `${NAV} a`;
 const OPENERS = `.${PREFIX}-menu-btn`;
 const CLOSE_BUTTON = `.${PREFIX}-nav-close`;
 const OVERLAY = `.${PREFIX}-overlay`;
@@ -42,6 +44,23 @@ const navigation = behavior({
   [ CLICK ]: {
     [ OPENERS ]: toggleNav,
     [ CLOSERS ]: toggleNav,
+    [ NAV_LINKS ]: function() {
+      // A navigation link has been clicked! We want to collapse any
+      // hierarchical navigation UI it's a part of, so that the user
+      // can focus on whatever they've just selected.
+
+      // Some navigation links are inside accordions; when they're
+      // clicked, we want to collapse those accordions.
+      const acc = this.closest(accordion.ACCORDION);
+      if (acc) {
+        accordion.getButtons(acc).forEach(btn => accordion.hide(btn));
+      }
+
+      // If the mobile navigation menu is active, we want to hide it.
+      if (document.body.classList.contains(ACTIVE_CLASS)) {
+        toggleNav.call(this);
+      }
+    },
   },
 });
 
