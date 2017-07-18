5a773ad8e60be063ed2853f2085a9c90f17b86d0
Atul Varma <varmaa@gmail.com>
2017-07-18 07:49:54 -0400

    Add tests for is-in-viewport.js (#2046).

--- src/js/utils/is-in-viewport.js
+++ src/js/utils/is-in-viewport.js
@@ -1,11 +1,14 @@
 // https://stackoverflow.com/a/7557433
-module.exports = function isElementInViewport (el) {
+function isElementInViewport (el, win=window,
+                              htmlEl=document.documentElement) {
   var rect = el.getBoundingClientRect();
 
   return (
     rect.top >= 0 &&
     rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+    rect.bottom <= (win.innerHeight || htmlEl.clientHeight) &&
+    rect.right <= (win.innerWidth || htmlEl.clientWidth)
   );
-};
+}
+
+module.exports = isElementInViewport;
