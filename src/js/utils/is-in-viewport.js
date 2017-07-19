93e117c1e4ca57c30582a3e1446e1cf0e26617a1
Atul Varma <varmaa@gmail.com>
2017-07-19 08:02:57 -0400

    Rename htmlEl to docEl.

--- src/js/utils/is-in-viewport.js
+++ src/js/utils/is-in-viewport.js
@@ -1,13 +1,13 @@
 // https://stackoverflow.com/a/7557433
 function isElementInViewport (el, win=window,
-                              htmlEl=document.documentElement) {
+                              docEl=document.documentElement) {
   var rect = el.getBoundingClientRect();
 
   return (
     rect.top >= 0 &&
     rect.left >= 0 &&
-    rect.bottom <= (win.innerHeight || htmlEl.clientHeight) &&
-    rect.right <= (win.innerWidth || htmlEl.clientWidth)
+    rect.bottom <= (win.innerHeight || docEl.clientHeight) &&
+    rect.right <= (win.innerWidth || docEl.clientWidth)
   );
 }
 
