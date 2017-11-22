5c6ce2e9f172d806392497890beafdf5ea2ced94
Scott Kellum <scott.a.kellum@accenturefederal.com>
2017-11-22 16:14:34 -0500

    infographics commit

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,11 +1,11 @@
 module.exports = {
-  accordion:  require('./accordion'),
-  banner:     require('./banner'),
-  footer:     require('./footer'),
-  navigation: require('./navigation'),
-  password:   require('./password'),
-  search:     require('./search'),
-  skipnav:    require('./skipnav'),
-  validator:  require('./validator'),
+  accordion:    require('./accordion'),
+  banner:       require('./banner'),
+  footer:       require('./footer'),
+  infographics: require('./infographics'),
+  navigation:   require('./navigation'),
+  password:     require('./password'),
+  search:       require('./search'),
+  skipnav:      require('./skipnav'),
+  validator:    require('./validator'),
 };
-
