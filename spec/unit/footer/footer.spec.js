89653659b97398eb7a7bbf462201cd4d957783cb
Maya Benari <hello@maya-benari.com>
2019-04-15 16:05:19 -0700

    Change name of list function to primary content

--- spec/unit/footer/footer.spec.js
+++ spec/unit/footer/footer.spec.js
@@ -7,7 +7,7 @@ const TEMPLATE = fs.readFileSync(path.join(__dirname, '/template.html'));
 
 const { DEBOUNCE_RATE } = behavior;
 const HIDDEN = 'hidden';
-const LIST_SELECTOR = '.usa-footer--big .usa-footer__primary-content--collapsible';
+const PRIMARY_CONTENT_SELECTOR = '.usa-footer--big .usa-footer__primary-content--collapsible';
 const BUTTON_SELECTOR = '.usa-footer__primary-link';
 
 /**
@@ -43,7 +43,7 @@ describe('big footer accordion', () => {
   beforeEach(() => {
     body.innerHTML = TEMPLATE;
 
-    lists = document.querySelectorAll(LIST_SELECTOR);
+    lists = document.querySelectorAll(PRIMARY_CONTENT_SELECTOR);
     buttons = document.querySelectorAll(BUTTON_SELECTOR);
 
     window.innerWidth = 1024;
