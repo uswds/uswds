7e0a6c4116c94e8bc350fe105da6d32c75fca007
James Mejia <james.mejia@bixal.com>
2020-04-23 10:16:06 -0500

    Add card js.
    Make card clickable if user clicks on card. Allow text to be selectable
    if time difference between mouseUp and mouseDown is over 200ms.

--- src/js/components/card.js
+++ src/js/components/card.js
@@ -1,18 +1,40 @@
+// https://inclusive-components.design/cards#theredundantclickevent
+
 const behavior = require('../utils/behavior');
+const select = require('../utils/select');
 const { prefix: PREFIX } = require("../config");
 const { CLICK } = require('../events');
 
-const CARD = `.${PREFIX}-card`;
+const CARD = `.${PREFIX}-card--clickable`;
+const CURSOR = 'pointer';
+const ACTION = `.${PREFIX}-card__action`;
 
-const handleClick = () => {
-  console.log('testing');
+const card = behavior(
+  {},
+  {
+    init(root) {
+      if (!CARD.length) {
+        return;
+      }
 
-};
+      let down;
+      let up;
 
-const card = behavior({
-  [CLICK]: {
-    [CARD]: handleClick
+      select(CARD, root).forEach(card => {
+        const [ cardAction ] = select(ACTION, card);
+
+        card.style.cursor = CURSOR;
+        card.onmousedown = () => down = +new Date();
+        card.onmouseup = () => {
+          let timeDifference = up - down;
+          up = +new Date();
+          if (timeDifference < 200) {
+            cardAction.click();
+          }
+        };
+      });
+    }
   }
-});
+);
 
 module.exports = card;
