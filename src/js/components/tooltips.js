edd2fe106a7d9f50e286741c59cfd0859830be1c
Jared Cunha <jaredcunha@gmail.com>
2020-05-20 14:27:40 -0400

    add comments

--- src/js/components/tooltips.js
+++ src/js/components/tooltips.js
@@ -26,13 +26,10 @@ const addListenerMulti = (element, eventNames, listener) => {
   }
 }
 
-/* Add one or more listeners to an element
-** @param {DOMElement} element - DOM element to add listeners to
-** @param {string} eventNames - space separated list of event names, e.g. 'click change'
-** @param {Function} listener - function to attach for each event as a listener
+/* Creates the tooltip
+** @param {HTMLElement} toolitp - the element that initializes the tooltip
 */
 const createToolTip = tooltip => {
-
   let tooltipID = "tooltip" + (Math.floor(Math.random()*90000) + 10000);
   let tooltipContent = tooltip.getAttribute("title");
   let wrapper = document.createElement('div');
@@ -40,7 +37,7 @@ const createToolTip = tooltip => {
   let position = tooltip.getAttribute("data-position") ? tooltip.getAttribute("data-position") : 'top';
   let originalPosition = position;
 
-  // Set attributes
+  // Set up tooltip attributes
   tooltip.setAttribute("aria-describedby", tooltipID);
   tooltip.setAttribute("tabindex", "0");
   tooltip.setAttribute("title", "");
@@ -49,25 +46,26 @@ const createToolTip = tooltip => {
   // insert wrapper before el in the DOM tree
   tooltip.parentNode.insertBefore(wrapper, tooltip);
 
-  // move el into wrapper
+  // set up the wrapper
   wrapper.appendChild(tooltip);
   wrapper.classList.add(WRAPPER_CLASS);
   wrapper.appendChild(tooltipBody);
 
-  // make accessible
+  // set up the tooltip body
   tooltipBody.classList.add(TOOLTIP_CLASS);
   tooltipBody.setAttribute("id", tooltipID);
   tooltipBody.setAttribute("role", "tooltip");
   tooltipBody.setAttribute("aria-hidden", "true");
 
-  // Place the text in the tooltip
+  // place the text in the tooltip
   tooltipBody.innerHTML = tooltipContent;
 
   const showToolTip = event => {
+    // This sets up the tooltip body. The opacity is 0, but
+    // we can begin running the calculations below.
     tooltipBody.classList.add(SET_CLASS);
-    tooltip.classList.add(TOOLTIP_CLASS + '--' + position);
 
-    // Adjust positioning in case there are margins.
+    // Calculate sizing and adjustments for positioning
     let tooltipWidth = tooltip.offsetWidth;
     let tooltipHeight = tooltip.offsetHeight;
     let offsetForRightMargin = parseInt(window.getComputedStyle(tooltip).getPropertyValue("margin-right"));
@@ -79,16 +77,27 @@ const createToolTip = tooltip => {
     let adjustToEdgeX = tooltipWidth + TRIANGLE_SIZE + SPACER;
     let adjustToEdgeY = tooltipHeight + TRIANGLE_SIZE + SPACER;
 
-    console.log(offsetForTooltipBodyHeight);
-
-    const resetToPosition = newPos => {
+    /* Position the tooltip body when the trigger is hovered
+    ** Removes old positioning classnames and reapplies. This allows
+    ** positioning to change in case the user resizes browser or DOM manipulation
+    ** causes tooltip to get clipped from viewport
+    **
+    ** @param {string} position - can be "top", "bottom", "right", "left"
+    */
+    const adjustToPosition = setPos => {
       tooltip.classList.remove(TOOLTIP_CLASS  + '--' + position);
-      position = newPos;
+      position = setPos;
       tooltip.classList.add(TOOLTIP_CLASS + '--' + position);
     }
 
+    /* Positions tooltip at the top
+    ** We check if the element is in theviewport so we know whether or not we
+    ** need to constrain the width
+    **
+    ** @param {HTMLElement} tooltip body
+    */
     const positionTop = e => {
-      resetToPosition("top");
+      adjustToPosition("top");
       e.style.marginLeft = adjustHorizontalCenter + "px"
       if (!isElementInViewport(e)) {
         e.classList.add(ADJUST_WIDTH_CLASS);
@@ -96,8 +105,14 @@ const createToolTip = tooltip => {
       e.style.marginBottom = adjustToEdgeY + offsetForBottomMargin + "px";
     }
 
+    /* Positions tooltip at the bottom
+    ** We check if the element is in theviewport so we know whether or not we
+    ** need to constrain the width
+    **
+    ** @param {HTMLElement} tooltip body
+    */
     const positionBottom = e => {
-      resetToPosition("bottom");
+      adjustToPosition("bottom");
       e.style.marginLeft = adjustHorizontalCenter + "px"
       if (!isElementInViewport(e)) {
         e.classList.add(ADJUST_WIDTH_CLASS);
@@ -105,18 +120,30 @@ const createToolTip = tooltip => {
       e.style.marginTop = adjustToEdgeY + offsetForTopMargin + "px";
     }
 
+    /* Positions tooltip at the right
+    **
+    ** @param {HTMLElement} tooltip body
+    */
     const positionRight = e => {
-      resetToPosition("right");
-      e.style.marginLeft = (adjustToEdgeX + offsetForLeftMargin) + "px")
+      adjustToPosition("right");
+      e.style.marginLeft = (adjustToEdgeX + offsetForLeftMargin) + "px"
       e.style.bottom = ((tooltipHeight - offsetForTooltipBodyHeight) / 2) + offsetForBottomMargin + "px";
     }
 
+    /* Positions tooltip at the right
+    **
+    ** @param {HTMLElement} tooltip body
+    */
     const positionLeft = e => {
-      resetToPosition("left");
-      e.style.marginRight = (adjustToEdgeX + offsetForRightMargin) + "px");
+      adjustToPosition("left");
+      e.style.marginRight = (adjustToEdgeX + offsetForRightMargin) + "px";
       e.style.bottom = ((tooltipHeight - offsetForTooltipBodyHeight) / 2) + offsetForBottomMargin + "px";
     }
 
+    /* We try to set the position based on the
+    ** original intention, but make adjustments
+    ** if the element is clipped out of the viewport
+    */
     switch(position) {
       case "top":
         positionTop(tooltipBody);
@@ -147,15 +174,24 @@ const createToolTip = tooltip => {
             positionTop(tooltipBody);
           }
         }
-        break
+      break
     }
 
-    // Actually show the tooltip
+    /* Actually show the tooltip. The VISIBLE_CLASS
+    ** will change the opacity to 1
+    */
     setTimeout(function(){
       tooltipBody.classList.add(VISIBLE_CLASS);
     }, 20);
   };
 
+  /* Removes all the properties to show and position the tooltip,
+  ** and resets the tooltip position to the original intention
+  ** in case the window is resized or the element is moved through
+  ** DOM maniulation.
+  **
+  ** @param {event}
+  */
   const hideToolTip = event => {
     tooltipBody.classList.remove(VISIBLE_CLASS);
     tooltipBody.classList.remove(SET_CLASS);
@@ -164,7 +200,7 @@ const createToolTip = tooltip => {
     position = originalPosition;
   };
 
-
+  // Listeners for showing and hiding the tooltip 
   addListenerMulti(tooltip, 'mouseenter focus', function(){
     showToolTip(event);
     return false;
@@ -176,7 +212,6 @@ const createToolTip = tooltip => {
   });
 
 }
-
 
 // Setup our function to run on various events
 const tooltips = behavior(
