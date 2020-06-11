98c06122a0e9a217012c5bf5d7fd311295d7b23d
Jared Cunha <jaredcunha@gmail.com>
2020-06-11 17:06:23 -0400

    refactor and lint fixing

--- src/js/components/dropzone.js
+++ src/js/components/dropzone.js
@@ -6,22 +6,28 @@ const DROPZONE = `.${PREFIX}-dropzone`;
 const INPUT = `.${PREFIX}-dropzone__input`;
 const INITIALIZED_CLASS = `${PREFIX}-dropzone--is-initialized`;
 const INSTRUCTIONS = `.${PREFIX}-dropzone__instructions`;
-const PREVIEW_CLASS = 'usa-dropzone__preview';
-const DRAG_CLASS = 'usa-dropzone--drag';
+const PREVIEW_CLASS = `${PREFIX}-dropzone__preview`;
+const DRAG_CLASS = `${PREFIX}-dropzone--drag`;
 const LOADING_CLASS = 'is-loading';
-const GENERIC_PREVIEW_CLASS = 'usa-dropzone__preview__image--generic';
+const GENERIC_PREVIEW_CLASS = `${PREFIX}-dropzone__preview__image--generic`;
 const SPACER_GIF = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
 
 
+/**
+ * Takes file name of file(s) selected and creates
+ * safe ID name, stripping invalid characters
+ *
+ * @param {String} name - The file name selected
+ * @returns {String} - ID with only valid characters
+ */
 const makeSafeForID = name => {
-    return name.replace(/[^a-z0-9]/g, function(s) {
-        const c = s.charCodeAt(0);
-        if (c === 32) return '-';
-        if (c >= 65 && c <= 90) return `img_${s.toLowerCase()}`;
-        return `__${("000", c.toString(16)).slice(-4)}`;
-    });
+  return name.replace(/[^a-z0-9]/g, function replaceName(s) {
+    const c = s.charCodeAt(0);
+    if (c === 32) return '-';
+    if (c >= 65 && c <= 90) return `img_${s.toLowerCase()}`;
+    return `__${("000", c.toString(16)).slice(-4)}`;
+  });
 }
-
 
 /**
  * Returns the root and message element
@@ -30,8 +36,6 @@ const makeSafeForID = name => {
  * @param {HTMLinputElement|HTMLTextAreaElement} inputEl The character count input element
  * @returns {CharacterCountElements} elements The root and message element.
  */
-
-
 const getinputElements = inputEl => {
   const dropzoneEl = inputEl.closest(DROPZONE);
   const dropzoneInstructions = inputEl.querySelector(INSTRUCTIONS);
@@ -53,18 +57,6 @@ const setupAttributes = inputEl => {
 
   dropzoneEl.classList.add(INITIALIZED_CLASS);
 
-  inputEl.ondragover = e => {
-    dropzoneEl.classList.add(DRAG_CLASS);
-  }
-
-  inputEl.ondragleave = e => {
-    dropzoneEl.classList.remove(DRAG_CLASS);
-  }
-
-  inputEl.ondrop = e => {
-    dropzoneEl.classList.remove(DRAG_CLASS);
-  }
-
 };
 
 
@@ -74,72 +66,78 @@ const setupAttributes = inputEl => {
  * @param {HTMLinputElement|HTMLTextAreaElement} inputEl The character count input element
  */
 
-const handleChange = inputEl => {
-  const { dropzoneEl, dropzoneInstructions } = getinputElements(inputEl);
-
-  inputEl.onchange = e => {
+const handleChange = (e, inputEl, dropzoneEl, dropzoneInstructions) => {
     const fileNames = e.target.files;
     const filePreviews = dropzoneEl.querySelectorAll(`.${PREVIEW_CLASS}`);
 
     // Get rid of existing previews if they exist
     if (filePreviews !== null){
-      Array.prototype.forEach.call( filePreviews, function( node ) {
-        node.parentNode.removeChild( node );
+      Array.prototype.forEach.call(filePreviews, function removePreviews(node) {
+        node.parentNode.removeChild(node);
       });
     }
 
-    for (let i = 0; i < fileNames.length; i++)
+    for (let i = 0; i < fileNames.length; i += 1)
     {
-     const reader  = new FileReader();
+     const reader = new FileReader();
+     const fileName = fileNames[i].name;
 
-     reader.onloadstart = function() {
+     reader.onloadstart = function createFilePreview() {
        const imageId = makeSafeForID(fileName);
        const previewImage = `<img id="${imageId}" src="${SPACER_GIF}" alt="" class="usa-dropzone__preview__image  ${LOADING_CLASS}"/>`;
 
        dropzoneInstructions.insertAdjacentHTML('afterend', `<div class="${PREVIEW_CLASS}" aria-hidden="true">${previewImage}${fileName}<div>`);
-
      }
 
-     reader.onloadend = function() {
+     reader.onloadend = function createGenericFilePreview() {
        const imageId = makeSafeForID(fileName);
        const previewImage = document.getElementById(imageId);
 
        previewImage.setAttribute("onerror",`this.onerror=null;this.src="${SPACER_GIF}"; this.classList.add("${GENERIC_PREVIEW_CLASS}")`)
-
        previewImage.classList.remove(LOADING_CLASS);
        previewImage.src = reader.result;
-
      }
-
-     const fileName = fileNames[i].name;
 
      if (fileNames[i]) {
         reader.readAsDataURL(fileNames[i]);
      }
-     else {
-       preview.src = "";
-     }
-
     }
-  }
 }
 
+const handleDragOver = el => {
+  el.classList.add(DRAG_CLASS);
+}
 
+const handleDragLeave = el => {
+  el.classList.remove(DRAG_CLASS);
+}
+
+const handleDrop = el => {
+  el.classList.remove(DRAG_CLASS);
+}
 
 
 const dropzone = behavior(
   {
-    input: {
-      [INPUT]() {
-        // handleChange(this);
-      }
-    }
   },
   {
     init(root) {
-      select(DROPZONE, root).forEach(input => {
-        setupAttributes(input);
-        handleChange(input);
+      select(DROPZONE, root).forEach(inputEl => {
+        const { dropzoneEl, dropzoneInstructions } = getinputElements(inputEl);
+
+        setupAttributes(inputEl);
+        inputEl.onchange = e => {
+          handleChange(e, inputEl, dropzoneEl, dropzoneInstructions)
+        }
+        dropzoneEl.ondragover = () => {
+          handleDragOver(dropzoneEl);
+        }
+        dropzoneEl.ondragleave = () => {
+          handleDragLeave(dropzoneEl);
+        }
+        dropzoneEl.ondrop = () => {
+          handleDrop(dropzoneEl);
+        }
       });
     }
   }
