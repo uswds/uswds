524ea9475bd85823b6b48033e8126d939312f2b2
William Bagayoko <wbagayoko@gmail.com>
2020-07-02 15:31:11 -0500

    3355 update ipad detection

--- src/js/utils/is-ios-device.js
+++ src/js/utils/is-ios-device.js
@@ -2,7 +2,8 @@
 function isIosDevice() {
   return (
     typeof navigator !== "undefined" &&
-    navigator.userAgent.match(/(iPod|iPhone|iPad)/g) &&
+    (navigator.userAgent.match(/(iPod|iPhone|iPad)/g) ||
+      (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1)) &&
     !window.MSStream
   );
 }
