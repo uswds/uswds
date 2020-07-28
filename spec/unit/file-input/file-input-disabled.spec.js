2643735f69752aa7ea811cdc491c7db4dc7469f8
Dano Williams <daniel.williams@gsa.gov>
2020-07-27 21:42:29 -0700

    Use template files for testing

--- spec/unit/file-input/file-input-disabled.spec.js
+++ spec/unit/file-input/file-input-disabled.spec.js
@@ -3,28 +3,33 @@ const fileInput = require("../../../src/js/components/file-input");
 const fs = require("fs");
 const path = require("path");
 
-const TEMPLATE = fs.readFileSync(path.join(__dirname, "/file-input-disabled.template.html"));
+const RENDER = "../../../build/components/render/";
+const TEMPLATE = fs.readFileSync(
+  path.join(__dirname, RENDER, "file-input--disabled.html")
+);
 
 describe("file input is disabled", () => {
- const { body } = document;
+  const { body } = document;
 
- let component;
+  let component;
 
- beforeEach(() => {
-   body.innerHTML = TEMPLATE;
-   fileInput.on();
-   component = body.querySelector(".usa-file-input");
-   fileInput.on(body);
- });
+  beforeEach(() => {
+    body.innerHTML = TEMPLATE;
+    fileInput.on();
+    component = body.querySelector(".usa-file-input");
+    fileInput.on(body);
+  });
 
- afterEach(() => {
-   body.innerHTML = "";
-   fileInput.off(body);
- });
+  afterEach(() => {
+    body.innerHTML = "";
+    fileInput.off(body);
+  });
 
- it('has disabled styling', () => {
-   const expectedClass = "usa-file-input--disabled";  
-   assert.strictEqual(component.getAttribute("class").includes(expectedClass), true);
- });
-
-})
+  it("has disabled styling", () => {
+    const expectedClass = "usa-file-input--disabled";
+    assert.strictEqual(
+      component.getAttribute("class").includes(expectedClass),
+      true
+    );
+  });
+});
