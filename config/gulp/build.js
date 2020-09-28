b745eefdc321c6a2160df25be20d8463c1632098
Dano Williams <daniel.williams@gsa.gov>
2020-09-28 16:21:21 -0700

    Don't include duplicate docs files in dist directory

--- config/gulp/build.js
+++ config/gulp/build.js
@@ -16,17 +16,6 @@ gulp.task("clean-dist", (done) => {
   return del("dist");
 });
 
-gulp.task("docs", (done) => {
-  dutil.logMessage("docs", "Copying documentation dist dir");
-
-  const stream = gulp
-    .src(["README.md", "LICENSE.md", "CONTRIBUTING.md"])
-    .pipe(gulp.dest("dist"));
-
-  done();
-  return stream;
-});
-
 gulp.task(
   "build",
   gulp.series(
@@ -36,7 +25,6 @@ gulp.task(
       done();
     },
     "clean-dist",
-    "docs",
     gulp.parallel("sass", "javascript", "images", "fonts"),
     // We need to copy the Sass to dist *after* the sass task, to ensure
     // that vendor libraries have been copied to the Sass directory first.
