f2e71cd44ecf91973fcf704afd197150db3b4862
Andrew Duthie <andrew.duthie@gsa.gov>
2020-11-04 15:06:00 -0500

    Try: Implement banner toggle as substituted noscript markup

--- src/js/components/banner.js
+++ src/js/components/banner.js
@@ -1,17 +1,33 @@
+const select = require("../utils/select");
 const behavior = require("../utils/behavior");
 const { CLICK } = require("../events");
 const { prefix: PREFIX } = require("../config");
 
 const HEADER = `.${PREFIX}-banner__header`;
+const CONTENT = `.${PREFIX}-banner__content`;
 const EXPANDED_CLASS = `${PREFIX}-banner__header--expanded`;
+const JS_ENABLED_CLASS = `${PREFIX}-banner--js-enabled`;
 
 const toggleBanner = function toggleEl(event) {
   event.preventDefault();
   this.closest(HEADER).classList.toggle(EXPANDED_CLASS);
 };
 
-module.exports = behavior({
-  [CLICK]: {
-    [`${HEADER} [aria-controls]`]: toggleBanner,
+function replaceNoScript(content) {
+  content.classList.add(JS_ENABLED_CLASS);
+  const noscript = content.querySelector("noscript");
+  if (noscript) noscript.outerHTML = noscript.textContent;
+}
+
+module.exports = behavior(
+  {
+    [CLICK]: {
+      [`${HEADER} [aria-controls]`]: toggleBanner,
+    },
   },
-});
+  {
+    init(root) {
+      select(CONTENT, root).forEach(replaceNoScript);
+    },
+  }
+);
