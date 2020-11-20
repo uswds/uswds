aa00f937c5ce841b154d09770e740aef5adc53e7
Jared Cunha <jared.cunha@gsa.gov>
2020-11-20 16:36:47 -0500

    update gulp series

--- config/gulp/build.js
+++ config/gulp/build.js
@@ -37,7 +37,8 @@ gulp.task(
     },
     "clean-dist",
     "docs",
-    "svg-sprite",
+    "build-sprite",
+    "rename-sprite",
     gulp.parallel("sass", "javascript", "images", "fonts"),
     // We need to copy the Sass to dist *after* the sass task, to ensure
     // that vendor libraries have been copied to the Sass directory first.
