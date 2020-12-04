0c16b2f44205bf538a4118889be32b84580d0132
jsarnik <jsarnik-a@jsarnik-l1.local>
2020-12-04 11:01:56 -0500

    update the global variable to be on window object

--- src/js/start.js
+++ src/js/start.js
@@ -1,8 +1,6 @@
 const domready = require("domready");
 
-window.uswdsPresent = true;
-const UswdsGlobalLibraryObject = {}; // GLOBAL variable to indicate that the uswds.js has loaded in the DOM.
-
+window.uswdsPresent = true; // GLOBAL variable to indicate that the uswds.js has loaded in the DOM.
 
 /**
  * The 'polyfills' define key ECMAScript 5 methods that may be missing from
@@ -17,9 +15,6 @@ const components = require("./components");
 uswds.components = components;
 
 domready(() => {
-  if(typeof UswdsGlobalLibraryObject !== 'undefined'){
-    console.log('test')
-  }
   const target = document.body;
   Object.keys(components).forEach((key) => {
     const behavior = components[key];
