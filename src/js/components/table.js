aff1bd2c291e4c09eb3b06c374763ca0a2b26db3
Sarah Rudder <sarah.rudder@gsa.gov>
2021-02-10 16:26:55 -0600

    Use DS prefix

--- src/js/components/table.js
+++ src/js/components/table.js
@@ -10,7 +10,7 @@ const DESCENDING = "descending";
 const SORT_OVERRIDE = "data-sort-value";
 
 const ICON_SOURCE = `
-  <svg class="usa-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <svg class="${PREFIX}-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
     <g id="descending">
       <path d="M19,15l-1.41-1.41L13,18.17V2H11v16.17l-4.59-4.59L5,15l7,7L19,15z"/>
     </g>
