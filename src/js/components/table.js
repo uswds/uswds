9563c96dbcd8788f8bede3f4cae425f23b12939d
James Mejia <james.mejia@gsa.gov>
2021-02-16 14:02:46 -0600

    Run prettier on table.js.

--- src/js/components/table.js
+++ src/js/components/table.js
@@ -28,7 +28,6 @@ const SORT_BUTTON = `.${SORT_BUTTON_CLASS}`;
 const SORTABLE_HEADER = `th[data-sortable]`;
 const ANNOUNCEMENT_REGION = `.${PREFIX}-table__announcement-region[aria-live="polite"]`;
 
-
 /** Gets the data-sort-value attribute value, if provided — otherwise, gets
  * the innerText or textContent — of the child element (HTMLTableCellElement)
  * at the specified index of the given table row
@@ -37,9 +36,10 @@ const ANNOUNCEMENT_REGION = `.${PREFIX}-table__announcement-region[aria-live="po
  * @param {array<HTMLTableRowElement>} tr
  * @return {boolean}
  */
-const getCellValue = (tr, index) => tr.children[index].getAttribute(SORT_OVERRIDE)
-  || tr.children[index].innerText
-  || tr.children[index].textContent;
+const getCellValue = (tr, index) =>
+  tr.children[index].getAttribute(SORT_OVERRIDE) ||
+  tr.children[index].innerText ||
+  tr.children[index].textContent;
 
 /**
  * Compares the values of two row array items at the given index, then sorts by the given direction
@@ -48,19 +48,27 @@ const getCellValue = (tr, index) => tr.children[index].getAttribute(SORT_OVERRID
  * @return {boolean}
  */
 const compareFunction = (index, isAscending) => (thisRow, nextRow) => {
-
   // get values to compare from data attribute or cell content
   const value1 = getCellValue(isAscending ? thisRow : nextRow, index);
   const value2 = getCellValue(isAscending ? nextRow : thisRow, index);
 
   // if neither value is empty, and if both values are already numbers, compare numerically
-  if (value1 && value2 && !Number.isNaN(Number(value1)) && !Number.isNaN(Number(value2))) {
+  if (
+    value1 &&
+    value2 &&
+    !Number.isNaN(Number(value1)) &&
+    !Number.isNaN(Number(value2))
+  ) {
     return value1 - value2;
-  } 
+  }
   // Otherwise, compare alphabetically based on current user locale
-  return value1.toString().localeCompare(value2, navigator.language, {numeric: true, ignorePunctuation: true})
-}
-
+  return value1
+    .toString()
+    .localeCompare(value2, navigator.language, {
+      numeric: true,
+      ignorePunctuation: true,
+    });
+};
 
 /**
  * Get an Array of column headers elements belonging directly to the given
@@ -81,12 +89,21 @@ const getColumnHeaders = (table) => {
 const updateSortLabel = (header) => {
   const headerName = header.innerText;
   const sortedAscending = header.getAttribute(SORTED) === ASCENDING;
-  const isSorted = header.getAttribute(SORTED) === ASCENDING || header.getAttribute(SORTED) === DESCENDING || false;
-  const headerLabel = `${headerName}', sortable column, currently ${isSorted ? `${sortedAscending ? `sorted ${ASCENDING}` : `sorted ${DESCENDING}`}`: 'unsorted'}`;
-  const headerButtonLabel = `Click to sort by ${headerName} in ${sortedAscending ? DESCENDING : ASCENDING} order.`;
+  const isSorted =
+    header.getAttribute(SORTED) === ASCENDING ||
+    header.getAttribute(SORTED) === DESCENDING ||
+    false;
+  const headerLabel = `${headerName}', sortable column, currently ${
+    isSorted
+      ? `${sortedAscending ? `sorted ${ASCENDING}` : `sorted ${DESCENDING}`}`
+      : "unsorted"
+  }`;
+  const headerButtonLabel = `Click to sort by ${headerName} in ${
+    sortedAscending ? DESCENDING : ASCENDING
+  } order.`;
   header.setAttribute("aria-label", headerLabel);
   header.querySelector(SORT_BUTTON).setAttribute("title", headerButtonLabel);
-}
+};
 
 /**
  * Remove the aria-sort attribute on the given header element, and reset the label and button icon
@@ -95,7 +112,7 @@ const updateSortLabel = (header) => {
 const unsetSort = (header) => {
   header.removeAttribute(SORTED);
   updateSortLabel(header);
-}
+};
 
 /**
  * Sort rows either ascending or descending, based on a given header's aria-sort attribute
@@ -104,10 +121,10 @@ const unsetSort = (header) => {
  * @return {boolean} true
  */
 const sortRows = (header, isAscending) => {
-  header.setAttribute(SORTED, isAscending === true ? DESCENDING : ASCENDING );
+  header.setAttribute(SORTED, isAscending === true ? DESCENDING : ASCENDING);
   updateSortLabel(header);
 
-  const tbody = header.closest(TABLE).querySelector('tbody');
+  const tbody = header.closest(TABLE).querySelector("tbody");
 
   // We can use Array.from() and Array.sort() instead once we drop IE11 support, likely in the summer of 2021
   //
@@ -119,22 +136,19 @@ const sortRows = (header, isAscending) => {
   // .forEach(tr => tbody.appendChild(tr) );
 
   // [].slice.call() turns array-like sets into true arrays so that we can sort them
-  const allRows = [].slice.call(tbody.querySelectorAll('tr'));
+  const allRows = [].slice.call(tbody.querySelectorAll("tr"));
   const allHeaders = [].slice.call(header.parentNode.children);
   const thisHeaderIndex = allHeaders.indexOf(header);
-  allRows.sort(
-    compareFunction(
-      thisHeaderIndex,
-      !isAscending
-    )
-  ).forEach(tr => {
-    [].slice.call(tr.children).forEach(td => td.removeAttribute("data-sort-active"));
+  allRows.sort(compareFunction(thisHeaderIndex, !isAscending)).forEach((tr) => {
+    [].slice
+      .call(tr.children)
+      .forEach((td) => td.removeAttribute("data-sort-active"));
     tr.children[thisHeaderIndex].setAttribute("data-sort-active", true);
-    tbody.appendChild(tr)
+    tbody.appendChild(tr);
   });
 
   return true;
-}
+};
 
 /**
  * Update the live region immediately following the table whenever sort changes.
@@ -143,17 +157,21 @@ const sortRows = (header, isAscending) => {
  */
 
 const updateLiveRegion = (table, sortedHeader) => {
-  const caption = table.querySelector('caption').innerText;
+  const caption = table.querySelector("caption").innerText;
   const sortedAscending = sortedHeader.getAttribute(SORTED) === ASCENDING;
   const headerLabel = sortedHeader.innerText;
   const liveRegion = table.nextElementSibling;
   if (liveRegion && liveRegion.matches(ANNOUNCEMENT_REGION)) {
-    const sortAnnouncement = `The table named "${caption}" is now sorted by ${headerLabel} in ${sortedAscending ? ASCENDING : DESCENDING } order.`;
+    const sortAnnouncement = `The table named "${caption}" is now sorted by ${headerLabel} in ${
+      sortedAscending ? ASCENDING : DESCENDING
+    } order.`;
     liveRegion.innerText = sortAnnouncement;
   } else {
-    throw new Error(`Table containing a sortable column header is not followed by an aria-live region.`)
+    throw new Error(
+      `Table containing a sortable column header is not followed by an aria-live region.`
+    );
   }
-}
+};
 
 /**
  * Toggle a header's sort state, optionally providing a target
@@ -193,12 +211,12 @@ const toggleSort = (header, isAscending) => {
 
 const createHeaderButton = (header) => {
   const buttonEl = document.createElement("button");
-  buttonEl.setAttribute('tabindex', '0');
+  buttonEl.setAttribute("tabindex", "0");
   buttonEl.classList.add(SORT_BUTTON_CLASS);
   buttonEl.innerHTML = `${ICON_SOURCE}`;
   header.appendChild(buttonEl);
   updateSortLabel(header);
-}
+};
 
 const table = behavior(
   {
@@ -207,7 +225,8 @@ const table = behavior(
         event.preventDefault();
         toggleSort(
           event.target.closest(SORTABLE_HEADER),
-          event.target.closest(SORTABLE_HEADER).getAttribute(SORTED) === ASCENDING
+          event.target.closest(SORTABLE_HEADER).getAttribute(SORTED) ===
+            ASCENDING
         );
       },
     },
@@ -217,7 +236,11 @@ const table = behavior(
       const sortableHeaders = select(SORTABLE_HEADER, root);
       sortableHeaders.forEach((header) => createHeaderButton(header));
 
-      const firstSorted = sortableHeaders.filter((header) => header.getAttribute(SORTED) === ASCENDING || header.getAttribute(SORTED) === DESCENDING)[0];
+      const firstSorted = sortableHeaders.filter(
+        (header) =>
+          header.getAttribute(SORTED) === ASCENDING ||
+          header.getAttribute(SORTED) === DESCENDING
+      )[0];
       if (typeof firstSorted === "undefined") {
         // no sortable headers found
         return;
@@ -225,15 +248,13 @@ const table = behavior(
       const sortDir = firstSorted.getAttribute(SORTED);
       if (sortDir === ASCENDING) {
         toggleSort(firstSorted, true);
-
-      }
-      else if (sortDir === DESCENDING) {
+      } else if (sortDir === DESCENDING) {
         toggleSort(firstSorted, false);
       }
     },
     TABLE,
     SORTABLE_HEADER,
-    SORT_BUTTON
+    SORT_BUTTON,
   }
 );
 
