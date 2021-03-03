12d4d9c815c9299870476b73a194546e113df099
scottqueen-bixal <scott.queen@gsa.gov>
2021-03-03 17:11:07 -0500

    getting our components before we run our a11y tests

--- spec/a11y.js
+++ spec/a11y.js
@@ -2,66 +2,6 @@ const chromeLauncher = require("chrome-launcher");
 const CDP = require("chrome-remote-interface");
 const axeTester = require("./axe-tester");
 const { getComponents } = require("./delayed-root-suite");
-// const fs = require("fs");
-// const path = require("path");
-// const ROOT_DIR = path.join(__dirname, "..");
-// const PL_BUILD_PATTERNS_DIR = path.join(ROOT_DIR, "build/patterns/");
-
-// get all rendered.html files
-const handles = [
-  "/patterns/components-usa-accordion-usa-accordion/components-usa-accordion-usa-accordion.rendered.html",
-];
-
-getComponents.then((components) => console.log({ components }));
-
-// const components = [];
-
-// fs.readdir(PL_BUILD_PATTERNS_DIR, (err, files) => {
-//   if (err) console.log(err);
-//   else {
-//     files.forEach((file) => {
-//       const builtComponentDir = PL_BUILD_PATTERNS_DIR + file;
-//       fs.readdir(builtComponentDir, (err, dir) => {
-//         if (err) console.log(err);
-//         dir.forEach((component) => {
-//           if (component.includes(".rendered.html")) {
-//             components.push("/patterns/" + file + "/" + component);
-//           }
-//         });
-//       });
-//     });
-//   }
-// });
-
-// const getComponents = new Promise((resolve, reject) => {
-//   const components = [];
-
-//   async function findAllComponents() {
-//     fs.readdir(PL_BUILD_PATTERNS_DIR, (err, files) => {
-//       if (err) console.log(err);
-//       else {
-//         files.forEach((file) => {
-//           const builtComponentDir = PL_BUILD_PATTERNS_DIR + file;
-//           fs.readdir(builtComponentDir, (err, dir) => {
-//             if (err) console.log(err);
-//             dir.forEach((component) => {
-//               if (component.includes(".rendered.html")) {
-//                 components.push("/patterns/" + file + "/" + component);
-//               }
-//             });
-//           });
-//         });
-//       }
-//     });
-//   }
-
-//   resolve(findAllComponents().then(() => {
-//     console.log(components);
-//     return components;
-//   }));
-// });
-
-// getComponents.then((value) => console.log(value));
 
 let chrome;
 let chromeHost;
@@ -77,7 +17,7 @@ async function launchChrome() {
     .then((newChrome) => {
       chrome = newChrome;
       chromeHost = chrome.host;
-      serverUrl = "http://localhost:3000";
+      serverUrl = "http://localhost:3000/";
     });
 }
 
@@ -101,30 +41,28 @@ async function loadPatternLabPreview(c, h) {
   return loadPage(c, url);
 }
 
-describe("a11y tests", function () {
-  this.timeout(20000);
+getComponents.then((handles) => {
+  describe("a11y tests", function () {
+    this.timeout(20000);
 
-  before(async () => {
-    await launchChrome();
-  });
+    before(async () => {
+      await launchChrome();
+    });
 
-  describe(`looking for violations`, () => {
-    console.log(handles);
-    // console.log(components);
-    handles.forEach((handle) => {
-      before(async () => {
-        await createChromeDevtoolsProtocol().then((client) => {
-          cdp = client;
-          return loadPatternLabPreview(cdp, handle);
+    describe(`looking for violations`, () => {
+      handles.forEach((handle) => {
+        before(async () => {
+          await createChromeDevtoolsProtocol().then((client) => {
+            cdp = client;
+            return loadPatternLabPreview(cdp, handle);
+          });
+          axeTester.load(cdp);
         });
-        axeTester.load(cdp);
+        after("shutdown chrome devtools protocol", () => cdp.close());
+        // test cases
+        it(`${handle} passes without a11y errors`, () =>
+          axeTester.run({ cdp, warn: true }));
       });
-      after("shutdown chrome devtools protocol", () => cdp.close());
-      // test cases
-      it(`${handle} passes without a11y errors`, () =>
-        axeTester.run({ cdp, warn: false }));
     });
   });
 });
-// });
-// }
