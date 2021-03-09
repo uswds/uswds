0e237c359fda4726e97fa126feee914f5eab7cbd
James Mejia <james.mejia@gsa.gov>
2021-03-09 12:40:41 -0600

    Return images stream instead of using callback.

--- config/gulp/images.js
+++ config/gulp/images.js
@@ -3,10 +3,8 @@ const dutil = require("./doc-util");
 
 const task = "images";
 
-gulp.task(task, (done) => {
+gulp.task(task, () => {
   dutil.logMessage(task, "Copying Images");
-  const stream = gulp.src("src/img/**/*").pipe(gulp.dest("dist/img"));
 
-  done();
-  return stream;
+  return gulp.src("src/img/**/*").pipe(gulp.dest("dist/img"));
 });
