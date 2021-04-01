5084d94b25f4b9faaa2e7dd207133f0d0dc32ba7
scottqueen-bixal <scott.queen@gsa.gov>
2021-04-01 13:22:38 -0400

    update composition for build export

---
+++ gulp-tasks/build.js
@@ -0,0 +1,46 @@
+const { src, dest, series, parallel } = require("gulp");
+const del = require("del");
+const dutil = require("./utils/doc-util");
+const cFlags = require("./utils/cflags");
+const { svgSprite } = require("./svg-sprite");
+const { copyDistSass, sass } = require("./sass");
+const { compileJavascript } = require("./javascript");
+const { fonts } = require("./fonts");
+const { images } = require("./images");
+
+function cleanDist(done) {
+  if (!cFlags.cleanup) {
+    dutil.logMessage(
+      "clean-dist",
+      "Skipping cleaning up the distribution directories."
+    );
+    return done();
+  }
+  dutil.logMessage("clean-dist", "Removing distribution directories.");
+
+  return del("dist");
+}
+
+function docs(done) {
+  dutil.logMessage("docs", "Copying documentation dist dir");
+
+  const stream = src(["README.md", "LICENSE.md", "CONTRIBUTING.md"]).pipe(
+    dest("dist")
+  );
+
+  done();
+  return stream;
+}
+
+exports.build = series(
+  (done) => {
+    dutil.logIntroduction();
+    dutil.logMessage("build", "Creating distribution directories.");
+    done();
+  },
+  cleanDist,
+  svgSprite,
+  docs,
+  parallel(sass, compileJavascript, images, fonts),
+  copyDistSass
+);
