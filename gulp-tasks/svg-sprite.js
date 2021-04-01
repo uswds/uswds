d19d4ad602d34dd0ee35ff11e1b57df1966d3c0a
scottqueen-bixal <scott.queen@gsa.gov>
2021-04-01 13:49:35 -0400

    no need for all that path building

--- gulp-tasks/svg-sprite.js
+++ gulp-tasks/svg-sprite.js
@@ -1,14 +1,8 @@
 /* eslint-disable arrow-body-style */
-const path = require("path");
-
-const ROOT_DIR = path.join(__dirname, "../");
-
-const { src, dest, series } = require("gulp");
-const del = require("del");
+const { src, dest } = require("gulp");
 const svgSprite = require("gulp-svg-sprite");
-const rename = require("gulp-rename");
 
-const svgPath = path.join(ROOT_DIR, "src/img");
+const svgPath = "../src/img";
 
 // More complex configuration example
 const config = {
@@ -31,23 +25,13 @@ const config = {
   },
 };
 
-function buildSprite(done) {
-  return src(`${svgPath}/usa-icons/**/*.svg`)
-    .pipe(svgSprite(config))
-    // eslint-disable-next-line no-console
-    .on("error", (error) => console.error("There was an error", error))
-    .pipe(dest(`${svgPath}`))
-    .on("end", () => done())
-};
-
-function renameSprite() {
-  return src(`${svgPath}/symbol/svg/sprite.symbol.svg`)
-    .pipe(rename(`${svgPath}/sprite.svg`))
-    .pipe(dest(`./`));
-};
-
-function cleanSprite() {
-  return del(`${svgPath}/symbol`)
-};
-
-exports.svgSprite = series(buildSprite, renameSprite, cleanSprite)
+module.exports = {
+  buildSprite(done) {
+    return src(`${svgPath}/usa-icons/**/*.svg`)
+      .pipe(svgSprite(config))
+      // eslint-disable-next-line no-console
+      .on("error", (error) => console.error("There was an error", error))
+      .pipe(dest(`${svgPath}`))
+      .on("end", () => done())
+  }
+}
