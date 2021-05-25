529977540fa2b3fe82359037492e67e145e161ea
James Mejia <james.mejia@gsa.gov>
2021-05-25 11:57:51 -0500

    Remove unused clean tasks.

--- gulp-tasks/clean.js
+++ gulp-tasks/clean.js
@@ -7,45 +7,16 @@ const del = require('del');
 const cFlags = require("./utils/cflags");
 const dutil = require("./utils/doc-util");
 
-// Export our tasks.
-module.exports = {
-
-  // Clean CSS files.
-  cleanCSS() {
-    return del(['./dist/css/*'], { force: true });
-  },
-
-  // Clean Fonts files.
-  cleanFonts() {
-    return del(['./dist/fonts/*'], { force: true });
-  },
-
-  // Clean Images files.
-  cleanImages() {
-    return del(['./dist/img/*'], { force: true });
-  },
-
-  // Clean JS files.
-  cleanJS() {
-    return del(['./dist/js/*'], { force: true });
-  },
-
-   // Clean Sass files.
-  cleanSass() {
-    return del(['./dist/scss/*'], { force: true });
-  },
-
-  // Clean generated Dist directory.
-  cleanDist(done) {
-    if (!cFlags.cleanup) {
-      dutil.logMessage(
-        "clean-dist",
-        "Skipping cleaning up the distribution directories."
-      );
-      return done();
-    }
-    dutil.logMessage("clean-dist", "Removing distribution directories.");
-
-    return del("dist");
+// Clean generated Dist directory.
+exports.cleanDist = done => {
+  if (!cFlags.cleanup) {
+    dutil.logMessage(
+      "clean-dist",
+      "Skipping cleaning up the distribution directories."
+    );
+    return done();
   }
+  dutil.logMessage("clean-dist", "Removing distribution directories.");
+
+  return del("dist");
 };
