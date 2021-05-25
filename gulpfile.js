ff9d27bfbd70994cf73a0a96a01170b03a092998
James Mejia <james.mejia@gsa.gov>
2021-05-25 13:11:51 -0500

    Add comments; remove unused imports.

--- gulpfile.js
+++ gulpfile.js
@@ -15,39 +15,39 @@ const { release } = require("./gulp-tasks/release");
 const { watch } = require("./gulp-tasks/watch");
 const { serve, buildPL, exitServer } = require("./gulp-tasks/serve");
 const { compileSass } = require("./gulp-tasks/sass");
-const { copyVendor, copySass, copyImages } = require("./gulp-tasks/copy");
+const { copyVendor } = require("./gulp-tasks/copy");
 const { cleanDist } = require("./gulp-tasks/clean");
 
 /**
- * Declare our exports
+ * *Flags*
  */
-
-// flags
 exports.noTest = noTest;
 exports.noCleanup = noCleanup;
 
-// Clean all directories.
+/**
+ * *Clean tasks*
+ */
 exports.cleanDist = cleanDist;
 
-// Lint Sass
+/**
+ * *Lint tasks*
+ */
 exports.lintSass = lintSass;
-
-// Lint JavaScript
 exports.lintJS = lintJS;
-
-// Lint Sass and JavaScript
 exports.lint = parallel(lintSass, lintJS);
 
-// tests
+/**
+ * *Test tasks*
+ * a11y: Accessibility tests; starts server, runs axe tests, and closes server.
+ * cover: Similar to unit tests.
+ * sassTests: Sass unit tests.
+ * unitTests: Component unit tests.
+ * test: Run all tests.
+ */
 exports.a11y = series(serve, a11y, exitServer);
-
 exports.cover = cover;
-
 exports.sassTests = sassTests;
-
 exports.unitTests = unitTests;
-
-// run all our required tests
 exports.test = series(
   typeCheck,
   lintJS,
@@ -59,16 +59,26 @@ exports.test = series(
   exitServer
 );
 
-// building
+/**
+ * *Build tasks*
+ * buildSprite: Generate new spritesheet based on SVGs in `src/img/usa-icons/`.
+ * sass: Lint, copy normalize, and compile sass.
+ * buildJS: Lint, copy normalize, and compile sass.
+ * release: Builds USWDS and returns a zip with sha256 hash and filesize.
+ */
 exports.buildSprite = buildSprite;
 exports.sass = series(lintSass, copyVendor, compileSass);
-exports.buildJS = compileJS;
+exports.buildJS = series(typeCheck, lintJS, compileJS);
 exports.release = release;
 
 // Build task for Pattern Lab.
 exports.styleguide = buildPL;
 
-// Watch task that runs a browsersync server.
+/**
+ * *Watch task*
+ * Builds USWDS and component library, creates local server, and watches
+ * for changes in scss, js, twig, yml, and unit tests.
+ */
 exports.watch = watch;
 
 // Default Task
