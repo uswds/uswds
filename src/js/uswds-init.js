4cdf6eb908d2a61682069d86d631084037347daa
Andrew Duthie <andrew.duthie@gsa.gov>
2021-06-16 11:05:21 -0400

    Use load event for verifying USWDS loaded

--- src/js/uswds-init.js
+++ src/js/uswds-init.js
@@ -1,35 +1,18 @@
-const docElem = window.document.documentElement;
-const loadingClass = "usa-js-loading";
-
-function addLoadingClass() {
-  docElem.className += ` ${loadingClass}`;
-}
-
-function revertClass() {
-  docElem.className = docElem.className.replace(loadingClass, "");
-}
-
-// Based on https://www.filamentgroup.com/lab/enhancing-optimistically.html
-if ("querySelector" in window.document && "addEventListener" in window) {
-  addLoadingClass();
+(() => {
+  document.documentElement.classList.add("usa-js-loading");
+  function revertClass() {
+    document.documentElement.classList.remove("usa-js-loading");
+  }
 
   const fallback = setTimeout(revertClass, 8000);
-  let timeout = 100;
 
-  const poll = () => {
-    setTimeout(() => {
-      timeout += 1;
-      if (typeof uswdsPresent !== "undefined") {
-        // USWDS library loaded
-        clearTimeout(fallback);
-        setTimeout(revertClass, 100);
-      } else if (timeout > 0) {
-        poll();
-      } else {
-        // USWDS library failed to load
-      }
-    }, 100);
-  };
+  function verifyLoaded() {
+    if (window.uswdsPresent) {
+      clearTimeout(fallback);
+      revertClass();
+      document.removeEventListener("load", verifyLoaded, true);
+    }
+  }
 
-  poll();
-}
+  document.addEventListener("load", verifyLoaded, true);
+})();
