1a04a19717043254e85a83038babd27d1be5104e
scottqueen-bixal <scott.queen@gsa.gov>
2021-08-20 22:38:20 -0400

    updated component

--- src/stories/usa-button.ts
+++ src/stories/usa-button.ts
@@ -2,59 +2,57 @@ import { LitElement, html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';
 import { styleMap } from 'lit/directives/style-map.js';
-// import Styles from './_button.scss'
-import Styles from '../stylesheets/components/_usa-button.scss'
+import { handleClasses } from './utils';
+import ButtonStyles from '../stylesheets/components/_usa-button.scss'
 
 @customElement('usa-button')
 export class ButtonComponent extends LitElement {
     /**
    * Is this the principal call to action on the page?
    */
-  @property({type: String}) mode?: 'secondary'
+  @property({type: String})
+  mode?: 'secondary'
   /**
    * How large should the button be?
    */
-  @property({type: String}) size?: 'big'
+  @property({type: String})
+  size?: 'big'
   /**
    * Which direction in hue should the button be?
    */
-  @property({type: String}) accent?: 'cool' | 'warm'
+  @property({type: String})
+  accent?: 'cool' | 'warm'
   /**
    * How should the button be presented?
    */
-  @property({type: Array}) variant?:
-    [
-      'base'
+  @property({type: Array})
+  variant?: (
+    'base'
       | 'outline'
       | 'inverse'
-      | 'unstyled'
-    ]
+      | 'unstyled')[]
   /**
    * Button contents
    */
-  @property({type: String}) label?: string
+  @property({type: String})
+  label?: string
   /**
    * Optional click handler
    */
-  @property() onClick?: () => void
+  @property()
+  onClick?: () => void
 
   static get styles() {
-		return [Styles];
+		return [ButtonStyles];
   }
 
-  render() { // Defines a template to be "rendered" as part of the component.
-    const buttonMode = this.mode ? `usa-button--${this.mode}` : null
-    const buttonSize = this.size ? `usa-button--${this.size}` : null
-    const buttonAccent = this.accent ? `usa-button--${this.accent}` : null
-    const buttonVariant = this.variant
-      ? this.variant.map((v) => `usa-button--${v}`).join(' ') : null
-
-    const handleButtonVariants = () => {
-      const VariantList = [buttonMode, buttonSize, buttonAccent, buttonVariant]
-      const ClassList:string = VariantList.indexOf(null) === -1 ? 'usa-button'
-        : ['usa-button', VariantList.filter((v) => v !== null).join(' ')].join(' ')
-      return ClassList
-    }
+  render() {
+    const classList = handleClasses([
+      this.mode ? `usa-button--${this.mode}` : null,
+      this.size ? `usa-button--${this.size}` : null,
+      this.accent ? `usa-button--${this.accent}` : null,
+      this.variant ? this.variant.map((v) => `usa-button--${v}`).join(' ') : null
+    ], 'usa-button')
 
     const includeWrapper = this.variant?.includes('outline')
       && this.variant?.includes('inverse') ? true : false
@@ -68,31 +66,34 @@ export class ButtonComponent extends LitElement {
       'max-width': 'fit-content'
     }
 
+    const buttonBase = () => html`
+      <button
+        type="button"
+        class=${classList}
+        @click=${this.onClick}
+      >
+        ${this.label}
+      </button>`
+
     return html`
       ${ includeWrapper
           ? html`
           <div
             class=${classMap(wrapperClasses)}
-            .style=${includeWrapper && styleMap(wrapperStyles)}>
-              <button
-              type="button"
-              class=${handleButtonVariants()}
-              @click=${this.onClick}
+            style=${styleMap(wrapperStyles)}
             >
-              ${this.label}
-            </button>
+            ${buttonBase()}
           </div>`
-          : html`
-          <button
-            type="button"
-            class=${handleButtonVariants()}
-            @click=${this.onClick}
-          >
-            ${this.label}
-          </button>`
+          : buttonBase()
         }
       `
   }
 }
 
 export default ButtonComponent
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "usa-button": ButtonComponent,
+  }
+}
