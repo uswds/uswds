452e2bd2c24300b4124b01814efbc9a1f563c73e
Niels Swimberghe <nielsswimberghe@gmail.com>
2021-09-07 17:06:53 -0400

    Add specs to test date range picker initialized using document.body or date range picker

--- spec/unit/date-range-picker/invalid-template-one-input.spec.js
+++ spec/unit/date-range-picker/invalid-template-one-input.spec.js
@@ -8,24 +8,32 @@ const INVALID_TEMPLATE_ONE_INPUT = fs.readFileSync(
   path.join(__dirname, "/invalid-template-one-input.template.html")
 );
 
-describe("Date range picker without second date picker", () => {
-  const { body } = document;
+const dateRangePickerSelector = () => document.querySelector('.usa-date-range-picker');
+const tests = [
+  { name: "document.body", selector: () => document.body },
+  { name: "date range picker", selector: dateRangePickerSelector }
+];
 
-  beforeEach(() => {
-    body.innerHTML = INVALID_TEMPLATE_ONE_INPUT;
-    DatePicker.on();
-  });
+tests.forEach(({name, selector: containerSelector}) => {
+  describe(`Date range picker without second date picker initialized at ${name}`, () => {
+    const { body } = document;
 
-  afterEach(() => {
-    body.textContent = "";
-    DatePicker.off(body);
-    DateRangePicker.off(body);
-  });
+    beforeEach(() => {
+      body.innerHTML = INVALID_TEMPLATE_ONE_INPUT;
+      DatePicker.on(containerSelector());
+    });
 
-  it('should throw an error when a date range picker without two "usa-date-picker" elements', () => {
-    assert.throws(() => DateRangePicker.on(), {
-      message:
-        ".usa-date-range-picker is missing second '.usa-date-picker' element"
+    afterEach(() => {
+      DatePicker.off(containerSelector());
+      DateRangePicker.off(containerSelector());
+      body.textContent = "";
+    });
+
+    it('should throw an error when a date range picker without two "usa-date-picker" elements', () => {
+      assert.throws(() => DateRangePicker.on(containerSelector()), {
+        message:
+          ".usa-date-range-picker is missing second '.usa-date-picker' element"
+      });
     });
   });
 });
