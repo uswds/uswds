66fd8c99045c3c1e06a3fef11546fa2f07658e75
James Mejia <james.mejia@gsa.gov>
2021-09-15 10:01:55 -0500

    Create navigation subcomponent.
    - Move menu macro to it's own file (can't call itself in same file)
    - Create primary navigation
    - Add megamenu variant

---
+++ src/components/usa-nav/usa-nav__primary/usa-nav-primary.stories.js
@@ -0,0 +1,24 @@
+import Component from "./usa-nav__primary.twig";
+import Data from "./usa-nav__primary.yml";
+import CloseIcon from "../../../img/usa-icons/close.svg"
+
+export default {
+  title: "Components/Navigation/Primary",
+  args: {
+    CloseIcon,
+  }
+};
+
+const Template = (args) => Component(args);
+
+export const Default = Template.bind({});
+Default.args = Data;
+
+export const Megamenu = Template.bind({});
+Megamenu.args = {
+  nav: {
+  ...Data.nav,
+    megamenu: true
+  }
+};
+
