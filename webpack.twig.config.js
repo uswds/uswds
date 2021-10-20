5cfa69237d117908f69e7eff9e2797f6b515e031
scottqueen-bixal <scott.queen@gsa.gov>
2021-10-20 15:52:16 -0400

    get all twig files, git all json files

--- webpack.twig.config.js
+++ webpack.twig.config.js
@@ -6,19 +6,49 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 // const twigFiles = [];
 // const componentsDir = path.join(__dirname, './src/components/')
 
-// fs.readdir(componentsDir, (_err, dir) =>
-//     dir.forEach( dirName => {
+// const twigFiles = fs.readdir(componentsDir, (_err, dir) =>
+//     dir.map( dirName => {
+//       console.log(dirName)
 //       const componentsFiles = `${componentsDir}${dirName}/`
-//       twigFiles.push(fs.readdir(
-//           componentsFiles, (_err2, files) => files.filter((e)=> console.log(path.extname(e).toLowerCase() === ext))
+//       return fs.readdir(
+//           componentsFiles, (_err2, files) => files.map(file => path.extname(file).toLowerCase() === ext
+//           ? file
+//           : null
 //         )
 //       )
 //     }
 //   )
 // );
-// // path.extname(e).toLowerCase() === ext
 
-// console.log(twigFiles)
+// filter out anything that starts with an underscore or is not a twig file
+function walk(dir, ext) {
+  console.log(`${ext}`)
+  let results = [];
+  const list = fs.readdirSync(dir);
+  list.forEach(file => {
+    // eslint-disable-next-line no-param-reassign
+    file = `${dir}/${file}`;
+    const stat = fs.statSync(file);
+    if (stat && stat.isDirectory() && path.basename(file).indexOf('_') !== 0) {
+      /* Recurse into a subdirectory */
+      results = results.concat(walk(file, ext));
+    } else if (
+      stat &&
+      !stat.isDirectory() &&
+      path.extname(file) === `${ext}` &&
+      path.basename(file).indexOf('_') !== 0
+    ) {
+      /* Is a file */
+      results.push(file);
+    }
+  });
+  return results;
+}
+
+const files = walk('./src/components', '.twig');
+const data = walk('./src/components', '.json');
+
+console.log(files, data)
 
 // TODO: for each data set, create an html file based on its closest relative twig template
 
