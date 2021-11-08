4e5a314e8275630426788f2fb6fda9119d69ca57
James Mejia <james.mejia@gsa.gov>
2021-11-08 15:51:58 -0600

    Fix modifiers in extended Headers.

--- src/components/usa-header/header.stories.js
+++ src/components/usa-header/header.stories.js
@@ -1,11 +1,11 @@
 import "../../stylesheets/uswds.scss";
 import Component from "./usa-header.twig";
 import ComponentExtended from "./usa-header--extended/usa-header--extended.twig";
-import usaHeaderData from "./usa-header.json";
-import usaHeaderMegamenuData from "./usa-header~megamenu.json";
-import usaHeaderExtendedData from "./usa-header--extended/usa-header--extended.json";
-import usaHeaderExtendedMegamenuData from "./usa-header--extended/usa-header--extended~megamenu.json";
-import {usaNavSecondaryData} from "../usa-nav/usa-nav__secondary/usa-nav__secondary.json";
+import headerData from "./usa-header.json";
+import headerMegamenuData from "./usa-header~megamenu.json";
+import headerExtendedData from "./usa-header--extended/usa-header--extended.json";
+import headerExtendedMegamenuData from "./usa-header--extended/usa-header--extended~megamenu.json";
+import navSecondaryData from "../usa-nav/usa-nav__secondary/usa-nav__secondary.json";
 import { SmallContent as SmallSearchContent } from "../usa-search/content";
 
 export default {
@@ -23,19 +23,19 @@ const Template = (args) => Component(args);
 const ExtendedTemplate = (args) => ComponentExtended(args);
 
 export const Default = Template.bind({});
-Default.args = usaHeaderData;
+Default.args = headerData;
 
 export const Megamenu = Template.bind({});
-Megamenu.args = usaHeaderMegamenuData;
+Megamenu.args = headerMegamenuData;
 
 export const Extended = ExtendedTemplate.bind({});
 Extended.args = {
-  ...usaHeaderExtendedData,
-  usaNavSecondaryData,
+  ...headerExtendedData,
+  navSecondaryData,
 };
 
 export const ExtendedMegamenu = ExtendedTemplate.bind({});
 ExtendedMegamenu.args = {
-  ...usaHeaderExtendedMegamenuData,
-  usaNavSecondaryData,
+  ...headerExtendedMegamenuData,
+  navSecondaryData,
 };
