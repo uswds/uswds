ae2ec8d95c1fea8d1c2be5264415392edf534daa
James Mejia <james.mejia@gsa.gov>
2021-11-09 12:30:17 -0600

    Override search setting in story, instead of template.

--- src/components/usa-header/header.stories.js
+++ src/components/usa-header/header.stories.js
@@ -4,7 +4,7 @@ import ComponentExtended from "./usa-header--extended/usa-header--extended.twig"
 import headerData from "./usa-header.json";
 import headerMegamenuData from "./usa-header~megamenu.json";
 import headerExtendedData from "./usa-header--extended/usa-header--extended.json";
-import headerExtendedMegamenuData from "./usa-header--extended/usa-header--extended~megamenu.json";
+import headerExtendedMegamenuData from "./usa-header--extended/usa-header--extended-megamenu.json";
 import navSecondaryData from "../usa-nav/usa-nav__secondary/usa-nav__secondary.json";
 import { SmallContent as SmallSearchContent } from "../usa-search/content";
 
@@ -31,11 +31,17 @@ Megamenu.args = headerMegamenuData;
 export const Extended = ExtendedTemplate.bind({});
 Extended.args = {
   ...headerExtendedData,
-  navSecondaryData,
+  navSecondaryData: {
+    ...navSecondaryData,
+    search: true
+  },
 };
 
 export const ExtendedMegamenu = ExtendedTemplate.bind({});
 ExtendedMegamenu.args = {
   ...headerExtendedMegamenuData,
-  navSecondaryData,
+  navSecondaryData: {
+    ...navSecondaryData,
+    search: true
+  },
 };
