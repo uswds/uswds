3acfdf9db00c9cffb8e6b44aeda0bd52692eb18a
Charlie Mahoney <charlie.mahoney@bixal.com>
2022-01-13 14:01:27 -0500

    Added file-input component

---
+++ src/components/usa-file-input/usa-file-input.stories.js
@@ -0,0 +1,39 @@
+import "../../stylesheets/uswds.scss";
+import Component from "./usa-file-input.twig";
+import {
+  Data,
+  DisabledData,
+  ErrorData,
+  MultipleData,
+  SpecificData,
+  WildcardData,
+} from "./content";
+
+export default {
+  title: "Components/File Input",
+  argTypes: {
+    label: { control: "text" },
+    hint: { control: "text" },
+    error_message: { control: "text" },
+  },
+};
+
+const Template = (args) => Component(args);
+
+export const Default = Template.bind({});
+Data.args = Data;
+
+export const Disabled = Template.bind({});
+Disabled.args = DisabledData;
+
+export const Error = Template.bind({});
+Error.args = ErrorData;
+
+export const Multiple = Template.bind({});
+Multiple.args = MultipleData;
+
+export const Specific = Template.bind({});
+Specific.args = SpecificData;
+
+export const Wildcard = Template.bind({});
+Wildcard.args = WildcardData;
