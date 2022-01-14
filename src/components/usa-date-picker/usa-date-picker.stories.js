e6cfdcc716b815fbe2793891b8acb9baca88b1db
Charlie Mahoney <charlie.mahoney@bixal.com>
2022-01-14 16:46:59 -0500

    Added Date Picker component variants to storybook

--- src/components/usa-date-picker/usa-date-picker.stories.js
+++ src/components/usa-date-picker/usa-date-picker.stories.js
@@ -27,3 +27,29 @@ const Template = (args) => Component(args);
 
 export const DatePicker = Template.bind({});
 DatePicker.args = Data;
+
+export const DefaultDate = Template.bind({});
+DefaultDate.args = {
+  defaultDate: "1995-03-07",
+};
+
+export const DefaultValue = Template.bind({});
+DefaultValue.args = {
+  defaultValue: "1995-03-07",
+};
+
+export const RestrictedDate = Template.bind({});
+RestrictedDate.args = {
+  minDate: "2022-01-07",
+  maxDate: "2022-01-14",
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+};
+
+export const RangeDate = Template.bind({});
+RangeDate.args = {
+  rangeDate: "2022-01-07",
+};
