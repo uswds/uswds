10794fdb395d9c203a38f67b798730e1866ffd5a
Charlie Mahoney <charlie.mahoney@bixal.com>
2022-01-18 14:06:51 -0500

    Added base Combo Box component to storybook

---
+++ src/components/usa-combo-box/usa-combo-box.stories.js
@@ -0,0 +1,17 @@
+import "../../stylesheets/uswds.scss";
+import Component from "./usa-combo-box.twig";
+
+import Data from "./usa-combo-box.json";
+
+export default {
+  title: "Components/Combo Box",
+  argTypes: {
+    label: { control: "text" },
+    default_value: { control: "text" },
+  },
+};
+
+const Template = (args) => Component(args);
+
+export const ComboBox = Template.bind({});
+ComboBox.args = Data;
