813cd144502d4863414fea33e611dc0b641fc68e
Charlie Mahoney <charlie.mahoney@bixal.com>
2022-01-18 15:42:27 -0500

    Added Pagintion component to storybook

---
+++ src/components/usa-pagination/content/index.js
@@ -0,0 +1,4 @@
+export { default as Data } from "./usa-pagination.json";
+export { default as UnboundedData } from "./usa-pagination~unbounded.json";
+export { default as EsData } from "./usa-pagination~lang-es.json";
+export { default as EsUnboundedData } from "./usa-pagination~unbounded-lang-es.json";
