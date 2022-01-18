8fbe65c89cb6ac905ee443b3d9c08e2c9470189e
Charlie Mahoney <charlie.mahoney@bixal.com>
2022-01-18 15:02:47 -0500

    Added Modal component to storybook

---
+++ src/components/usa-modal/usa-modal.stories.js
@@ -0,0 +1,20 @@
+import "../../stylesheets/uswds.scss";
+import Component from "./usa-modal.twig";
+import Data from "./usa-modal.json";
+import ForcedActionData from "./usa-modal~forced-action.json";
+import LargeData from "./usa-modal~large.json";
+
+export default {
+  title: "Components/Modal",
+};
+
+const Template = (args) => Component(args);
+
+export const Default = Template.bind({});
+Default.args = Data;
+
+export const ForcedAction = Template.bind({});
+ForcedAction.args = ForcedActionData;
+
+export const Large = Template.bind({});
+Large.args = LargeData;
