1cc1c51b15879a4b86fc97c5cb5e546c0459a721
Charlie Mahoney <charlie.mahoney@bixal.com>
2022-01-18 13:45:54 -0500

    Added Card component and variants to storybook

---
+++ src/components/usa-card/usa-card.stories.js
@@ -0,0 +1,25 @@
+import "../../stylesheets/uswds.scss";
+import Component from "./usa-card.twig";
+
+import Data from "./usa-card.json";
+import FlagData from "./usa-card~flag.json";
+import StandardData from "./usa-card~standard.json";
+import MediaData from "./usa-card~media.json";
+
+export default {
+  title: "Components/Card",
+};
+
+const Template = (args) => Component(args);
+
+export const Default = Template.bind({});
+Default.args = Data;
+
+export const Flag = Template.bind({});
+Flag.args = FlagData;
+
+export const Standard = Template.bind({});
+Standard.args = StandardData;
+
+export const Media = Template.bind({});
+Media.args = MediaData;
