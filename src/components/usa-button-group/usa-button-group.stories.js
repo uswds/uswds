7581625e3f812a664f6b6009a9ed6dd72bd77ea7
Charlie Mahoney <charlie.mahoney@bixal.com>
2022-01-27 13:59:19 -0500

    Removed unused text control from button-group

--- src/components/usa-button-group/usa-button-group.stories.js
+++ src/components/usa-button-group/usa-button-group.stories.js
@@ -5,11 +5,6 @@ import SegmentedData from "./usa-button-group~segmented.json";
 
 export default {
   title: "Components/Button Group",
-  argTypes: {
-    text: {
-      control: "text",
-    },
-  },
 };
 
 const Template = (args) => Component(args);
