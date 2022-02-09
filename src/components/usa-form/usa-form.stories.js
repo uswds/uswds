0bdc4cef4de53af96529545627d183abb4eaf8ce
Amy Leadem <amy.leadem@bixal.com>
2022-02-09 11:54:14 -0700

    add stories for forms

---
+++ src/components/usa-form/usa-form.stories.js
@@ -0,0 +1,16 @@
+import FormAddress from "../../templates/usa-address-form.twig";
+import FormName from "../../templates/usa-name-form.twig";
+import FormPassword from "../../templates/usa-password-reset-form.twig";
+
+
+export default {
+  title: "Components/Forms",
+};
+
+const AddressTemplate = (args) => FormAddress (args);
+const NameTemplate = (args) => FormName(args);
+const PasswordTemplate = (args) => FormPassword(args);
+
+export const MailingAddress = AddressTemplate.bind({});
+export const Name = NameTemplate.bind({});
+export const ResetPassword = PasswordTemplate.bind({});
