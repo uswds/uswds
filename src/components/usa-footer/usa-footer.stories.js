18e44f7fda6aec8fb2423383f743522aca53099a
Amy Leadem <amy.leadem@bixal.com>
2022-02-09 12:22:52 -0700

    add footer stories

---
+++ src/components/usa-footer/usa-footer.stories.js
@@ -0,0 +1,26 @@
+import Medium from "./usa-footer.twig";
+import Big from "./usa-footer--big.twig";
+import Slim from "./usa-footer--slim.twig";
+
+import { 
+  Data, 
+  BigData,
+  SlimData
+} from "./content";
+
+export default {
+  title: "Components/Footer",
+};
+
+const Template = (args) => Medium(args);
+const BigTemplate = (args) => Big(args);
+const SlimTemplate = (args) => Slim(args);
+
+export const BigFooter = BigTemplate.bind({});
+BigFooter.args = BigData;
+
+export const MediumFooter = Template.bind({});
+MediumFooter.args = Data;
+
+export const SlimFooter = SlimTemplate.bind({});
+SlimFooter.args = SlimData;
