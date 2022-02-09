ec73e67ae5b93b04260bde1662953fb9dcdc78f1
Amy Leadem <amy.leadem@bixal.com>
2022-02-09 09:35:25 -0700

    setup files for identifier component

---
+++ src/components/usa-identifier/content/index.js
@@ -0,0 +1,8 @@
+export { default as Data } from "./usa-identifier.json";
+export { default as EsData } from "./usa-identifier~lang-es.json";
+export { default as MultipleLogosData } from "./usa-identifier~multiple-logos.json";
+export { default as EsMultipleLogosData } from "./usa-identifier~multiple-logos-lang-es.json";
+export { default as NoLogosData } from "./usa-identifier~no-logos.json";
+export { default as EsNoLogosData } from "./usa-identifier~no-logos-lang-es.json";
+export { default as TaxpayerData } from "./usa-identifier~taxpayer-disclaimer.json";
+export { default as EsTaxpayerData } from "./usa-identifier~taxpayer-disclaimer-lang-es.json";
