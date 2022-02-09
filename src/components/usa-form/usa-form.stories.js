c9338dcafcfdedc9df3d7ae3d68c30b1a4cfa979
Amy Leadem <amy.leadem@bixal.com>
2022-02-09 14:55:22 -0700

    Simplify sign-on form template to act as component

--- src/components/usa-form/usa-form.stories.js
+++ src/components/usa-form/usa-form.stories.js
@@ -1,10 +1,9 @@
 import FormAddress from "../../templates/usa-address-form.twig";
 import FormName from "../../templates/usa-name-form.twig";
 import FormPassword from "../../templates/usa-password-reset-form.twig";
-import FormSignIn from "../../templates/usa-sign-in/includes/_usa-sign-in-inner.twig";
+import FormSignIn from "../../templates/usa-sign-in/usa-sign-in-form/usa-sign-in-form.twig";
 import DataSignIn from "../../templates/usa-sign-in/usa-sign-in.json";
-import FormSignInMultiple from "../../templates/usa-sign-in/includes/_usa-sign-in-multiple-inner.twig";
-import DataSignInMultiple from "../../templates/usa-sign-in/usa-sign-in--multiple/usa-sign-in--multiple.json";
+
 
 export default {
   title: "Components/Forms",
@@ -14,8 +13,6 @@ const AddressTemplate = (args) => FormAddress(args);
 const NameTemplate = (args) => FormName(args);
 const PasswordTemplate = (args) => FormPassword(args);
 const SignInTemplate = (args) => FormSignIn(args);
-const SignInMultipleTemplate = (args) => FormSignInMultiple(args);
-
 
 export const MailingAddress = AddressTemplate.bind({});
 export const Name = NameTemplate.bind({});
@@ -24,5 +21,3 @@ export const ResetPassword = PasswordTemplate.bind({});
 export const SignIn = SignInTemplate.bind({});
 SignIn.args = DataSignIn;
 
-export const SignInMultiple = SignInMultipleTemplate.bind({});
-SignInMultiple.args = DataSignInMultiple;
