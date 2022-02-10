67d8690cfc38903e01a7b341fbb30edfbab1d021
Amy Leadem <amy.leadem@bixal.com>
2022-02-10 09:25:08 -0700

    set up story for templates/documentation

---
+++ src/templates/usa-documentation/usa-docs.stories.js
@@ -0,0 +1,10 @@
+import Component from "./usa-docs.twig";
+
+export default {
+  title: "Templates/Documentation Page",
+};
+
+const Template = (args) => Component(args);
+
+export const DocumentationPage = Template.bind({});
+
