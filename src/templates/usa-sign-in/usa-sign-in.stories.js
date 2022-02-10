930edcb5882c95075d8cde9ca447fead5c2fe3a6
Amy Leadem <amy.leadem@bixal.com>
2022-02-10 16:18:23 -0700

    restructure signin templates

--- src/templates/usa-sign-in/usa-sign-in.stories.js
+++ src/templates/usa-sign-in/usa-sign-in.stories.js
@@ -1,11 +1,17 @@
 import SignInForm from "./usa-sign-in.twig";
+import SignInMultipleForm from "./usa-sign-in--multiple.twig";
 
 import { DefaultContent as BannerData } from "../../components/usa-banner/content";
 import { headerData as HeaderData } from "../../components/usa-header/content";
 import { LevelTwoData as SidenavData } from "../../components/usa-sidenav/content";
 import { Data as IdentifierData } from "../../components/usa-identifier/content";
 import { Data as FooterData } from "../../components/usa-footer/content";
-import { SignInData, EsSignInData } from "./content";
+import { 
+  SignInData, 
+  EsSignInData,
+  SignInMultipleData, 
+  EsSignInMultipleData,
+} from "./content";
 
 export default {
   title: "Pages/Sign In",
@@ -31,4 +37,21 @@ export const SignInPageSpanish = () => SignInForm({
   ...FooterData, 
   ...EsSignInData,
 });
-  
+
+export const MultipleSignInPage = () => SignInMultipleForm({ 
+  ...BannerData, 
+  ...HeaderData, 
+  ...SidenavData, 
+  ...IdentifierData, 
+  ...FooterData, 
+  ...SignInMultipleData,
+});
+
+export const MultipleSignInPageSpanish = () => SignInMultipleForm({ 
+  ...BannerData, 
+  ...HeaderData, 
+  ...SidenavData, 
+  ...IdentifierData, 
+  ...FooterData, 
+  ...EsSignInMultipleData,
+});
