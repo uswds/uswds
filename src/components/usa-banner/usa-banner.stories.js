a2873f8b3ec24b4ec2f1056167e8125343ec2056
gscottqueen <scott.queen@bixal.com>
2022-02-10 07:31:01 -0500

    importing common module breaks

--- src/components/usa-banner/usa-banner.stories.js
+++ src/components/usa-banner/usa-banner.stories.js
@@ -1,11 +1,15 @@
+// import * as banner from "@uswds/usa-banner"
 import "@uswds/stylesheets/uswds.scss";
+import { useEffect } from "@storybook/addons";
 import component from "./usa-banner.twig";
 import {
   DefaultContent,
-  DefaultContentLangEs,
-  MilContent,
-  MilContentLangEs,
+  // DefaultContentLangEs,
+  // MilContent,
+  // MilContentLangEs,
 } from "./content";
+
+console.log(DefaultContent)
 
 const defaults = DefaultContent;
 
@@ -20,25 +24,42 @@ export default {
 //   console.info( "This page is not reloaded");
 // }
 
+// const Template = (...args) => {
+//   useEffect(() => {
+//     // console.log(window)
+//     console.log("on")
+//     // banner.on()
+
+//     return () => {
+//       console.log("off")
+//       // window.location.reload()
+//       // forceReRender(true)
+//     };
+//   })
+//   return component(...args);
+// }
+
+// banner.on()
+
 const Template = (...args) => component(...args)
 
 export const Default = Template.bind({});
 Default.args = defaults;
 
-export const DefaultSpanish = Template.bind({});
-DefaultSpanish.args = {
-  ...defaults,
-  ...DefaultContentLangEs,
-};
+// export const DefaultSpanish = Template.bind({});
+// DefaultSpanish.args = {
+//   ...defaults,
+//   ...DefaultContentLangEs,
+// };
 
-export const Mil = Template.bind({});
-Mil.args = {
-  ...defaults,
-  ...MilContent,
-};
+// export const Mil = Template.bind({});
+// Mil.args = {
+//   ...defaults,
+//   ...MilContent,
+// };
 
-export const MilSpanish = Template.bind({});
-MilSpanish.args = {
-  ...defaults,
-  ...MilContentLangEs,
-};
+// export const MilSpanish = Template.bind({});
+// MilSpanish.args = {
+//   ...defaults,
+//   ...MilContentLangEs,
+// };
