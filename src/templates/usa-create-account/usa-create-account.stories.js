469671dd63fbded74423255cf68337eb8d2d9d97
Amy Leadem <amy.leadem@bixal.com>
2022-02-10 13:04:53 -0700

    add sign-in page template

--- src/templates/usa-create-account/usa-create-account.stories.js
+++ src/templates/usa-create-account/usa-create-account.stories.js
@@ -1,23 +1,23 @@
-import Component from "./usa-docs.twig";
+import Component from "./usa-create-account.twig";
 
 import { DefaultContent as BannerData } from "../../components/usa-banner/content";
 import { headerData as HeaderData } from "../../components/usa-header/content";
-import { LevelTwoData as SidenavData } from "../../components/usa-sidenav/content";
 import { Data as IdentifierData } from "../../components/usa-identifier/content";
 import { Data as FooterData } from "../../components/usa-footer/content";
+import { FormData } from "./usa-create-account.json";
+
 
 export default {
-  title: "Templates/Documentation Page",
+  title: "Templates/Create Account Page",
   parameters: {
     layout: 'fullscreen',
   },
 };
 
-export const DocumentationPage = () => Component({ 
-    ...BannerData, 
-    ...HeaderData, 
-    ...SidenavData, 
-    ...IdentifierData, 
-    ...FooterData, 
-  });
-  
+export const CreateAccountPage = () => Component({ 
+  ...BannerData, 
+  ...HeaderData, 
+  ...IdentifierData, 
+  ...FooterData, 
+  ...FormData 
+});
--- src/templates/usa-sign-in/usa-sign-in.stories.js
+++ src/templates/usa-sign-in/usa-sign-in.stories.js
@@ -1,4 +1,4 @@
-import Component from "./usa-docs.twig";
+import Component from "./usa-sign-in.twig";
 
 import { DefaultContent as BannerData } from "../../components/usa-banner/content";
 import { headerData as HeaderData } from "../../components/usa-header/content";
@@ -7,13 +7,13 @@ import { Data as IdentifierData } from "../../components/usa-identifier/content"
 import { Data as FooterData } from "../../components/usa-footer/content";
 
 export default {
-  title: "Templates/Documentation Page",
+  title: "Templates/Sign In Page",
   parameters: {
     layout: 'fullscreen',
   },
 };
 
-export const DocumentationPage = () => Component({ 
+export const SignInPage = () => Component({ 
     ...BannerData, 
     ...HeaderData, 
     ...SidenavData, 
