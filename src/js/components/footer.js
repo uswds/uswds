26c2f0f9711cfeff4fd6761faa1555dd39389b8e
Mike Herchel <mike@herchel.com>
2022-03-03 12:52:52 -0500

    Refactoring JS so CSS styles against aria-hidden attribute.

--- src/js/components/footer.js
+++ src/js/components/footer.js
@@ -3,32 +3,18 @@ const select = require("../utils/select");
 const { CLICK } = require("../events");
 const { prefix: PREFIX } = require("../config");
 
-const HIDDEN = "hidden";
 const SCOPE = `.${PREFIX}-footer--big`;
 const NAV = `${SCOPE} nav`;
 const BUTTON = `${NAV} .${PREFIX}-footer__primary-link`;
-const COLLAPSIBLE = `.${PREFIX}-footer__primary-content--collapsible`;
-
 const HIDE_MAX_WIDTH = 480;
 
 function showPanel() {
   if (window.innerWidth < HIDE_MAX_WIDTH) {
-    const collapseEl = this.closest(COLLAPSIBLE);
-    const isOpen = !collapseEl.classList.contains(HIDDEN);
-    collapseEl.classList.toggle(HIDDEN, isOpen);
-    collapseEl.querySelector(BUTTON)?.setAttribute('aria-expanded', !isOpen);
-
-    // NB: this *should* always succeed because the button
-    // selector is scoped to ".{prefix}-footer-big nav"
-    const collapsibleEls = select(COLLAPSIBLE, collapseEl.closest(NAV));
-
-    collapsibleEls.forEach((el) => {
-      if (el !== collapseEl) {
-        el.classList.add(HIDDEN);
-      }
-    });
+    const isOpen = this.getAttribute('aria-expanded') === 'true';
+    this.setAttribute('aria-expanded', !isOpen);
   }
 }
+
 /**
  * Swaps the <h4> element for a <button> element (and vice-versa)
  *
@@ -59,8 +45,8 @@ function toggleHtmlTag(isMobile) {
 }
 
 const toggleHidden = (isHidden) =>
-  select(COLLAPSIBLE).forEach((list) =>
-    list.classList.toggle(HIDDEN, isHidden)
+  select(BUTTON).forEach((button) =>
+    button.setAttribute('aria-expanded', !isHidden)
   );
 
 const resize = (event) => {
