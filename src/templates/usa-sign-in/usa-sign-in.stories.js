5e602ae05e959d6105b733d44728b1f7eb09dfd1
Amy Leadem <amy.leadem@bixal.com>
2022-03-04 12:49:48 -0700

    update file structure on usa-sign-in and referenc(ed/ing) files

--- src/templates/usa-sign-in/usa-sign-in.stories.js
+++ src/templates/usa-sign-in/usa-sign-in.stories.js
@@ -14,13 +14,11 @@ import FooterContent from "../../components/usa-footer/usa-footer.json";
 
 // Import sign in form elements
 import SignInForm from "./usa-sign-in.twig";
-import SignInMultipleForm from "./usa-sign-in--multiple.twig";
-import {
-  DefaultContent,
-  EsContent,
-  MultipleContent,
-  EsMultipleContent,
-} from "./content";
+import SignInMultipleForm from "./usa-sign-in--multiple/usa-sign-in--multiple.twig";
+import DefaultContent from "./usa-sign-in.json";
+import EsContent from "./usa-sign-in~lang-es.json";
+import MultipleContent from "./usa-sign-in--multiple/usa-sign-in--multiple.json";
+import EsMultipleContent from "./usa-sign-in--multiple/usa-sign-in--multiple~lang-es.json";
 
 export default {
   title: "Pages/Sign-In",
