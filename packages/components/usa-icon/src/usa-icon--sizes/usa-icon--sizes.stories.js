dc99499d8e143066ee34e5d4240b38bc7c1770da
Dan O. Williams <daniel.williams@gsa.gov>
2022-03-08 23:16:26 -0800

    Move template and system style indexes to root

--- packages/components/usa-icon/src/usa-icon--sizes/usa-icon--sizes.stories.js
+++ packages/components/usa-icon/src/usa-icon--sizes/usa-icon--sizes.stories.js
@@ -1,5 +1,5 @@
 // TODO: Why is this here and not elsewhere?
-import "../../../../bundles/uswds/src/styles/_index.scss";
+import "../../../../bundles/uswds/_index.scss";
 import Component from "./usa-icon--sizes.twig";
 import Content from "./usa-icon--sizes.json";
 
