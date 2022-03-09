65b6593391bbc9d93c5b0133d447f5ee6a4abb98
Dan O. Williams <daniel.williams@gsa.gov>
2022-03-09 11:40:31 -0800

    Exclude sass tests from unit test glob

--- gulp-tasks/test.js
+++ gulp-tasks/test.js
@@ -2,14 +2,14 @@ const { src } = require("gulp");
 const mocha = require("gulp-spawn-mocha");
 
 const mochaConfig = {
-  config: "src/utils/test/.mocharc.json",
+  config: "packages/utils/test/.mocharc.json",
 };
 
 // Export our tasks.
 module.exports = {
   // run unit test.
   unitTests() {
-    return src("packages/**/*.spec.js").pipe(mocha(mochaConfig));
+    return src("packages/**/[!sass]*.spec.js").pipe(mocha(mochaConfig));
   },
 
   sassTests() {
