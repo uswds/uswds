30ea32c81bee8d3404cb82bd01093498398b5726
Dan O. Williams <daniel.williams@gsa.gov>
2022-03-10 11:02:01 -0800

    Revert outputPath test

--- .storybook/main.js
+++ .storybook/main.js
@@ -116,7 +116,7 @@ module.exports = {
           loader: "file-loader",
           options: {
             name: "[name].[ext]",
-            outputPath: "../dist/img",
+            //outputPath: "../dist/img",
           },
         },
         include: path.resolve(__dirname, "../packages"),
