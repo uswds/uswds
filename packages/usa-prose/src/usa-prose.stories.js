8ccc18a6ba06f15b0afd91bebd9794d10040405e
Amy Leadem <amy.leadem@bixal.com>
2022-03-31 09:57:39 -0700

    add usa-prose to storybook

---
+++ packages/usa-prose/src/usa-prose.stories.js
@@ -0,0 +1,9 @@
+import Component from "./usa-prose.twig";
+
+export default {
+  title: "Components/Prose",
+};
+
+const Template = (args) => Component(args);
+
+export const Default = Template.bind({});
