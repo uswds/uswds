d52d74817834a1abe815f57496720878055db1fa
Amy Leadem <amy.leadem@bixal.com>
2022-04-12 13:07:56 -0700

    remove multi-column test on checkbox stories

--- packages/usa-checkbox/src/usa-checkbox.stories.js
+++ packages/usa-checkbox/src/usa-checkbox.stories.js
@@ -1,7 +1,6 @@
 import Component from "./usa-checkbox.twig";
 import Tile from "./usa-checkbox--tile.twig";
-import TestComponent from "./test/test-patterns/test-usa-multi-column-checkboxes.twig";
-import TestComponentMultiple from "./test/test-patterns/test-usa-checkbox.twig";
+import TestComponent from "./test/test-patterns/test-usa-checkbox.twig";
 
 export default {
   title: "Components/Form Inputs/Checkbox",
@@ -10,9 +9,7 @@ export default {
 const Template = (args) => Component(args);
 const TileTemplate = (args) => Tile(args);
 const TestTemplate = (args) => TestComponent(args);
-const TestTemplateMultiple = (args) => TestComponentMultiple(args);
 
 export const Default = Template.bind({});
 export const CheckboxTile = TileTemplate.bind({});
-export const TestMultipleColumns = TestTemplate.bind({});
-export const TestMultipleFieldsets = TestTemplateMultiple.bind({});
+export const Test = TestTemplate.bind({});
