863da24931a52e38c1d577204ef5fe37b5f8f8f0
Amy Leadem <amy.leadem@bixal.com>
2022-05-17 07:06:45 -0700

    Add controls with related props and initProps

--- packages/usa-date-range-picker/src/date-range-picker.stories.js
+++ packages/usa-date-range-picker/src/date-range-picker.stories.js
@@ -1,4 +1,5 @@
 import Component from "./usa-date-range-picker.twig";
+import Content from "./usa-date-range-picker.json";
 
 export default {
   title: "Components/Form Inputs/Date Range Picker",
@@ -7,3 +8,21 @@ export default {
 const Template = (args) => Component(args);
 
 export const Default = Template.bind({});
+Default.args = Content;
+
+export const DefaultValue = Template.bind({});
+DefaultValue.args = {
+  defaultValueStart: "1995-03-07",
+  defaultValueEnd: "1995-03-15",
+};
+
+export const RestrictedDate = Template.bind({});
+RestrictedDate.args = {
+  minDate: "2022-01-07",
+  maxDate: "2022-01-25",
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+};
