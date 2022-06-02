6d8e739912d63c571d2104b7cbcd494a904d9b5c
Charlie Mahoney <charlie.mahoneyy@gmail.com>
2022-06-02 12:29:22 -0400

    Begin creating test environment

--- packages/usa-select/src/usa-select.stories.js
+++ packages/usa-select/src/usa-select.stories.js
@@ -1,9 +1,18 @@
 import Component from "./usa-select.twig";
+import Content from "./usa-select.json";
 
 export default {
   title: "Components/Form Inputs/Select",
+
+  argTypes: {
+    variant: {
+      options: ["default", "multiple"],
+      control: { type: "radio" },
+    },
+  },
 };
 
 const Template = (args) => Component(args);
 
 export const Select = Template.bind({});
+Select.args = Content;
