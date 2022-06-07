b430dd95731640cce0334b47cbf3d664aa235dde
Amy Leadem <amy.leadem@bixal.com>
2022-06-07 11:45:11 -0700

    Update storybook names and argTypes

--- packages/usa-date-range-picker/src/date-range-picker.stories.js
+++ packages/usa-date-range-picker/src/date-range-picker.stories.js
@@ -1,28 +1,84 @@
 import Component from "./usa-date-range-picker.twig";
-import Content from "./usa-date-range-picker.json";
 
 export default {
   title: "Components/Form Inputs/Date Range Picker",
+  argTypes: {
+    defaultStart: {
+      name: "Default Value: Start (YYYY-MM-DD)",
+      control: { type: 'text' },
+    },
+    defaultEnd: {
+      name: "Default Value: End (YYYY-MM-DD)",
+      control: { type: 'text' },
+    },
+    restrictedStart: {
+      name: "Restricted Date: Start (YYYY-MM-DD)",
+      control: { type: 'text' },
+    },
+    restrictedEnd: {
+      name: "Restricted Date: End (YYYY-MM-DD)",
+      control: { type: 'text' },
+    },
+    disabled: {
+      name: "Disabled",
+      defaultValue: 'false',
+      control: { type: 'boolean' },
+    },
+  },
 };
+
 
 const Template = (args) => Component(args);
 
 export const Default = Template.bind({});
-Default.args = Content;
 
 export const DefaultValue = Template.bind({});
-DefaultValue.args = {
-  defaultValueStart: "1995-03-07",
-  defaultValueEnd: "1995-03-15",
+DefaultValue.argTypes = {
+  defaultStart: {
+    defaultValue: "1995-03-06",
+  },
+  defaultEnd: {
+    defaultValue: "1995-03-15",
+  },
+  restrictedStart: {
+    table: { disable: true },
+  },
+  restrictedEnd: {
+    table: { disable: true },
+  },
 };
 
 export const RestrictedDate = Template.bind({});
-RestrictedDate.args = {
-  minDate: "2022-01-07",
-  maxDate: "2022-01-25",
+RestrictedDate.argTypes = {
+  restrictedStart: {
+    defaultValue: "1995-03-06",
+  },
+  restrictedEnd: {
+    defaultValue: "1995-03-15",
+  },
+  defaultStart: {
+    table: { disable: true },
+  },
+  defaultEnd: {
+    table: { disable: true },
+  },
 };
 
 export const Disabled = Template.bind({});
-Disabled.args = {
-  disabled: true,
+Disabled.argTypes = {
+  disabled: {
+    defaultValue: 'true',
+  },
+  defaultStart: {
+    table: { disable: true },
+  },
+  defaultEnd: {
+    table: { disable: true },
+  },
+  restrictedStart: {
+    table: { disable: true },
+  },
+  restrictedEnd: {
+    table: { disable: true },
+  },
 };
