603e3d26630b0eb2286eb267753a117d07873d37
Amy Leadem <amy.leadem@bixal.com>
2022-06-09 13:19:39 -0700

    Create aspect ratio comparison page

--- packages/usa-add-aspect/src/usa-add-aspect.stories.js
+++ packages/usa-add-aspect/src/usa-add-aspect.stories.js
@@ -1,9 +1,12 @@
 import Component from "./usa-add-aspect.twig";
+import TestComponent from "./test/usa-add-aspect-comparison.twig";
 
 export default {
   title: "Components/Add Aspect",
 };
 
 const Template = (args) => Component(args);
+const TestTemplate = (args) => TestComponent(args);
 
 export const AddAspect = Template.bind({});
+export const AspectComparison = TestTemplate.bind({});
