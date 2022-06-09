3a90cef86679c8c96459e61fb056a8f04aa81df1
Amy Leadem <amy.leadem@bixal.com>
2022-06-09 13:53:35 -0700

    Add aspect-ratio comparison for embed container

--- packages/usa-embed-container/src/usa-embed-container.stories.js
+++ packages/usa-embed-container/src/usa-embed-container.stories.js
@@ -1,13 +1,22 @@
 import Component from "./usa-embed-container.twig";
+import TestComponent from "./test/usa-embed-container-comparison.twig";
 
 export default {
   title: "Components/Embed Container",
 };
 
 const Template = (args) => Component(args);
+const TestTemplate = (args) => TestComponent(args);
 
 export const EmbedContainer = Template.bind({});
 EmbedContainer.parameters = {
+  axe: {
+    skip: true,
+  },
+};
+
+export const Test = TestTemplate.bind({});
+Test.parameters = {
   axe: {
     skip: true,
   },
