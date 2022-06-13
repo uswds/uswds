23a5c3f55d886c5437f123235922d7f1cb4c98d7
Charlie Mahoney <charlie.mahoneyy@gmail.com>
2022-06-13 15:56:50 -0400

    Updates initialization on validation component
    - Adds aria-controls to validation inputs
    - Moves aria-live and aria-atomic to parent list instead of list items
    - Sets aria-checked=false by default on initialization

--- packages/usa-validation/src/index.js
+++ packages/usa-validation/src/index.js
@@ -4,6 +4,7 @@ const { prefix: PREFIX } = require("../../uswds-core/src/js/config");
 const selectOrMatches = require("../../uswds-core/src/js/utils/select-or-matches");
 
 const VALIDATE_INPUT = "input[data-validation-element]";
+const CHECKLIST = `.${PREFIX}-checklist`;
 const CHECKLIST_ITEM = `.${PREFIX}-checklist__item`;
 
 function change() {
@@ -30,14 +31,21 @@ const validator = behavior(
     init(root) {
       selectOrMatches(VALIDATE_INPUT, root).forEach((item) => {
         const validationParent = item.parentNode;
+        const checklist = validationParent.querySelectorAll(CHECKLIST);
         const checklistItems =
           validationParent.querySelectorAll(CHECKLIST_ITEM);
+
+        item.setAttribute("aria-controls", "validate-code");
+
+        checklist.forEach((list) => {
+          list.setAttribute("aria-live", "polite");
+          list.setAttribute("aria-atomic", true);
+        });
 
         checklistItems.forEach((listItem) => {
           const newSpan = createHiddenLabel();
           listItem.appendChild(newSpan);
-          listItem.setAttribute("aria-live", "polite");
-          listItem.setAttribute("aria-atomic", true);
+          listItem.setAttribute("aria-checked", "false");
         });
       });
     },
