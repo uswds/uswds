36d95de946d3bf8842cbe75ceac692e75aa0314b
mitchmoccia <code@3dx8.com>
2022-07-03 23:09:39 -0400

    Add early version of in-page navigation JS

--- packages/usa-in-page-navigation/src/index.js
+++ packages/usa-in-page-navigation/src/index.js
@@ -1,230 +1,110 @@
-const keymap = require("receptor/keymap");
-const behavior = require("../../uswds-core/src/js/utils/behavior");
-const select = require("../../uswds-core/src/js/utils/select");
-const toggle = require("../../uswds-core/src/js/utils/toggle");
-const FocusTrap = require("../../uswds-core/src/js/utils/focus-trap");
-const accordion = require("../../usa-accordion/src/index");
-const ScrollBarWidth = require("../../uswds-core/src/js/utils/scrollbar-width");
 
-const { CLICK } = require("../../uswds-core/src/js/events");
-const { prefix: PREFIX } = require("../../uswds-core/src/js/config");
+function ready(fn) {
+  document.addEventListener('DOMContentLoaded', fn, false);
+}
 
-const BODY = "body";
-const HEADER = `.${PREFIX}-header`;
-const NAV = `.${PREFIX}-nav`;
-const NAV_PRIMARY = `.${PREFIX}-nav__primary`;
-const NAV_PRIMARY_ITEM = `.${PREFIX}-nav__primary-item`;
-const NAV_CONTROL = `button.${PREFIX}-nav__link`;
-const NAV_LINKS = `${NAV} a`;
-const NON_NAV_HIDDEN_ATTRIBUTE = `data-nav-hidden`;
-const OPENERS = `.${PREFIX}-menu-btn`;
-const CLOSE_BUTTON = `.${PREFIX}-nav__close`;
-const OVERLAY = `.${PREFIX}-overlay`;
-const CLOSERS = `${CLOSE_BUTTON}, .${PREFIX}-overlay`;
-const TOGGLES = [NAV, OVERLAY].join(", ");
-const NON_NAV_ELEMENTS = `body > *:not(${HEADER}):not([aria-hidden])`;
-const NON_NAV_HIDDEN = `[${NON_NAV_HIDDEN_ATTRIBUTE}]`;
+ready(() => {
+  const motionQuery = window.matchMedia('(prefers-reduced-motion)');
 
-const ACTIVE_CLASS = "usa-js-mobile-nav--active";
-const VISIBLE_CLASS = "is-visible";
-
-let inPageNavigation;
-let navActive;
-let nonNavElements;
-
-const isActive = () => document.body.classList.contains(ACTIVE_CLASS);
-const SCROLLBAR_WIDTH = ScrollBarWidth();
-const INITIAL_PADDING = window
-  .getComputedStyle(document.body)
-  .getPropertyValue("padding-right");
-const TEMPORARY_PADDING = `${
-  parseInt(INITIAL_PADDING.replace(/px/, ""), 10) +
-  parseInt(SCROLLBAR_WIDTH.replace(/px/, ""), 10)
-}px`;
-
-const hideNonNavItems = () => {
-  nonNavElements = document.querySelectorAll(NON_NAV_ELEMENTS);
-
-  nonNavElements.forEach((nonNavElement) => {
-    nonNavElement.setAttribute("aria-hidden", true);
-    nonNavElement.setAttribute(NON_NAV_HIDDEN_ATTRIBUTE, "");
-  });
-};
-
-const showNonNavItems = () => {
-  nonNavElements = document.querySelectorAll(NON_NAV_HIDDEN);
-
-  if (!nonNavElements) {
-    return;
-  }
-
-  // Remove aria-hidden from non-header elements
-  nonNavElements.forEach((nonNavElement) => {
-    nonNavElement.removeAttribute("aria-hidden");
-    nonNavElement.removeAttribute(NON_NAV_HIDDEN_ATTRIBUTE);
-  });
-};
-
-// Toggle all non-header elements #3527.
-const toggleNonNavItems = (active) => {
-  if (active) {
-    hideNonNavItems();
-  } else {
-    showNonNavItems();
-  }
-};
-
-const toggleNav = (active) => {
-  const { body } = document;
-  const safeActive = typeof active === "boolean" ? active : !isActive();
-
-  body.classList.toggle(ACTIVE_CLASS, safeActive);
-
-  select(TOGGLES).forEach((el) =>
-    el.classList.toggle(VISIBLE_CLASS, safeActive)
-  );
-
-  inPageNavigation.focusTrap.update(safeActive);
-
-  const closeButton = body.querySelector(CLOSE_BUTTON);
-  const menuButton = document.querySelector(OPENERS);
-
-  body.style.paddingRight =
-    body.style.paddingRight === TEMPORARY_PADDING
-      ? INITIAL_PADDING
-      : TEMPORARY_PADDING;
-
-  toggleNonNavItems(safeActive);
-
-  if (safeActive && closeButton) {
-    // The mobile nav was just activated. Focus on the close button, which is
-    // just before all the nav elements in the tab order.
-    closeButton.focus();
-  } else if (
-    !safeActive &&
-    document.activeElement === closeButton &&
-    menuButton
-  ) {
-    // The mobile nav was just deactivated, and focus was on the close
-    // button, which is no longer visible. We don't want the focus to
-    // disappear into the void, so focus on the menu button if it's
-    // visible (this may have been what the user was just focused on,
-    // if they triggered the mobile nav by mistake).
-    menuButton.focus();
-  }
-
-  return safeActive;
-};
-
-const resize = () => {
-  const closer = document.body.querySelector(CLOSE_BUTTON);
-
-  if (isActive() && closer && closer.getBoundingClientRect().width === 0) {
-    // When the mobile nav is active, and the close box isn't visible,
-    // we know the user's viewport has been resized to be larger.
-    // Let's make the page state consistent by deactivating the mobile nav.
-    inPageNavigation.toggleNav.call(closer, false);
-  }
-};
-
-const onMenuClose = () => inPageNavigation.toggleNav.call(inPageNavigation, false);
-
-const hideActiveNavDropdown = () => {
-  if (!navActive) {
-    return;
-  }
-
-  toggle(navActive, false);
-  navActive = null;
-};
-
-const focusNavButton = (event) => {
-  const parentNavItem = event.target.closest(NAV_PRIMARY_ITEM);
-
-  // Only shift focus if within dropdown
-  if (!event.target.matches(NAV_CONTROL)) {
-    parentNavItem.querySelector(NAV_CONTROL).focus();
-  }
-};
-
-const handleEscape = (event) => {
-  hideActiveNavDropdown();
-  focusNavButton(event);
-};
-
-inPageNavigation = behavior(
-  {
-    [CLICK]: {
-      [NAV_CONTROL]() {
-        // If another nav is open, close it
-        if (navActive !== this) {
-          hideActiveNavDropdown();
-        }
-        // store a reference to the last clicked nav link element, so we
-        // can hide the dropdown if another element on the page is clicked
-        if (!navActive) {
-          navActive = this;
-          toggle(navActive, true);
-        }
-
-        // Do this so the event handler on the body doesn't fire
-        return false;
-      },
-      [BODY]: hideActiveNavDropdown,
-      [OPENERS]: toggleNav,
-      [CLOSERS]: toggleNav,
-      [NAV_LINKS]() {
-        // A navigation link has been clicked! We want to collapse any
-        // hierarchical navigation UI it's a part of, so that the user
-        // can focus on whatever they've just selected.
-
-        // Some navigation links are inside accordions; when they're
-        // clicked, we want to collapse those accordions.
-        const acc = this.closest(accordion.ACCORDION);
-
-        if (acc) {
-          accordion.getButtons(acc).forEach((btn) => accordion.hide(btn));
-        }
-
-        // If the mobile navigation menu is active, we want to hide it.
-        if (isActive()) {
-          inPageNavigation.toggleNav.call(inPageNavigation, false);
-        }
-      },
+  const InPageNavigation = {
+    container: document.querySelector('.in-page-navigation'),
+    links: null,
+    headings: null,
+    intersectionOptions: {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1,
     },
-    keydown: {
-      [NAV_PRIMARY]: keymap({ Escape: handleEscape }),
-    },
-    focusout: {
-      [NAV_PRIMARY](event) {
-        const nav = event.target.closest(NAV_PRIMARY);
 
-        if (!nav.contains(event.relatedTarget)) {
-          hideActiveNavDropdown();
+    previousSection: null,
+    observer: null,
+
+    init() {
+      this.handleObserver = this.handleObserver.bind(this);
+
+      this.setUpObserver();
+      this.findLinksAndHeadings();
+      this.observeSections();
+
+      this.links.forEach((link) => {
+        link.addEventListener('click', this.handleLinkClick.bind(this));
+      });
+    },
+
+    handleLinkClick(evt) {
+      evt.preventDefault();
+      let id = evt.target.hash.replace('#', '');
+
+      let section = this.headings.find((heading) => {
+        return heading.getAttribute('id') === id;
+      });
+
+      section.setAttribute('tabindex', -1);
+      //section.focus();
+
+      window.scroll({
+        behavior: motionQuery.matches ? 'instant' : 'smooth',
+        top: section.offsetTop + 140,
+        block: 'start',
+      });
+
+      if (this.container.classList.contains('usa-current')) {
+        this.container.classList.remove('usa-current');
+      }
+    },
+
+    handleObserver(entries, observer) {
+      entries.forEach((entry) => {
+        let href = `#${entry.target.getAttribute('id')}`,
+          link = this.links.find((l) => l.getAttribute('href') === href);
+
+        if (entry.isIntersecting && entry.intersectionRatio >= 1) {
+          link.classList.add('is-visible');
+          this.previousSection = entry.target.getAttribute('id');
+        } else {
+          link.classList.remove('is-visible');
         }
-      },
-    },
-  },
-  {
-    init(root) {
-      const trapContainer = root.matches(NAV) ? root : root.querySelector(NAV);
 
-      if (trapContainer) {
-        inPageNavigation.focusTrap = FocusTrap(trapContainer, {
-          Escape: onMenuClose,
-        });
+        this.highlightFirstActive();
+      });
+    },
+
+    highlightFirstActive() {
+      let firstVisibleLink = this.container.querySelector('.is-visible');
+
+      this.links.forEach((link) => {
+        link.classList.remove('usa-current');
+      });
+
+      if (firstVisibleLink) {
+        firstVisibleLink.classList.add('usa-current');
       }
 
-      resize();
-      window.addEventListener("resize", resize, false);
+      if (!firstVisibleLink && this.previousSection) {
+        this.container
+          .querySelector(`a[href="#${this.previousSection}"]`)
+          .classList.add('usa-current');
+      }
     },
-    teardown() {
-      window.removeEventListener("resize", resize, false);
-      navActive = false;
-    },
-    focusTrap: null,
-    toggleNav,
-  }
-);
 
-module.exports = inPageNavigation;
+    observeSections() {
+      this.headings.forEach((heading) => {
+        this.observer.observe(heading);
+      });
+    },
+
+    setUpObserver() {
+      this.observer = new IntersectionObserver(this.handleObserver, this.intersectionOptions);
+    },
+
+    findLinksAndHeadings() {
+      this.links = [...this.container.querySelectorAll('a')];
+      this.headings = this.links.map((link) => {
+        let id = link.getAttribute('href');
+        return document.querySelector(id);
+      });
+    },
+  };
+
+  InPageNavigation.init();
+});
