01dafee042d688fac29c43c56a2473f2be307ec9
Amy Leadem <amy.leadem@bixal.com>
2022-07-15 08:53:08 -0700

    Restructure button-group twig and json to apply aria-disabled to elements

--- packages/usa-button-group/src/usa-button-group.stories.js
+++ packages/usa-button-group/src/usa-button-group.stories.js
@@ -3,6 +3,27 @@ import { DefaultContent, SegmentedContent } from "./content";
 
 export default {
   title: "Components/Button Group",
+  argTypes: {
+    disabled: {
+      control: {type: 'boolean'},
+      defaultValue: false,
+      name: "disabled",
+    },
+    aria_disabled: {
+      control: {type: 'boolean'},
+      defaultValue: false,
+      name: "aria-disabled",
+    },
+    segmented: {
+      table: { disable: true }
+    },
+    modifiers: {
+      table: { disable: true }
+    },
+    modifier: {
+      table: { disable: true }
+    }
+  }
 };
 
 const Template = (args) => Component(args);
