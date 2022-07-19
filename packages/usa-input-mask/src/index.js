3943b50635bb202e80e8cbcf700c3dedf1626392
Jonathan Bobel <jonathan.bobel@gsa.gov>
2022-07-19 14:33:55 -0400

    Updates to Input Mask

--- packages/usa-input-mask/src/index.js
+++ packages/usa-input-mask/src/index.js
@@ -1,2 +1,15 @@
-const inputMask = document.getElementsById('input-type-text-mask');
-inputMask.addClass('hey');
+const { InputMask } = require("./usa-input-mask.stories");
+
+    // Phone Mask
+    document.getElementById('phone').addEventListener('input', (e) => {
+        const x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+        e.target.value = !x[2] ? x[1] : `(${x[1]}) ${x[2]}${x[3] ? `-${x[3]}` : ''}`;
+    });
+
+    // SSN Mask
+    document.getElementById('ssn').addEventListener('input', (e) => {
+        const x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,2})(\d{0,4})/);
+        e.target.value = `${x[1]} ${x[2]} ${x[3]}`;
+    });
+
+module.exports = InputMask; 
