322a82d31b19f90c0eade9493d2fb2086dc1fce8
Jonathan Bobel <jonathan.bobel@gsa.gov>
2022-07-20 15:26:47 -0400

    Testing zip regex

--- packages/usa-input-mask/src/index.js
+++ packages/usa-input-mask/src/index.js
@@ -16,6 +16,10 @@ ready(() => {
         e.target.value = `${x[1]} ${x[2]} ${x[3]}`;
     });
 
-});
+    // // Zip Mask
+    // document.getElementById('zip-code').addEventListener('input', (e) => {
+    //     const x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,2})(\d{0,4})/);
+    //     e.target.value = `${x[1]} ${x[2]} ${x[3]}`;
+    // });
 
-// module.exports = inputMask;
+});
