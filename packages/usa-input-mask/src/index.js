69d553e2c9ac08ef60f8f16f6e9dc22d996fd7ba
mitchmoccia <code@3dx8.com>
2022-07-26 20:16:52 -0400

    Work in progress refactoring JBs code

--- packages/usa-input-mask/src/index.js
+++ packages/usa-input-mask/src/index.js
@@ -1,156 +1,165 @@
 const Sanitizer = require("../../uswds-core/src/js/utils/sanitizer");
 
+const MASKED = "masked";
+const MASKED_CLASS = `.${MASKED}`;
+const SHELL = "shell";
+
 function ready(fn) {
   document.addEventListener("DOMContentLoaded", fn, false);
 }
 
 ready(() => {
- 
   const masking = {
-  // User defined Values
-  maskedInputs : document.getElementsByClassName('masked'), 
-  maskedNumber: "XdDmMyY9",
-  maskedLetter: "_",
+    // User defined Values
+    maskedInputs: document.getElementsByClassName(MASKED),
+    maskedNumber: "XdDmMyY9",
+    maskedLetter: "_",
 
-  init () {
-    masking.setUpMasks(masking.maskedInputs);
-    masking.maskedInputs = document.querySelectorAll(".masked"); // Repopulating. Needed b/c static node list was created above.
-    masking.activateMasking(masking.maskedInputs);
-  },
+    init() {
+      masking.setUpMasks(masking.maskedInputs);
+      masking.maskedInputs = document.querySelectorAll(MASKED_CLASS); // Repopulating. Needed b/c static node list was created above.
+      masking.activateMasking(masking.maskedInputs);
+    },
 
-  setUpMasks (inputs) {
-    let i;
-    const l = inputs.length;
+    setUpMasks(inputs) {
+      let i;
+      const l = inputs.length;
 
-    for (i = 0; i < l; i += 1) {
-      masking.createShell(inputs[i]);
-    }
-  },
-
-  // replaces each masked input with a shall containing the input and it's mask.
-  createShell (input) {
-    let text = "";
-    const placeholder = input.getAttribute("placeholder");
-
-    if (placeholder.length > 0 && placeholder !== " ") {
-      input.setAttribute("maxlength", placeholder.length);
-      input.setAttribute("data-placeholder", placeholder);
-      input.removeAttribute("placeholder");
-    }
-
-    text =
-      `${'<span class="shell">' +
-      '<span aria-hidden="true" id="'}${ 
-      input.id 
-      }Mask"><i></i>${ 
-      placeholder 
-      }</span>${ 
-      input.outerHTML 
-      }</span>`;
-
-    input.outerHTML = text; // eslint-disable-line no-param-reassign
-
-  },
-
-  setValueOfMask (e) {
-    const {value} = e.target;
-    const placeholder = e.target.getAttribute("data-placeholder");
-
-    return `<i>${  value  }</i>${  placeholder.substr(value.length)}`;
-  },
-
-  // add event listeners
-  activateMasking (inputs) {
-    let i;
-    let l;
-
-    for (i = 0, l = inputs.length; i < l; i += 1) {
-      masking.maskedInputs[i].addEventListener(
-        "keyup",
-        (e) => {
-          masking.handleValueChange(e);
-        },
-        false
-      );
-    }
-  },
-
-  handleValueChange (e) {
-    const id = e.target.getAttribute("id");
-
-    switch (
-      e.keyCode // allows navigating thru input
-    ) {
-      case 20: // caplocks
-      case 17: // control
-      case 18: // option
-      case 16: // shift
-      case 37: // arrow keys
-      case 38:
-      case 39:
-      case 40:
-      case 9: // tab (let blur handle tab)
-        return;
-      // no default
-    }
-
-    document.getElementById(id).value = masking.handleCurrentValue(e);
-    // document.getElementById(`${id}Mask`).innerHTML = Sanitizer.escapeHTML`${masking.setValueOfMask(e)}`;
-    document.getElementById(`${id}Mask`).innerHTML = `${masking.setValueOfMask(e)}`;
-    
-  },
-
-  handleCurrentValue (e) {
-    const isCharsetPresent = e.target.getAttribute("data-charset");
-    const placeholder = isCharsetPresent || e.target.getAttribute("data-placeholder");
-    const {value} = e.target;
-    const l = placeholder.length;
-    let newValue = "";
-    let i;
-    let j;
-    let isInt;
-    let isLetter;
-
-    // strip special characters
-    const strippedValue = isCharsetPresent
-      ? value.replace(/\W/g, "")
-      : value.replace(/\D/g, "");
-
-    for (i = 0, j = 0; i < l; i++) {
-      // eslint-disable-next-line no-multi-assign, no-restricted-globals
-      const x = (isInt = !isNaN(parseInt(strippedValue[j], 10)));
-      isLetter = strippedValue[j] ? strippedValue[j].match(/[A-Z]/i) : false;
-      const matchesNumber = masking.maskedNumber.indexOf(placeholder[i]) >= 0;
-      const matchesLetter = masking.maskedLetter.indexOf(placeholder[i]) >= 0;
-
-      if (
-        (matchesNumber && isInt) ||
-        (isCharsetPresent && matchesLetter && isLetter)
-      ) {
-        newValue += strippedValue[j++];
-      } else if (
-        (!isCharsetPresent && !isInt && matchesNumber) ||
-        (isCharsetPresent &&
-          ((matchesLetter && !isLetter) || (matchesNumber && !isInt)))
-      ) {
-        // masking.errorOnKeyEntry(); // write your own error handling function
-        return newValue;
-      } else {
-        newValue += placeholder[i];
+      for (i = 0; i < l; i += 1) {
+        masking.createShell(inputs[i]);
       }
-      // break if no characters left and the pattern is non-special character
-      if (strippedValue[j] === undefined) {
-        break;
+    },
+
+    // replaces each masked input with a shall containing the input and it's mask.
+    createShell(input) {
+      let text = "";
+      const placeholder = input.getAttribute("placeholder");
+
+      if (placeholder.length > 0 && placeholder !== " ") {
+        input.setAttribute("maxlength", placeholder.length);
+        input.setAttribute("data-placeholder", placeholder);
+        input.removeAttribute("placeholder");
       }
-    }
-    if (e.target.getAttribute("data-valid-example")) {
-      return masking.validateProgress(e, newValue);
-    }
-    return newValue;
-  },
 
-};
+      /* input.insertAdjacentHTML(
+        "beforeend",
+        Sanitizer.escapeHTML`
+        <span class="shell"><span aria-hidden="true" id="${input.id}Mask"><i></i>${placeholder}</span>${input.outerHTML}</span>
+        `
+      ); */
 
-masking.init();
+      text = `<span class="shell"><span aria-hidden="true" id="${input.id}Mask"><i></i>${placeholder}</span>${input.outerHTML}</span>`;
 
+      input.outerHTML = text; // eslint-disable-line no-param-reassign
+    },
+
+    setValueOfMask(e) {
+      const { value } = e.target;
+      const placeholder = e.target.getAttribute("data-placeholder");
+
+      return `<i>${value}</i>${placeholder.substr(value.length)}`;
+    },
+
+    // add event listeners
+    activateMasking(inputs) {
+      let i;
+      let l;
+
+      for (i = 0, l = inputs.length; i < l; i += 1) {
+        masking.maskedInputs[i].addEventListener(
+          "keyup",
+          (e) => {
+            masking.handleValueChange(e);
+          },
+          false
+        );
+      }
+    },
+
+    handleValueChange(e) {
+      const id = e.target.getAttribute("id");
+
+      switch (
+        e.keyCode // allows navigating thru input
+      ) {
+        case 20: // caplocks
+        case 17: // control
+        case 18: // option
+        case 16: // shift
+        case 37: // arrow keys
+        case 38:
+        case 39:
+        case 40:
+        case 9: // tab (let blur handle tab)
+          return;
+        // no default
+      }
+
+      document.getElementById(id).value = masking.handleCurrentValue(e);
+      // document.getElementById(`${id}Mask`).innerHTML = Sanitizer.escapeHTML`${masking.setValueOfMask(e)}`;
+      document.getElementById(
+        `${id}Mask`
+      ).innerHTML = `${masking.setValueOfMask(e)}`;
+
+      /* const theVal = Sanitizer.escapeHTML`${masking.setValueOfMask(e)}`; 
+
+      document.getElementById(
+        `${id}Mask`
+      ).insertAdjacentElement("beforeend", theVal); */
+
+
+    },
+
+    handleCurrentValue(e) {
+      const isCharsetPresent = e.target.getAttribute("data-charset");
+      const placeholder =
+        isCharsetPresent || e.target.getAttribute("data-placeholder");
+      const { value } = e.target;
+      const l = placeholder.length;
+      let newValue = "";
+      let i;
+      let j;
+      let isInt;
+      let isLetter;
+
+      // strip special characters
+      const strippedValue = isCharsetPresent
+        ? value.replace(/\W/g, "")
+        : value.replace(/\D/g, "");
+
+      for (i = 0, j = 0; i < l; i += 1) {
+        // eslint-disable-next-line no-multi-assign, no-restricted-globals
+        const x = (isInt = !isNaN(parseInt(strippedValue[j], 10)));
+        isLetter = strippedValue[j] ? strippedValue[j].match(/[A-Z]/i) : false;
+        const matchesNumber = masking.maskedNumber.indexOf(placeholder[i]) >= 0;
+        const matchesLetter = masking.maskedLetter.indexOf(placeholder[i]) >= 0;
+
+        if (
+          (matchesNumber && isInt) ||
+          (isCharsetPresent && matchesLetter && isLetter)
+        ) {
+          newValue += strippedValue[j++];
+        } else if (
+          (!isCharsetPresent && !isInt && matchesNumber) ||
+          (isCharsetPresent &&
+            ((matchesLetter && !isLetter) || (matchesNumber && !isInt)))
+        ) {
+          // masking.errorOnKeyEntry(); // write your own error handling function
+          return newValue;
+        } else {
+          newValue += placeholder[i];
+        }
+        // break if no characters left and the pattern is non-special character
+        if (strippedValue[j] === undefined) {
+          break;
+        }
+      }
+      if (e.target.getAttribute("data-valid-example")) {
+        return masking.validateProgress(e, newValue);
+      }
+      return newValue;
+    },
+  };
+
+  masking.init();
 });
