2c93c42d424156ffd0e77a73c91dc44c309e557e
Amy Leadem <amy.leadem@bixal.com>
2022-08-16 13:47:19 -0700

    Create alert comparison test story

--- packages/usa-alert/src/usa-alert.stories.js
+++ packages/usa-alert/src/usa-alert.stories.js
@@ -1,5 +1,7 @@
 import Component from "./usa-alert.twig";
 import TestComponent from "./test/test-patterns/test-usa-alert-lists.twig";
+import ComparisonComponent from "./test/test-patterns/test-alert-comparison.twig";
+
 import {
   DefaultContent,
   EmergencyContent,
@@ -18,6 +20,7 @@ export default {
 
 const Template = (args) => Component(args);
 const TestTemplate = (args) => TestComponent(args);
+const ComparisonTemplate = (args) => ComparisonComponent(args);
 
 export const Default = Template.bind({});
 Default.args = DefaultContent;
@@ -47,3 +50,5 @@ export const Warning = Template.bind({});
 Warning.args = WarningContent;
 
 export const Test = TestTemplate.bind({});
+
+export const AlertComparison = ComparisonTemplate.bind({});
