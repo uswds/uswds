48f1d6a59453fe11e857a5f782a954fd2e7c495f
Amy Leadem <amy.leadem@bixal.com>
2022-08-17 14:00:19 -0700

    Create test with sidenav and layout-docs

--- packages/usa-in-page-navigation/src/usa-in-page-navigation.stories.js
+++ packages/usa-in-page-navigation/src/usa-in-page-navigation.stories.js
@@ -1,11 +1,17 @@
 import Component from "./usa-in-page-navigation.twig";
 import Content from "./usa-in-page-navigation.json";
+import TestComponent from "./test/test-in-page-navigation-inner.twig";
+
 
 export default {
   title: "Components/In-Page Navigation",
 };
 
 const Template = (args) => Component(args);
+const TestTemplate = (args) => TestComponent(args);
 
 export const Default = Template.bind({});
 Default.args = Content;
+
+export const Test = TestTemplate.bind({});
+Test.args = Content;
