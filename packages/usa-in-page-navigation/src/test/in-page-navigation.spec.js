233956b593adb64793d8a1dc3289b94f61b5ae44
mitchmoccia <code@3dx8.com>
2022-08-22 04:36:41 -0400

    Add unit tests for in-page navigation

--- packages/usa-in-page-navigation/src/test/in-page-navigation.spec.js
+++ packages/usa-in-page-navigation/src/test/in-page-navigation.spec.js
@@ -2,18 +2,17 @@ const assert = require("assert");
 const fs = require("fs");
 const path = require("path");
 const sinon = require("sinon");
-const { default: matchMediaPolyfill } = require("mq-polyfill");
 const behavior = require("../index");
 
-const HIDE_MAX_WIDTH = 768;
+const HIDE_MAX_WIDTH = 639;
 const TEMPLATE = fs.readFileSync(path.join(__dirname, "/template.html"));
 const STYLES = fs.readFileSync(
   `${__dirname}/../../../../dist/css/uswds.min.css`
 );
-
+const THE_NAV = ".usa-in-page-nav";
 const PRIMARY_CONTENT_SELECTOR =
   ".usa-in-page-nav-container .usa-in-page-nav .usa-in-page-nav-list";
-const IN_PAGE_NAV_SELECTOR = ".usa-in-page-nav__link";
+// const IN_PAGE_NAV_SELECTOR = ".usa-in-page-nav__link";
 
 /**
  * Resize the window's width, then dispatch a 'resize' event
@@ -28,7 +27,6 @@ function resizeTo(width) {
 }
 
 const assertHidden = (el, hidden) => {
-  console.log(">> element:", el);
   assert.strictEqual(
     window.getComputedStyle(el).display === "none",
     hidden,
@@ -45,67 +43,49 @@ const tests = [
 ];
 
 tests.forEach(({ name, selector: containerSelector }) => {
-  describe.only(`in page navigation initialized at ${name}`, () => {
+  describe.only(`in-page navigation initialized at ${name}`, () => {
     const { body } = document;
-    // const windowIntersectionObserver = window.IntersectionObserver;
-
     document.head.insertAdjacentHTML("beforeend", `<style>${STYLES}</style>`);
 
-    let theLinks;
+    let theNav;
+    // let theLinks;
     let theList;
-    let sandbox;
 
     before(() => {
       const observe = sinon.spy();
       const mockIntersectionObserver = sinon.stub().returns({ observe });
-      // mockIntersectionObserver.mockReturnValue({
-      //   observe,
-      // });
       window.IntersectionObserver = mockIntersectionObserver;
-      matchMediaPolyfill(window);
     });
 
     beforeEach(() => {
       body.innerHTML = TEMPLATE;
 
+      theNav = document.querySelector(THE_NAV);
       theList = document.querySelector(PRIMARY_CONTENT_SELECTOR);
-      theLinks = () => document.querySelectorAll(IN_PAGE_NAV_SELECTOR);
+      // theLinks = () => document.querySelectorAll(IN_PAGE_NAV_SELECTOR);
 
-      window.innerWidth = 400;
+      window.innerWidth = 1024;
       behavior.on(containerSelector());
     });
 
     afterEach(() => {
       behavior.off(containerSelector(body));
       body.innerHTML = "";
-      // window.IntersectionObserver = windowIntersectionObserver;
+    });
+
+    it("defines a max width", () => {
+      assert(typeof HIDE_MAX_WIDTH === "number", "no value defined");
     });
 
     it("hides at small screens", () => {
       resizeTo(400);
-
-      assertHidden(theList, true);
+      assertHidden(theNav, true);
     });
 
-    /* it("defines a max width", () => {
-      assert(typeof HIDE_MAX_WIDTH === "number", "no value defined");
-    });
-
-     it("hides at small screens", () => {
-      resizeTo(400);
-      assertHidden(theList[0], true);
-    });
-
-    it("hides then shows again on larger screens", () => {
+    it("show on larger screens", () => {
       resizeTo(400);
       resizeTo(1024);
-      assertHidden(theList[0], false);
+      assertHidden(theList, false);
     });
-
-    it("navigates to section when clicked", () => {
-      resizeTo(400);
-      theLinks()[0].click();
-      assertHidden(theList[0], false);
-    }); */
   });
 });
