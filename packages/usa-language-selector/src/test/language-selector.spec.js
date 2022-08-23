31d0da36289c05209e886b913e0ca6d6555368b4
mitchmoccia <code@3dx8.com>
2022-08-23 17:11:58 -0400

    Add unstyled language selector plus write unit tests

--- packages/usa-language-selector/src/test/language-selector.spec.js
+++ packages/usa-language-selector/src/test/language-selector.spec.js
@@ -1,46 +1,91 @@
 const assert = require("assert");
 const fs = require("fs");
 const path = require("path");
-const skipnav = require("../index");
+const LanguageSelector = require("../index");
 
 const TEMPLATE = fs.readFileSync(path.join(__dirname, "/template.html"));
 
-describe("skip nav link", () => {
+const EVENTS = {
+  escape(el) {
+    const escapeKeyEvent = new KeyboardEvent("keydown", {
+      key: "Escape",
+      bubbles: true,
+    });
+
+    el.dispatchEvent(escapeKeyEvent);
+  },
+  focusOut(el) {
+    const focusOutEvent = new Event("focusout", {
+      bubbles: true,
+      cancelable: true,
+    });
+
+    el.dispatchEvent(focusOutEvent);
+  },
+};
+
+describe("language selector component", () => {
   const { body } = document;
 
-  let skipLink;
-  let topLink;
-  let main;
+  let language;
+  let languageList;
+  let languageButton;
+  let languageLink;
 
   beforeEach(() => {
     body.innerHTML = TEMPLATE;
-    skipLink = body.querySelector(".usa-skipnav");
-    topLink = body.querySelector(".usa-footer__return-to-top a");
-    main = body.querySelector("main");
-    skipnav.on(body);
+    language = body.querySelector(".usa-language-container");
+    languageList = body.querySelector(".usa-language__submenu");
+    languageButton = body.querySelector(".usa-language__link");
+    languageLink = language.querySelector("a");
+    LanguageSelector.on(body);
   });
 
   afterEach(() => {
     body.innerHTML = "";
-    skipnav.off(body);
+    LanguageSelector.off(body);
   });
 
-  it('skipnav link sets tabindex="0" when clicked', () => {
-    skipLink.click();
-    assert.strictEqual(main.getAttribute("tabindex"), "0");
+  it("shows the language dropdown when the language button is clicked", () => {
+    const languageMenu = body.querySelector("#language-options");
+
+    languageButton.click();
+    assert.strictEqual(languageMenu.getAttribute("hidden"), null);
   });
 
-  it('skipnav link sets tabindex="-1" when blurred', () => {
-    skipLink.click();
-    assert.strictEqual(main.getAttribute("tabindex"), "0");
+  it("hides the visible language menu when the body is clicked", () => {
+    const languageMenu = body.querySelector("#language-options");
 
-    main.focus(); // XXX jsdom doesn't do this for us
-    main.blur();
-    assert.strictEqual(main.getAttribute("tabindex"), "-1");
+    languageButton.click();
+    assert.strictEqual(languageMenu.getAttribute("hidden"), null);
+    body.click();
+    assert.strictEqual(languageMenu.hasAttribute("hidden"), true);
   });
 
-  it('return to top link sets tabindex="0" when clicked', () => {
-    topLink.click();
-    assert.strictEqual(main.getAttribute("tabindex"), "0");
+  it("collapses dropdown when a language link is clicked", () => {
+    languageButton.click();
+    languageLink.click();
+    assert.strictEqual(languageButton.getAttribute("aria-expanded"), "false");
+  });
+
+  it("collapses dropdown when the Escape key is hit", () => {
+    languageButton.click();
+    EVENTS.escape(languageButton);
+    assert.strictEqual(languageButton.getAttribute("aria-expanded"), "false");
+  });
+
+  it("contains a role of button", () => {
+    assert.strictEqual(languageButton.getAttribute("role"), "button");
+  });
+
+  it("contains aria-controls of language-options", () => {
+    assert.strictEqual(
+      languageButton.getAttribute("aria-controls"),
+      "language-options"
+    );
+  });
+
+  it("contains an id of language-options", () => {
+    assert.strictEqual(languageList.getAttribute("id"), "language-options");
   });
 });
--- packages/usa-language-selector/src/usa-language-selector.stories.js
+++ packages/usa-language-selector/src/usa-language-selector.stories.js
@@ -1,9 +1,11 @@
 import Component from "./usa-language-selector.twig";
 import ComponentMultiple from "./usa-language-selector--multiple/usa-language-selector--multiple.twig";
 import ComponentHeader from "./usa-language-selector--header/usa-language-selector--header.twig";
+import UnstyledHeader from "./usa-language-selector--unstyled/usa-language-selector--unstyled.twig";
 import DefaultContent from "./usa-language-selector.json";
 import MultipleContent from "./usa-language-selector--multiple/usa-language-selector--multiple.json";
 import HeaderContent from "./usa-language-selector--header/usa-language-selector--header.json";
+import UnstyledContent from "./usa-language-selector--unstyled/usa-language-selector--unstyled.json";
 
 export default {
   title: "Components/Language Selector",
@@ -12,6 +14,7 @@ export default {
 const Template = (args) => Component(args);
 const MultipleTemplate = (args) => ComponentMultiple(args);
 const HeaderTemplate = (args) => ComponentHeader(args);
+const UnstyledTemplate = (args) => UnstyledHeader(args);
 
 export const TwoLanguages = Template.bind({});
 TwoLanguages.args = DefaultContent;
@@ -21,3 +24,6 @@ ThreeOrMoreLanguages.args = MultipleContent;
 
 export const InHeaderExample = HeaderTemplate.bind({});
 InHeaderExample.args = HeaderContent;
+
+export const UnstyledExample = UnstyledTemplate.bind({});
+UnstyledExample.args = UnstyledContent;
