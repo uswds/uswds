cdf721bced2ac42f8fec02afa99bbf62b9c9ca8e
mitchmoccia <code@3dx8.com>
2022-08-29 22:27:07 -0400

    New version of the input mask component

--- packages/usa-input-mask/src/index.js
+++ packages/usa-input-mask/src/index.js
@@ -1,133 +1,219 @@
+const selectOrMatches = require("../../uswds-core/src/js/utils/select-or-matches");
 const behavior = require("../../uswds-core/src/js/utils/behavior");
 const { prefix: PREFIX } = require("../../uswds-core/src/js/config");
 const { CLICK } = require("../../uswds-core/src/js/events");
 
-const MASKED = `${PREFIX}-masked`;
-const MASKED_CLASS = `.${MASKED}`;
-const MASK = `${PREFIX}-input-mask`;
-const MASK_CONTENT = `${MASK}--content`;
-const PLACEHOLDER = "placeholder";
+const MASKED_CLASS = `${PREFIX}-masked`;
+const MASKED = `.${MASKED_CLASS}`;
+const MASK_CHAR = "_";
 
-// User defined Values
-// const maskedInputs = document.getElementsByClassName(MASKED_CLASS);
-const maskedNumber = "XdDmMyY9";
-const maskedLetter = "_";
+const DATA_MASK_ATTRIBUTE = "data-mask";
+const DATA_MASK_START_ATTRIBUTE = "data-mask-start";
 
-// replaces each masked input with a shall containing the input and it's mask.
-const createShell = (input) => {
-  const placeholder = input.getAttribute(`${PLACEHOLDER}`);
-  if (placeholder) {
-    input.setAttribute("maxlength", placeholder.length);
-    input.setAttribute("data-placeholder", placeholder);
-    input.removeAttribute(`${PLACEHOLDER}`);
-  } else {
-    return;
+const KEY_DELETE = "Delete";
+const KEY_BACKSPACE = "Backspace";
+const KEY_ARROW_UP = "ArrowUp";
+const KEY_ARROW_DOWN = "ArrowDown";
+const KEY_ARROW_LEFT = "ArrowLeft";
+const KEY_ARROW_RIGHT = "ArrowRight";
+const KEY_TAB = "Tab";
+
+/**
+ * Check if a character is a number
+ *
+ * @param {number} theNum the key to check
+ */
+const isNum = (theNum) =>
+  !Number.isNaN(theNum) && !Number.isNaN(parseInt(theNum, 10));
+
+/**
+ * Check if the key is a delete or backspace key
+ *
+ * @param {string} theKey the key to check
+ */
+const isDeletionKey = (theKey) =>
+  theKey === KEY_DELETE || theKey === KEY_BACKSPACE;
+
+/**
+ * Check if the key is a navigation key
+ *
+ * @param {string} theKey the key to check
+ */
+const isDirectionKey = (theKey) =>
+  theKey === KEY_ARROW_UP ||
+  theKey === KEY_ARROW_DOWN ||
+  theKey === KEY_ARROW_LEFT ||
+  theKey === KEY_ARROW_RIGHT ||
+  theKey === KEY_TAB;
+
+const isPlaceholder = (theMask) => theMask === MASK_CHAR;
+
+/**
+ * Find the first position of the mask character in a string.
+ *
+ * @param {HTMLElement} inputEl an input element
+ * @param {string} charReplace the character to replace the position with
+ */
+const placeholderPosition = (inputEl, charReplace) =>
+  inputEl.value.indexOf(charReplace);
+
+/**
+ * Set the max length to the length of the mask
+ *
+ * @param {HTMLElement} inputEl an input element
+ * @param {string} theMask the mask to set the max length to
+ */
+const setMaxLength = (inputEl, theMask) => {
+  const currentElement = inputEl;
+  currentElement.maxLength = theMask.length;
+};
+
+/**
+ * Set the value of the input element to the mask
+ *
+ * @param {HTMLElement} inputEl an input element
+ * @param {string} theMask the mask to set the value to
+ */
+const setInputValue = (inputEl, theMask) => {
+  const currentElement = inputEl;
+  currentElement.value = theMask;
+};
+
+/**
+ * Set the character start position based on the mask pattern
+ *
+ * @param {HTMLElement} inputEl an input element
+ */
+const setStartPosition = (inputEl) => {
+  const start = placeholderPosition(inputEl, MASK_CHAR) - 1;
+  inputEl.setAttribute(DATA_MASK_START_ATTRIBUTE, start);
+};
+
+/**
+ * Set up the mask on an input element
+ *
+ * @param {HTMLElement} inputEl an input element
+ * @param {string} theMask the mask to set the value to
+ */
+const setUpMasks = (inputEl, theMask) => {
+  setMaxLength(inputEl, theMask);
+  setInputValue(inputEl, theMask);
+  setStartPosition(inputEl, theMask);
+};
+
+/**
+ * Handle the focus event
+ *
+ * @param {HTMLElement} el an input element
+ */
+const focused = (theEl) => {
+  const currentElement = theEl;
+  const placeholderPos = placeholderPosition(currentElement, MASK_CHAR);
+  currentElement.selectionStart = placeholderPos;
+  currentElement.selectionEnd = placeholderPos;
+};
+
+/**
+ * Replace a character at a given position in a string
+ *
+ * @param {string} theMask the mask to set the value to
+ * @param {number} thePosition the position to replace the character at
+ * @param {string} charTyped the character to replace the position with
+ * @returns {string} the html string
+ */
+const replaceAt = (theMask, thePosition, charTyped) => {
+  let thePos = thePosition;
+  const theReplacement =
+    theMask.substring(0, thePos) + charTyped + theMask.substring((thePos += 1));
+  return theReplacement;
+};
+
+/**
+ * Change the character at a given position in a string
+ *
+ * @param {HTMLElement} el an input element
+ * @param {string} charTyped the character to replace the position with
+ * @param {number} maskLength The length of the mask
+ * @param {number} forwardOrBack The direction of the cursor in the mask
+ */
+const changeChar = (el, charTyped, maskLength, forwardOrBack = 1) => {
+  const theEl = el;
+  const theValue = theEl.value;
+  let currentPosition;
+  currentPosition =
+    forwardOrBack > 0 ? theEl.selectionStart : theEl.selectionStart - 1;
+  let currentValue = "";
+  if (currentPosition === maskLength) {
+    return false;
   }
-
-  const shell = document.createElement("span");
-  shell.classList.add(MASK);
-  shell.setAttribute("data-mask", placeholder);
-  console.log("shell ", shell); // eslint-disable-line no-console
-
-  const content = document.createElement("span");
-  content.classList.add(MASK_CONTENT);
-  content.setAttribute("aria-hidden", "true");
-  content.id = `${input.id}Mask`;
-  content.textContent = placeholder;
-
-  shell.appendChild(content);
-  input.parentNode.insertBefore(shell, input);
-  shell.appendChild(input);
-};
-
-const setUpMasks = (inputs) => {
-  for (let i = 0; i < inputs.length; i += 1) {
-    createShell(inputs[i]);
+  if (
+    !isNum(theValue[currentPosition]) &&
+    !isPlaceholder(theValue[currentPosition])
+  ) {
+    do {
+      currentPosition += forwardOrBack;
+      if (currentPosition === maskLength) {
+        return false;
+      }
+    } while (
+      !isNum(theValue[currentPosition]) &&
+      !isPlaceholder(theValue[currentPosition])
+    );
   }
+  currentValue = replaceAt(theValue, currentPosition, charTyped);
+  theEl.value = currentValue;
+  if (forwardOrBack > 0) {
+    currentPosition += forwardOrBack;
+  }
+  theEl.selectionStart = currentPosition;
+  theEl.selectionEnd = currentPosition;
+
+  return null;
 };
 
-const setValueOfMask = (el) => {
-  const { value } = el;
-  const placeholder = el.getAttribute("data-placeholder");
-  const placeholderVal = `${placeholder.substr(value.length)}`;
-
-  const theIEl = document.createElement("i");
-  theIEl.textContent = value;
-  return [theIEl, placeholderVal];
-};
-
-const handleCurrentValue = (el) => {
-  const isCharsetPresent = el.getAttribute("data-charset");
-  const placeholder = isCharsetPresent || el.getAttribute("data-placeholder");
-  const { value } = el;
-  const l = placeholder.length;
-  let newValue = "";
-  let i;
-  let j;
-  let isInt;
-  let isLetter;
-
-  // strip special characters
-  const strippedValue = isCharsetPresent
-    ? value.replace(/\W/g, "")
-    : value.replace(/\D/g, "");
-
-  for (i = 0, j = 0; i < l; i += 1) {
-    // eslint-disable-next-line no-multi-assign, no-restricted-globals
-    isInt = !isNaN(parseInt(strippedValue[j], 10));
-    isLetter = strippedValue[j] ? strippedValue[j].match(/[A-Z]/i) : false;
-    const matchesNumber = maskedNumber.indexOf(placeholder[i]) >= 0;
-    const matchesLetter = maskedLetter.indexOf(placeholder[i]) >= 0;
-
-    if (
-      (matchesNumber && isInt) ||
-      (isCharsetPresent && matchesLetter && isLetter)
-    ) {
-      newValue += strippedValue[j++];
-    } else if (
-      (!isCharsetPresent && !isInt && matchesNumber) ||
-      (isCharsetPresent &&
-        ((matchesLetter && !isLetter) || (matchesNumber && !isInt)))
-    ) {
-      // errorOnKeyEntry(); // write your own error handling function
-      return newValue;
+/**
+ * Handle the typed key event
+ *
+ * @param {KeyboardEvent} event the key event
+ * @param {HTMLElement} el an input element
+ */
+const input = (event, el) => {
+  const theMask = el.getAttribute(DATA_MASK_ATTRIBUTE);
+  const theStart = el.getAttribute(DATA_MASK_START_ATTRIBUTE);
+  if ((isDirectionKey(event.key) || event.preventDefault(), isNum(event.key))) {
+    changeChar(el, event.key, theMask.length);
+  } else if (isDeletionKey(event.key)) {
+    if (event.key === KEY_BACKSPACE) {
+      changeChar(el, MASK_CHAR, parseInt(theStart, 10), -1);
     } else {
-      newValue += placeholder[i];
-    }
-    // break if no characters left and the pattern is non-special character
-    if (strippedValue[j] === undefined) {
-      break;
+      changeChar(el, MASK_CHAR, theMask.length);
     }
   }
-  if (el.getAttribute("data-valid-example")) {
-    // return validateProgress(e, newValue);
-  }
-  return newValue;
-};
-const handleValueChange = (el) => {
-  const id = el.getAttribute("id");
-
-  document.getElementById(id).value = handleCurrentValue(el);
-  const maskVal = setValueOfMask(el);
-  const maskEl = document.getElementById(`${id}Mask`);
-  maskEl.innerHTML = "";
-  maskEl.replaceChildren(maskVal[0], maskVal[1]);
 };
 
 const inputMaskEvents = {
   [CLICK]: {},
-
+  focusin: {
+    [MASKED]() {
+      focused(this);
+    },
+  },
   keydown: {
-    [MASKED_CLASS]() {
-      handleValueChange(this);
+    [MASKED](event) {
+      input(event, this);
     },
   },
 };
 
 const inputMask = behavior(inputMaskEvents, {
-  init() {
-    const maskedInputs = document.querySelectorAll(MASKED_CLASS);
-    setUpMasks(maskedInputs);
+  init(root) {
+    selectOrMatches(MASKED, root).forEach((maskedInput) => {
+      setUpMasks(
+        maskedInput,
+        maskedInput.getAttribute(DATA_MASK_ATTRIBUTE),
+        MASK_CHAR
+      );
+    });
   },
 });
 
