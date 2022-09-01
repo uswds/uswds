9ad9435414cbb7c7c6ab0412ee2b8d503ccd17b1
mitchmoccia <code@3dx8.com>
2022-08-31 21:36:17 -0400

    Adjust languages in dropdown and add title attribute with native and English languages

--- packages/usa-language-selector/src/usa-language-selector.stories.js
+++ packages/usa-language-selector/src/usa-language-selector.stories.js
@@ -1,9 +1,9 @@
 import Component from "./usa-language-selector.twig";
-import ComponentMultiple from "./usa-language-selector--multiple/usa-language-selector--multiple.twig";
+import ComponentThreeOrMore from "./usa-language-selector--three-or-more/usa-language-selector--three-or-more.twig";
 import ComponentHeader from "./usa-language-selector--header/usa-language-selector--header.twig";
 import UnstyledHeader from "./usa-language-selector--unstyled/usa-language-selector--unstyled.twig";
 import DefaultContent from "./usa-language-selector.json";
-import MultipleContent from "./usa-language-selector--multiple/usa-language-selector--multiple.json";
+import ThreeOrMoreContent from "./usa-language-selector--three-or-more/usa-language-selector--three-or-more.json";
 import HeaderContent from "./usa-language-selector--header/usa-language-selector--header.json";
 import UnstyledContent from "./usa-language-selector--unstyled/usa-language-selector--unstyled.json";
 
@@ -12,15 +12,15 @@ export default {
 };
 
 const Template = (args) => Component(args);
-const MultipleTemplate = (args) => ComponentMultiple(args);
+const ThreeOrMoreTemplate = (args) => ComponentThreeOrMore(args);
 const HeaderTemplate = (args) => ComponentHeader(args);
 const UnstyledTemplate = (args) => UnstyledHeader(args);
 
 export const TwoLanguages = Template.bind({});
 TwoLanguages.args = DefaultContent;
 
-export const ThreeOrMoreLanguages = MultipleTemplate.bind({});
-ThreeOrMoreLanguages.args = MultipleContent;
+export const ThreeOrMoreLanguages = ThreeOrMoreTemplate.bind({});
+ThreeOrMoreLanguages.args = ThreeOrMoreContent;
 
 export const InHeaderExample = HeaderTemplate.bind({});
 InHeaderExample.args = HeaderContent;
