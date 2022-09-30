f98c7540a6786ee1d72eb254854e467d6cfdd589
James Mejia <james.mejia@gsa.gov>
2022-09-30 16:24:05 -0500

    Add JSDoc comments to debounce utility

--- packages/uswds-core/src/js/utils/debounce.js
+++ packages/uswds-core/src/js/utils/debounce.js
@@ -1,4 +1,12 @@
-module.exports = function(callback, delay = 500) {
+/**
+ * Call a function every X amount of milliseconds.
+ *
+ * @param  {Function} callback - A callback function to be debounced
+ * @param  {number} delay - Milliseconds to wait before calling function
+ * @returns {Function} A debounced function
+ * @example const updateStatus = debounce((string) => console.log(string), 2000)
+ */
+module.exports = function debounce(callback, delay = 500) {
   let timer = null;
   return (...args) => {
     window.clearTimeout(timer);
@@ -6,4 +14,4 @@ module.exports = function(callback, delay = 500) {
       callback.apply(this, args);
     }, delay);
   };
-}
+};
