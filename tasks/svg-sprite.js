8dea48294744a81fe8d83108df437526d05e6f1c
James Mejia <james.mejia@gsa.gov>
2022-10-04 16:08:45 -0500

    Add changes from be48bfe

--- tasks/svg-sprite.js
+++ tasks/svg-sprite.js
@@ -10,6 +10,41 @@ const iconConfig = require("../packages/usa-icon/src/usa-icons.config");
 
 const svgPath = "dist/img";
 
+// More complex configuration example
+const config = {
+  shape: {
+    dimension: {
+      // Set maximum dimensions
+      maxWidth: 24,
+      maxHeight: 24,
+    },
+    id: {
+      separator: "-",
+    },
+    spacing: {
+      // Add padding
+      padding: 0,
+    },
+  },
+  mode: {
+    symbol: true,
+  },
+};
+
+function cleanIcons() {
+  return del(`${svgPath}/usa-icons`);
+}
+
+function collectIcons() {
+  dutil.logMessage("collectIcons", "Collecting default icon set in dist/img/usa-icons");
+  return src([
+    `node_modules/@material-design-icons/svg/filled/{${iconConfig.material}}.svg`,
+    `packages/usa-icon/src/img/material-icons-deprecated/{${iconConfig.materialDeprecated}}.svg`,
+    `packages/usa-icon/src/img/uswds-icons/{${iconConfig.uswds}}.svg`,
+  ])
+    .pipe(dest(`${svgPath}/usa-icons`))
+}
+
 function buildSprite(done) {
   return (
     src(`${svgPath}/usa-icons/*.svg`)
