a428b31a5cff4be7d75b4fd5b5fc2d24d40ae57b
mitchmoccia <code@3dx8.com>
2022-10-22 11:23:10 -0400

    Separate input mask examples into individual files and update unit tests

---
+++ packages/usa-input-mask/src/content/index.js
@@ -0,0 +1,3 @@
+export { default as SsnContent } from "./usa-input-mask-ssn.json";
+export { default as PhoneContent } from "./usa-input-mask-phone.json";
+export { default as ZipContent } from "./usa-input-mask-zip.json";
--- packages/usa-input-mask/src/usa-input-mask.stories.js
+++ packages/usa-input-mask/src/usa-input-mask.stories.js
@@ -1,4 +1,9 @@
 import Component from "./usa-input-mask.twig";
+import {
+  SsnContent,
+  PhoneContent,
+  ZipContent,
+} from "./content";
 
 export default {
   title: "Components/Form Inputs/Text Input Mask",
@@ -6,4 +11,11 @@ export default {
 
 const Template = (args) => Component(args);
 
-export const InputMask = Template.bind({});
+export const SSN = Template.bind({});
+SSN.args = SsnContent;
+
+export const Phone = Template.bind({});
+Phone.args = PhoneContent;
+
+export const ZIP = Template.bind({});
+ZIP.args = ZipContent;
