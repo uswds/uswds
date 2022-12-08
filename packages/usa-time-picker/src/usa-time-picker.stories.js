3d46e9552f456deb393b04787de14155cf5c8b21
Charlie Mahoney <charlie.mahoneyy@gmail.com>
2022-12-08 14:34:32 -0500

    Time Picker: Add disabled example

--- packages/usa-time-picker/src/usa-time-picker.stories.js
+++ packages/usa-time-picker/src/usa-time-picker.stories.js
@@ -1,4 +1,5 @@
 import Component from "./usa-time-picker.twig";
+import Disabled from "./usa-time-picker~disabled.json";
 
 export default {
   title: "Components/Form Inputs/Time Picker",
@@ -7,7 +8,11 @@ export default {
 const Template = (args) => Component(args);
 
 export const TimePicker = Template.bind({});
+
 export const TimePickerDefaultValue = Template.bind({});
 TimePickerDefaultValue.args = {
   defaultValue: "1:00pm"
 };
+
+export const TimePickerDisabled = Template.bind({});
+TimePickerDisabled.args = Disabled;
