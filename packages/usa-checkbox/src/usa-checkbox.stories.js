9f13406e9f61d7efe1495c65877527df287be629
James Mejia <james.mejia@gsa.gov>
2022-12-20 09:18:29 -0600

    Update checkbox stories
    - Simplify controls for disabled/aria-disabled
    - Remove previous test (controls now handle that test)

--- packages/usa-checkbox/src/usa-checkbox.stories.js
+++ packages/usa-checkbox/src/usa-checkbox.stories.js
@@ -1,32 +1,16 @@
 import Component from "./usa-checkbox.twig";
 import Tile from "./usa-checkbox--tile.twig";
-import TestComponent from "./test/test-patterns/test-usa-checkbox.twig";
 
 export default {
   title: "Components/Form Inputs/Checkbox",
-  argTypes: {
-    disabled: {
-      control: {type: 'boolean'},
-      defaultValue: false,
-      name: "disabled"
-    },
-    aria_disabled: {
-      control: {type: 'boolean'},
-      defaultValue: false,
-      name: "aria-disabled",
-    }
+  args: {
+    disabled: false,
+    aria_disabled: false
   }
 };
 
 const Template = (args) => Component(args);
 const TileTemplate = (args) => Tile(args);
-const TestTemplate = (args) => TestComponent(args);
 
 export const Default = Template.bind({});
 export const CheckboxTile = TileTemplate.bind({});
-export const Test = TestTemplate.bind({});
-Test.argTypes = {
-  aria_disabled: {
-    table: { disable: true },
-  },
-}
