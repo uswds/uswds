b1ca9f0601641496b338f02e974b2fbe75123453
James Mejia <james.mejia@gsa.gov>
2022-12-20 09:09:42 -0600

    Consolidate character count controls
    There's not much to the component, so consolidating controls.

--- packages/usa-character-count/src/usa-character-count.stories.js
+++ packages/usa-character-count/src/usa-character-count.stories.js
@@ -1,11 +1,13 @@
 import Component from "./usa-character-count.twig";
-import disabledOption from "./index.json";
 
 export default {
   title: "Components/Form Inputs/Character Count",
+  args: {
+    disabled: false,
+    aria_disabled: false
+  }
 };
 
 const Template = (args) => Component(args);
 
 export const CharacterCount = Template.bind({});
-CharacterCount.args = disabledOption;
