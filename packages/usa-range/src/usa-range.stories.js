50f287c95f7a67eaa014099df3d2e27682721456
Charlie Mahoney <charlie.mahoneyy@gmail.com>
2022-12-27 12:29:07 -0500

    Normalize usa-range disabled options

--- packages/usa-range/src/usa-range.stories.js
+++ packages/usa-range/src/usa-range.stories.js
@@ -1,11 +1,16 @@
 import Component from "./usa-range.twig";
-import Default from "./usa-range.json";
 
 export default {
   title: "Components/Form Inputs/Range",
+  argTypes: {
+    disabled_state: {
+      name: "Disabled state",
+      control: { type: "radio" },
+      options: ["none", "disabled", "aria_disabled"],
+    },
+  }
 };
 
 const Template = (args) => Component(args);
 
 export const Range = Template.bind({});
-Range.args = Default;
