4f12d4e8347d496fc309761030152b5dd9fdec1c
James Mejia <james.mejia@gsa.gov>
2022-12-28 11:58:54 -0600

    Convert flags to esm

--- tasks/flags.js
+++ tasks/flags.js
@@ -1,19 +1,19 @@
-// import dutil from "./utils/doc-util";
-// import cFlags from "./utils/cflags";
+import dutil from "./utils/doc-util";
+import cFlags from "./utils/cflags";
 
-// export default {
-//   noTest(done) {
-//     dutil.logMessage("no-test", "Disabling linting and tests for all assets.");
-//     cFlags.test = false;
-//     done();
-//   },
+export default {
+  noTest(done) {
+    dutil.logMessage("no-test", "Disabling linting and tests for all assets.");
+    cFlags.test = false;
+    done();
+  },
 
-//   noCleanup(done) {
-//     dutil.logMessage(
-//       "no-cleanup",
-//       "Disabling cleanup of distribution directories."
-//     );
-//     cFlags.cleanup = false;
-//     done();
-//   }
-// }
+  noCleanup(done) {
+    dutil.logMessage(
+      "no-cleanup",
+      "Disabling cleanup of distribution directories."
+    );
+    cFlags.cleanup = false;
+    done();
+  }
+}
