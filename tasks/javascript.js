8da0522f4407ebb2f9cbe773b7ca8de43adca733
James Mejia <james.mejia@gsa.gov>
2022-12-28 11:56:27 -0600

    Convert JS tasks to ESM
    Replace browserify (CJS) with gulp-esbuild (ESM) for bundling

--- tasks/javascript.js
+++ tasks/javascript.js
@@ -1,65 +1,69 @@
-import { dest, src } from "gulp";
-import buffer from "vinyl-buffer";
-import browserify from "browserify";
+import gulp from "gulp";
+import { createGulpEsbuild } from "gulp-esbuild";
 import childProcess from "child_process";
 import rename from "gulp-rename";
-import source from "vinyl-source-stream";
-import sourcemaps from "gulp-sourcemaps";
 import uglify from "gulp-uglify";
 import merge from "merge-stream";
-import dutil from "./utils/doc-util";
+import sourcemaps from "gulp-sourcemaps";
+import dutil from "./utils/doc-util.js";
 
-export default {
-  compileJS() {
-    dutil.logMessage("javascript", "Compiling JavaScript");
-    const packageName = dutil.pkg.name.replace("@uswds/", "");
-    const streams = Object.entries({
-      [packageName]: browserify({
-        entries: ["packages/uswds-core/src/js/start.js"],
-        debug: true,
+const { src, dest } = gulp;
+const gulpEsbuild = createGulpEsbuild({
+  piping: true,
+});
+
+/**
+ * Builds minified and unminified versions of uswds-init.js and uswds.js with sourcemaps
+ */
+function compileJS() {
+  const packageName = dutil.pkg.name.replace("@uswds/", "");
+
+  dutil.logMessage("javascript", "Compiling JavaScript");
+
+  const streams = Object.entries({
+    [packageName]: src("./packages/uswds-core/src/js/start.js").pipe(
+      gulpEsbuild({
+        bundle: true,
+        target: "es6",
       })
-        .transform("babelify", {
-          global: true,
-          presets: ["@babel/preset-env"],
+    ),
+    "uswds-init": src("packages/uswds-core/src/js/uswds-init.js"),
+  }).map(([basename, stream]) =>
+    stream
+      .pipe(rename({ basename }))
+      .pipe(dest("dist/js"))
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .on("error", function handleError(error) {
+        dutil.logError(error);
+        this.emit("end");
+      })
+      .pipe(uglify())
+      .pipe(
+        rename({
+          suffix: ".min",
         })
-        .bundle()
-        .pipe(source(`${packageName}.js`))
-        .pipe(buffer()),
-      "uswds-init": src("packages/uswds-core/src/js/uswds-init.js"),
-    }).map(([basename, stream]) =>
-      stream
-        .pipe(rename({ basename }))
-        .pipe(dest("dist/js"))
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .on("error", function handleError(error) {
-          dutil.logError(error);
-          this.emit('end');
-        })
-        .pipe(uglify())
-        .pipe(
-          rename({
-            suffix: ".min",
-          })
-        )
-        .pipe(sourcemaps.write("."))
-        .pipe(dest("dist/js"))
-    );
+      )
+      .pipe(sourcemaps.write("."))
+      .pipe(dest("dist/js"))
+  );
 
-    return merge(streams);
-  },
-  typeCheck() {
-    return new Promise((resolve, reject) => {
-      childProcess
-        .spawn("./node_modules/.bin/tsc", { stdio: "inherit" })
-        .on("error", reject)
-        .on("exit", (code) => {
-          if (code === 0) {
-            dutil.logMessage("typecheck", "TypeScript likes our code!");
-            resolve();
-          } else {
-            reject(new Error("TypeScript failed, see output for details!"));
-          }
-        });
-    });
-  },
-};
+  return merge(streams);
+}
+
+function typeCheck() {
+  return new Promise((resolve, reject) => {
+    childProcess
+      .spawn("./node_modules/.bin/tsc", { stdio: "inherit" })
+      .on("error", reject)
+      .on("exit", (code) => {
+        if (code === 0) {
+          dutil.logMessage("typecheck", "TypeScript likes our code!");
+          resolve();
+        } else {
+          reject(new Error("TypeScript failed, see output for details!"));
+        }
+      });
+  });
+}
+
+export { compileJS, typeCheck };
