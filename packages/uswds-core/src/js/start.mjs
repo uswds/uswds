05290fba63cfbcbe08720290755e68f8d8f8beb1
James Mejia <james.mejia@gsa.gov>
2022-12-30 10:31:01 -0600

    Core JS use ESM
    - Use "mjs" extension to prevent node errors
    - Update entrypoint references
    - Create new USWDS object in start.js to keep config immutable
    - Update sass test references

--- packages/uswds-core/src/js/start.mjs
+++ packages/uswds-core/src/js/start.mjs
@@ -1,19 +1,22 @@
-window.uswdsPresent = true; // GLOBAL variable to indicate that the uswds.js has loaded in the DOM.
-
 /**
  * The 'polyfills' define key ECMAScript 5 methods that may be missing from
  * older browsers, so must be loaded first.
  */
-require("./polyfills");
+import "./polyfills";
+import svg4everybody from "./polyfills/svg4everybody";
 
-const uswds = require("./config");
+import { prefix } from "./config";
+import components from "./index";
 
-const components = require("./index");
-const svg4everybody = require("./polyfills/svg4everybody");
+const uswds = {
+  prefix,
+  components,
+};
 
+window.uswdsPresent = true; // GLOBAL variable to indicate that the uswds.js has loaded in the DOM.
 uswds.components = components;
 
-const initComponents = () => {
+export const initComponents = () => {
   const target = document.body;
   Object.keys(components).forEach((key) => {
     const behavior = components[key];
@@ -28,5 +31,4 @@ if (document.readyState === "loading") {
   initComponents();
 }
 
-exports.default = uswds;
-exports.initComponents = initComponents;
+export default uswds;
