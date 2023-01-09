687156b0f3418558f3d26662cba65c522a01ebe2
Charlie Mahoney <charlie.mahoneyy@gmail.com>
2023-01-09 14:43:43 -0500

    Prefix/Suffix: Remove error example and add disabled stories

--- packages/usa-input-prefix-suffix/src/usa-input-prefix-suffix.stories.js
+++ packages/usa-input-prefix-suffix/src/usa-input-prefix-suffix.stories.js
@@ -4,10 +4,10 @@ import suffix from "./usa-input-suffix.twig";
 export default {
   title: "Components/Form Inputs/Input Prefix or Suffix",
   argTypes: {
-    state: {
-      name: "State",
+    disabled_state: {
+      name: "Disabled State",
       control: "radio",
-      options: ["default", "error", "disabled", "aria_disabled"],
+      options: ["default", "disabled", "aria_disabled"],
       defaultValue: "default"
     }
   }
@@ -17,4 +17,29 @@ const PrefixTemplate = (args) => prefix(args);
 const SuffixTemplate = (args) => suffix(args);
 
 export const Prefix = PrefixTemplate.bind({});
+export const PrefixDisabled = PrefixTemplate.bind({});
+PrefixDisabled.argTypes = {
+  disabled_state: {
+    defaultValue: "disabled"
+  }
+}
+export const PrefixAriaDisabled = PrefixTemplate.bind({});
+PrefixAriaDisabled.argTypes = {
+  disabled_state: {
+    defaultValue: "aria_disabled"
+  }
+}
+
 export const Suffix = SuffixTemplate.bind({});
+export const SuffixDisabled = SuffixTemplate.bind({});
+SuffixDisabled.argTypes = {
+  disabled_state: {
+    defaultValue: "disabled"
+  }
+}
+export const SuffixAriaDisabled = SuffixTemplate.bind({});
+SuffixAriaDisabled.argTypes = {
+  disabled_state: {
+    defaultValue: "aria_disabled"
+  }
+}
