519d35dc77b8a7a03f9237a30d5ec59b5f06ce9c
Charlie Mahoney <charlie.mahoneyy@gmail.com>
2023-01-27 16:13:40 -0500

    Move imageId variable so that it can be referenced by both load start and end functions

--- packages/usa-file-input/src/index.js
+++ packages/usa-file-input/src/index.js
@@ -256,9 +256,11 @@ const handleChange = (e, fileInputEl, instructions, dropTarget) => {
       );
     }
 
+    let imageId;
+
     // Starts with a loading image while preview is created
     reader.onloadstart = function createLoadingImage() {
-      const imageId = createUniqueID(makeSafeForID(fileName));
+      imageId = createUniqueID(makeSafeForID(fileName));
 
       instructions.insertAdjacentHTML(
         "afterend",
@@ -270,7 +272,6 @@ const handleChange = (e, fileInputEl, instructions, dropTarget) => {
 
     // Not all files will be able to generate previews. In case this happens, we provide several types "generic previews" based on the file extension.
     reader.onloadend = function createFilePreview() {
-      const imageId = createUniqueID(makeSafeForID(fileName));
       const previewImage = document.getElementById(imageId);
       if (fileName.indexOf(".pdf") > 0) {
         previewImage.setAttribute(
