12b5991bf9bc32b2d65dcec44507ca0dd02a9e01
mitchmoccia <code@3dx8.com>
2023-02-01 20:40:20 -0500

    Vastly improve in-page nav unit test assertions

--- packages/usa-in-page-navigation/src/test/in-page-navigation.spec.js
+++ packages/usa-in-page-navigation/src/test/in-page-navigation.spec.js
@@ -9,8 +9,9 @@ const TEMPLATE = fs.readFileSync(path.join(__dirname, "/template.html"));
 const STYLES = fs.readFileSync(
   `${__dirname}/../../../../dist/css/uswds.min.css`
 );
+const THE_MAIN_CONTENT = ".usa-in-page-nav__main";
 const THE_NAV_CONTAINER = ".usa-in-page-nav__container";
-const PRIMARY_CONTENT_SELECTOR =
+const THE_NAV_LIST =
   ".usa-in-page-nav .usa-in-page-nav__container .usa-in-page-nav__list";
 
 /**
@@ -49,8 +50,9 @@ tests.forEach(({ name, selector: containerSelector }) => {
     document.head.insertAdjacentHTML("beforeend", `<style>${STYLES}</style>`);
 
     let root;
+    let theMainContent;
     let theNavContainer;
-    let theList;
+    let theNavList;
 
     before(() => {
       const observe = sinon.spy();
@@ -64,8 +66,9 @@ tests.forEach(({ name, selector: containerSelector }) => {
 
       root = inPageNavSelector();
 
+      theMainContent = root.querySelector(THE_MAIN_CONTENT);
       theNavContainer = root.querySelector(THE_NAV_CONTAINER);
-      theList = root.querySelector(PRIMARY_CONTENT_SELECTOR);
+      theNavList = root.querySelector(THE_NAV_LIST);
 
       window.innerWidth = 1024;
     });
@@ -79,15 +82,36 @@ tests.forEach(({ name, selector: containerSelector }) => {
       assert(typeof HIDE_MAX_WIDTH === "number", "no value defined");
     });
 
-    it("hides at small screens", () => {
+    it("hides on small screens", () => {
       resizeTo(400);
       assertHidden(theNavContainer, true);
     });
 
-    it("show on larger screens", () => {
+    it("shows on larger screens", () => {
       resizeTo(400);
       resizeTo(1024);
-      assertHidden(theList, false);
+      assertHidden(theNavList, false);
+    });
+
+    it("sets the in-page nav h4 title tag text to 'On this page'", () => {
+      assert.strictEqual(
+        theNavContainer.querySelector("h4").textContent,
+        "On this page"
+      );
+    });
+
+    it("checks the first in-page nav item text is 'Section 1'", () => {
+      assert.strictEqual(
+        theNavList.querySelector(".usa-in-page-nav__item").textContent,
+        "Section 1"
+      );
+    });
+
+    it("checks that the first h2 tag in the main content section contains an anchor tag with id 'section_0'", () => {
+      assert.strictEqual(
+        theMainContent.querySelector("h2 > a").id,
+        "section_0"
+      );
     });
   });
 });
