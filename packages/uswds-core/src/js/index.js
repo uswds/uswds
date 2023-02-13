1820ddca08b122e10b3eba83f5b4a0d37b999c71
Charlie Mahoney <charlie.mahoneyy@gmail.com>
2023-02-13 10:19:31 -0500

    Replace focus js with css

--- packages/uswds-core/src/js/index.js
+++ packages/uswds-core/src/js/index.js
@@ -8,7 +8,6 @@ const fileInput = require("../../../usa-file-input/src/index");
 const footer = require("../../../usa-footer/src/index");
 const inPageNavigation = require("../../../usa-in-page-navigation/src/index");
 const inputMask = require("../../../usa-input-mask/src/index");
-const inputPrefixSuffix = require("../../../usa-input-prefix-suffix/src/index");
 const languageSelector = require("../../../usa-language-selector/src/index");
 const modal = require("../../../usa-modal/src/index");
 const navigation = require("../../../usa-header/src/index");
@@ -31,7 +30,6 @@ module.exports = {
   footer,
   inPageNavigation,
   inputMask,
-  inputPrefixSuffix,
   languageSelector,
   modal,
   navigation,
