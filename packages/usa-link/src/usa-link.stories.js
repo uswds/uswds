e56e0b31df6acd8f0333d912c8016044c62c57e0
James Mejia <james.mejia@gsa.gov>
2023-03-01 09:17:20 -0600

    Create tests for screen reader

--- packages/usa-link/src/usa-link.stories.js
+++ packages/usa-link/src/usa-link.stories.js
@@ -1,9 +1,13 @@
 import Component from "./usa-link.twig";
+import TestComponent from "./test/test-patterns/test-external-sr.twig";
 
 export default {
   title: "Components/Link",
 };
 
 const Template = (args) => Component(args);
+const TestTemplate = (args) => TestComponent(args);
 
 export const Link = Template.bind({});
+
+export const Test = TestTemplate.bind({});
