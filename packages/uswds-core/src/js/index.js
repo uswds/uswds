c7af4c1edf1f4e6b231100f6fe155498e415a1cf
Charlie Mahoney <charlie.mahoneyy@gmail.com>
2023-03-02 11:06:21 -0500

    Memorable date: Remove dynamic disabled styles - This went against our disabled patterns guidance - Possible enhancement down the line

--- packages/uswds-core/src/js/index.js
+++ packages/uswds-core/src/js/index.js
@@ -9,7 +9,6 @@ const footer = require("../../../usa-footer/src/index");
 const inPageNavigation = require("../../../usa-in-page-navigation/src/index");
 const inputMask = require("../../../usa-input-mask/src/index");
 const languageSelector = require("../../../usa-language-selector/src/index");
-const memorableDate = require("../../../usa-memorable-date/src/index");
 const modal = require("../../../usa-modal/src/index");
 const navigation = require("../../../usa-header/src/index");
 const password = require("../../../_usa-password/src/index");
@@ -32,7 +31,6 @@ module.exports = {
   inPageNavigation,
   inputMask,
   languageSelector,
-  memorableDate,
   modal,
   navigation,
   password,
