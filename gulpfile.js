8b6541a9b73594104b794d696f70dabc6ea3e5ac
Dan O. Williams <daniel.williams@gsa.gov>
2023-03-07 10:13:50 -0800

    Add compileSass task for troubleshooting

--- gulpfile.js
+++ gulpfile.js
@@ -60,6 +60,7 @@ exports.test = series(
  */
 exports.buildSpriteStandalone = buildSpriteStandalone;
 exports.buildSprite = buildSprite;
+exports.compileSass = compileSass;
 exports.buildSass = series(lintSass, compileSass);
 exports.buildJS = series(typeCheck, compileJS);
 exports.buildUSWDS = build;
