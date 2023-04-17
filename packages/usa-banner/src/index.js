4550a41b2f424e20ddb2df05b987609fe9196484
James Mejia <james.mejia@gsa.gov>
2023-04-17 13:02:00 -0500

    Use accordion JS in banner

--- packages/usa-banner/src/index.js
+++ packages/usa-banner/src/index.js
@@ -1,17 +1,30 @@
 const behavior = require("../../uswds-core/src/js/utils/behavior");
 const { CLICK } = require("../../uswds-core/src/js/events");
 const { prefix: PREFIX } = require("../../uswds-core/src/js/config");
+// const select = require("../../uswds-core/src/js/utils/select");
+const accordion = require("../../usa-accordion/src/index");
 
 const HEADER = `.${PREFIX}-banner__header`;
 const EXPANDED_CLASS = `${PREFIX}-banner__header--expanded`;
+// const EXPANDED = "aria-expanded";
+// const BUTTON = `.${PREFIX}-banner__button[aria-controls]`;
 
 const toggleBanner = function toggleEl(event) {
   event.preventDefault();
   this.closest(HEADER).classList.toggle(EXPANDED_CLASS);
 };
 
-module.exports = behavior({
-  [CLICK]: {
-    [`${HEADER} [aria-controls]`]: toggleBanner,
+const banner = behavior(
+  {
+    [CLICK]: {
+      [`${HEADER} [aria-controls]`]: toggleBanner,
+    },
   },
-});
+  {
+    init(root) {
+      accordion.on(root);
+    }
+  }
+);
+
+module.exports = banner;
