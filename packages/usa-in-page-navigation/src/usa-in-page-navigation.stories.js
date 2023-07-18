355c8341b55092459a235d8b1225c84d7d631e48
Amy Leadem <amy.leadem@bixal.com>
2023-07-18 09:38:49 -0700

    Add Storybook test for data-main-content-selector

--- packages/usa-in-page-navigation/src/usa-in-page-navigation.stories.js
+++ packages/usa-in-page-navigation/src/usa-in-page-navigation.stories.js
@@ -3,9 +3,22 @@ import Content from "./usa-in-page-navigation.json";
 
 export default {
   title: "Components/In-Page Navigation",
+  argTypes: {
+    mainContentSelector: {
+      name: "Turn on custom content selector",
+      defaultValue: false,
+      type: "boolean",
+    },
+  },
 };
 
 const Template = (args) => Component(args);
 
 export const Default = Template.bind({});
 Default.args = Content;
+
+export const TestCustomContentSelector = Template.bind();
+TestCustomContentSelector.args = {
+  ...Content,
+  mainContentSelector: true
+}
