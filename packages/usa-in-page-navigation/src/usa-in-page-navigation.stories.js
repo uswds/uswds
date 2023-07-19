0ea2bbf67fc2fc3ce9a7f581ab3abd590358bb38
Amy Leadem <amy.leadem@bixal.com>
2023-07-19 09:57:53 -0700

    Add test hidden headers story

--- packages/usa-in-page-navigation/src/usa-in-page-navigation.stories.js
+++ packages/usa-in-page-navigation/src/usa-in-page-navigation.stories.js
@@ -3,9 +3,22 @@ import Content from "./usa-in-page-navigation.json";
 
 export default {
   title: "Components/In-Page Navigation",
+  argTypes: {
+    hiddenHeader: {
+      name: "Add hidden headers to main",
+      defaultValue: false,
+      type: "boolean",
+    },
+  },
 };
 
 const Template = (args) => Component(args);
 
 export const Default = Template.bind({});
 Default.args = Content;
+
+export const TestHiddenHeader = Template.bind();
+TestHiddenHeader.args = {
+  ...Content,
+  hiddenHeader: true,
+};
