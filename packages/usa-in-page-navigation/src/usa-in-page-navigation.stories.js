b6ae998243e87664f0c3238543a36284f9766f53
Amy Leadem <amy.leadem@bixal.com>
2023-07-19 10:58:36 -0700

    Add storybook tests for data-main-content-selector

--- packages/usa-in-page-navigation/src/usa-in-page-navigation.stories.js
+++ packages/usa-in-page-navigation/src/usa-in-page-navigation.stories.js
@@ -1,24 +1,33 @@
 import Component from "./usa-in-page-navigation.twig";
+import TestHiddenHeaderComponent from "./test/test-pattern/test-custom-content-selector.twig";
 import Content from "./usa-in-page-navigation.json";
 
 export default {
   title: "Components/In-Page Navigation",
   argTypes: {
-    mainContentSelector: {
-      name: "Turn on custom content selector",
+    customContentSelector: {
+      name: "Add data-main-content-selector",
       defaultValue: false,
+      table: { disable: true },
       type: "boolean",
     },
   },
 };
 
 const Template = (args) => Component(args);
+const TestHiddenHeaderTemplate = (args) => TestHiddenHeaderComponent (args);
 
 export const Default = Template.bind({});
 Default.args = Content;
 
-export const TestCustomContentSelector = Template.bind();
-TestCustomContentSelector.args = {
-  ...Content,
-  mainContentSelector: true,
+export const TestCustomContentSelectorOn = TestHiddenHeaderTemplate.bind();
+TestCustomContentSelectorOn.argTypes = {
+  defaultValue: true,
+  table: { disable: false },
+};
+
+export const TestCustomContentSelectorOff = TestHiddenHeaderTemplate.bind();
+TestCustomContentSelectorOff.argTypes = {
+  defaultValue: true,
+  table: { disable: false },
 };
