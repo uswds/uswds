394a4638a6b054211002f90a3c87c3a809d29c67
Amy Leadem <amy.leadem@bixal.com>
2023-08-10 14:06:52 -0700

    Make unit tests more resilient

--- packages/usa-in-page-navigation/src/test/in-page-navigation-custom-content.spec.js
+++ packages/usa-in-page-navigation/src/test/in-page-navigation-custom-content.spec.js
@@ -5,8 +5,9 @@ const sinon = require("sinon");
 const behavior = require("../index");
 
 const TEMPLATE = fs.readFileSync(
-  path.join(__dirname, "/in-page-nav-custom-content.template.html")
+  path.join(__dirname, "/in-page-navigation-custom-content.template.html")
 );
+const THE_NAV = ".usa-in-page-nav";
 const PRIMARY_CONTENT_SELECTOR =
   ".usa-in-page-nav-container .usa-in-page-nav .usa-in-page-nav__list";
 
@@ -19,11 +20,13 @@ const tests = [
 ];
 
 tests.forEach(({ name, selector: containerSelector }) => {
-  describe(`in-page navigation initialized at ${name}`, () => {
+  describe(`in-page navigation pulls in headers from the custom content region at ${name}`, () => {
     const { body } = document;
 
-    let theList;
-    let listLinks;
+    let navList;
+    let navListLinks;
+    let customContentSelector;
+    let customContentHeaderList;
 
     before(() => {
       const observe = sinon.spy();
@@ -36,8 +39,10 @@ tests.forEach(({ name, selector: containerSelector }) => {
 
       behavior.on(containerSelector());
 
-      theList = document.querySelector(PRIMARY_CONTENT_SELECTOR);
-      listLinks = Array.from(theList.getElementsByTagName("a"));
+      navList = document.querySelector(PRIMARY_CONTENT_SELECTOR);
+      navListLinks = Array.from(navList.getElementsByTagName("a"));
+      customContentSelector = document.querySelector(THE_NAV).getAttribute('data-main-content-selector');
+      customContentHeaderList =  document.querySelector(customContentSelector).querySelectorAll("h2, h3");
     });
 
     afterEach(() => {
@@ -46,19 +51,19 @@ tests.forEach(({ name, selector: containerSelector }) => {
       window.location.hash = "";
     });
 
-    it("creates only one link item in the nav list", () => {
-      assert.equal(listLinks.length === 1, true);
+    it("pulls the headers from the designated custom content region into the nav list", () => {
+      assert.equal(customContentHeaderList.length === navListLinks.length, true);
     });
 
     it("creates a link in the nav list for the header that is inside the custom content region", () => {
-      const customRegionLink = listLinks.filter((link) =>
+      const customRegionLink = navListLinks.filter((link) =>
         link.href.includes("#header-in-content-region")
       );
       assert.equal(customRegionLink.length === 1, true);
     });
 
     it("does not create a link in the nav list for the header that is outside the custom content region", () => {
-      const mainRegionLink = listLinks.filter((link) =>
+      const mainRegionLink = navListLinks.filter((link) =>
         link.href.includes("#header-not-in-content-region")
       );
       assert.equal(mainRegionLink.length === 0, true);
