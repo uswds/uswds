288e9eb04c83afba9bd8953517ea7f6ae3e6a519
Amy Leadem <amy.leadem@bixal.com>
2023-08-16 16:30:49 -0700

    Create story for custom header

--- packages/usa-in-page-navigation/src/usa-in-page-navigation.stories.js
+++ packages/usa-in-page-navigation/src/usa-in-page-navigation.stories.js
@@ -1,5 +1,6 @@
 import Component from "./usa-in-page-navigation.twig";
 import TestCustomContentComponent from "./test/test-patterns/test-custom-content-selector.twig";
+import TestCustomHeaderComponent from "./test/test-patterns/test-custom-header-selector.twig";
 import TestHiddenHeaderComponent from "./test/test-patterns/test-hidden-headers.twig";
 import Content from "./usa-in-page-navigation.json";
 
@@ -9,6 +10,7 @@ export default {
 
 const Template = (args) => Component(args);
 const TestCustomContentTemplate = (args) => TestCustomContentComponent(args);
+const TestCustomHeaderTemplate = (args) => TestCustomHeaderComponent(args);
 const TestHiddenHeaderTemplate = (args) => TestHiddenHeaderComponent(args);
 
 export const Default = Template.bind({});
@@ -18,4 +20,15 @@ export const TestCustomContentSelector = TestCustomContentTemplate.bind();
 TestCustomContentSelector.args = {
   customContentSelector: true,
 };
+
+export const TestCustomHeaderSelector = TestCustomHeaderTemplate.bind();
+TestCustomHeaderSelector.argTypes = {
+  includeHeaders: {
+    defaultValue: "All",
+    name: "Include these headers in link list",
+    options: ["All", "h1", "h2", "h3", "h4", "h5", "h6"],
+    control: { type: "radio" },
+  },
+}
+
 export const TestHiddenHeaders = TestHiddenHeaderTemplate.bind();
