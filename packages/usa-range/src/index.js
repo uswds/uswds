944db02390bb73bacc057a7fdbd323fa5f0a7da4
Charlie Mahoney <charlie.mahoneyy@gmail.com>
2023-10-30 10:06:24 -0400

    Update preposition logic and JSDocs comment

--- packages/usa-range/src/index.js
+++ packages/usa-range/src/index.js
@@ -7,25 +7,26 @@ const RANGE_CLASSNAME = `${PREFIX}-range`;
 const RANGE = `.${RANGE_CLASSNAME}`;
 
 /**
- * Called when range slider includes the data-optional-unit attribute.
- * Collects necessary attributes, then constructs and appends aria-valuetext attribute for screen reader context.
+ * Update range callout for screen readers using the optional data attributes.
+ *
+ * Get optional data attributes, construct and appends aria-valuetext attribute.
+ * 
+ * @example
+ * 
+ * <input id="usa-range" class="usa-range" type="range" min="0" max="100" step="10" value="20" role="slider" data-text-unit="degrees"> 
+ *
+ * Callout returns "20 degrees of 100."
  *
  * @param {HTMLInputElement} targetRange - The range slider input element
  */
 const updateCallout = (targetRange) => {
   const rangeSlider = targetRange;
-  const optionalPrep = rangeSlider.getAttribute("data-text-preposition");
-  const unit = rangeSlider.getAttribute("data-text-unit");
+  const defaultPrep = "of";
+  const optionalPrep = rangeSlider.dataset.textPreposition;
+  const prep = optionalPrep || defaultPrep;
+  const unit = rangeSlider.dataset.textUnit;
   const val = rangeSlider.value;
   const max = rangeSlider.getAttribute("max");
-
-  let prep;
-
-  if (rangeSlider.getAttribute("data-text-preposition")) {
-    prep = optionalPrep;
-  } else {
-    prep = "of";
-  }
 
   const callout = `${val} ${unit || ""} ${prep} ${max}`;
 
