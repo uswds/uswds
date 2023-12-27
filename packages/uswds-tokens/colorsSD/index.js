3e1c234402fe488d2606d06994c01fe1329bd3a6
Charlie Mahoney <charlie.mahoneyy@gmail.com>
2023-12-27 14:10:01 -0500

    Fix directory check

--- packages/uswds-tokens/colorsSD/index.js
+++ packages/uswds-tokens/colorsSD/index.js
@@ -3,5 +3,5 @@ const { readdirSync, statSync } = require("fs");
 const { join } = require("path");
 
 const dirs = (p) =>
-  readdirSync(p).filter((f) => statSync(join(p, f)).isFile());
+  readdirSync(p).filter((f) => statSync(join(p, f)).isDirectory());
 module.exports = dirs(__dirname);
