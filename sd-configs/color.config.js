e7a27a7af50d87257683cd125d6dfcb8a259ac24
Charlie Mahoney <charlie.mahoneyy@gmail.com>
2024-01-02 13:11:55 -0500

    Custom color formatter

--- sd-configs/color.config.js
+++ sd-configs/color.config.js
@@ -1,6 +1,9 @@
+const StyleDictionary = require("style-dictionary");
+const _ = require("lodash");
+const fs = require('fs');
 const colorTokens = require("../packages/uswds-tokens/colors")
 
-module.exports = {
+const USWDSTokens = StyleDictionary.extend({
   source: ["packages/uswds-tokens/colors/*.json"],
   platforms: {
     "scss/category": {
@@ -8,8 +11,8 @@ module.exports = {
       buildPath: "packages/uswds-core/src/styles/tokens/color/",
       files: colorTokens.map((tokenCategory) => ({
         destination: `_${tokenCategory}.scss`,
-        format: "scss/map-deep",
         mapName: `system-color-${tokenCategory}`,
+        format: "custom/format/color-map",
         filter: {
           attributes: {
             category: tokenCategory
@@ -18,4 +21,11 @@ module.exports = {
       })),
     },
   },
-}
+})
+
+USWDSTokens.registerFormat({
+  name: 'custom/format/color-map',
+  formatter: _.template(fs.readFileSync(`${__dirname  }/templates/scss-color-map.template`))
+});
+
+USWDSTokens.buildPlatform('scss/category');
