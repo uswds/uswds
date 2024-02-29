72444cf497b149fe14fb4bcd993072a0a8097e9d
Andrew Duthie <andrew.duthie@gsa.gov>
2024-02-29 01:04:33 -0500

    Replace receptor behavior with custom optimized implementation
    - Reduces size of behavior by 66%, minimal component import (accordion) by 50%
    - Narrows scope to features in use
    - Avoid baked-in polyfills not necessary for current browser supports
    - Remove third-party dependencies to reduce maintenance overhead and security vulnerability attack surface area
    - Add TypeScript annotations for IDEs supporting code autocompletion (e.g. VS Code)
    
    ```
    echo "import './packages/usa-banner/src/index.js'" | npx esbuild --bundle --minify | brotli-size
    ```
    
    Before: 1.84 kB
    After: 896 B
    Diff: -944 B (-51.3%)
    
    ```
    echo "import './packages/uswds-core/src/js/utils/behavior'" | npx esbuild --bundle --minify | brotli-size
    ```
    
    Before: 1.43 kB
    After: 473 B
    Diff: -957 B (-66.9%)

--- packages/uswds-core/src/js/utils/test/behavior.spec.js
+++ packages/uswds-core/src/js/utils/test/behavior.spec.js
@@ -8,10 +8,117 @@ describe("behavior", () => {
     assert(component && typeof component === "object");
   });
 
-  it("has on() and off() methods", () => {
+  it("has on(), off(), add(), remove() functions", () => {
     const component = behavior({});
     assert.strictEqual(typeof component.on, "function");
     assert.strictEqual(typeof component.off, "function");
+    assert.strictEqual(typeof component.add, "function");
+    assert.strictEqual(typeof component.remove, "function");
+  });
+
+  it("includes additional properties from given arguments", () => {
+    const component = behavior({}, { extra: true });
+
+    assert.strictEqual(component.extra, true);
+  });
+
+  it("calls event handlers as function", () => {
+    const click = sinon.stub();
+    const component = behavior({ click });
+    component.on();
+    const event = new Event("click", { bubbles: true });
+    document.body.dispatchEvent(event);
+
+    assert(click.calledOnceWithExactly(event));
+    assert.strictEqual(click.getCall(0).thisValue, document.body);
+  });
+
+  it("scopes event handling by the given target", () => {
+    const click = sinon.stub();
+    const component = behavior({ click });
+    const event = new Event("click", { bubbles: true });
+    const div = document.createElement("div");
+    document.body.appendChild(div);
+    component.on(div);
+    document.body.dispatchEvent(event);
+
+    assert(click.notCalled);
+    div.dispatchEvent(event);
+
+    assert(click.calledOnceWithExactly(event));
+    assert.strictEqual(click.getCall(0).thisValue, div);
+  });
+
+  it("calls event handlers as object for matched selector", () => {
+    const divClick = sinon.stub();
+    const spanClick = sinon.stub();
+    const component = behavior({ click: { div: divClick, span: spanClick } });
+    const event = new Event("click", { bubbles: true });
+    const div = document.createElement("div");
+    const span = document.createElement("span");
+    document.body.appendChild(div);
+    document.body.appendChild(span);
+    component.on();
+    div.dispatchEvent(event);
+
+    assert(divClick.calledOnceWithExactly(event));
+    assert.strictEqual(divClick.getCall(0).thisValue, div);
+    assert(spanClick.notCalled);
+  });
+
+  it("calls event handlers as object for matched ancestor selector", () => {
+    const bodyClick = sinon.stub();
+    const component = behavior({ click: { body: bodyClick } });
+    const event = new Event("click", { bubbles: true });
+    const div = document.createElement("div");
+    document.body.appendChild(div);
+    component.on();
+    div.dispatchEvent(event);
+
+    assert(bodyClick.calledOnceWithExactly(event));
+    assert.strictEqual(bodyClick.getCall(0).thisValue, document.body);
+  });
+
+  it("calls event handlers as object for multiple matches", () => {
+    const bodyClick = sinon.stub();
+    const divClick = sinon.stub();
+    const component = behavior({ click: { div: divClick, body: bodyClick } });
+    const event = new Event("click", { bubbles: true });
+    const div = document.createElement("div");
+    document.body.appendChild(div);
+    component.on();
+    div.dispatchEvent(event);
+
+    assert(divClick.calledOnceWithExactly(event));
+    assert.strictEqual(divClick.getCall(0).thisValue, div);
+    assert(bodyClick.calledOnceWithExactly(event));
+    assert.strictEqual(bodyClick.getCall(0).thisValue, document.body);
+  });
+
+  it("skips other event handlers once one returns false", () => {
+    const bodyClick = sinon.stub();
+    const divClick = sinon.stub().returns(false);
+    const component = behavior({ click: { div: divClick, body: bodyClick } });
+    const event = new Event("click", { bubbles: true });
+    const div = document.createElement("div");
+    document.body.appendChild(div);
+    component.on();
+    div.dispatchEvent(event);
+
+    assert(divClick.calledOnceWithExactly(event));
+    assert.strictEqual(divClick.getCall(0).thisValue, div);
+    assert(bodyClick.notCalled);
+  });
+
+  it("calls event handlers with space separated event names", () => {
+    const click = sinon.stub();
+    const component = behavior({ "keydown click": click });
+    component.on();
+    const event = new Event("click", { bubbles: true });
+    document.body.dispatchEvent(event);
+
+    assert(click.calledOnceWithExactly(event));
+    assert.strictEqual(click.getCall(0).thisValue, document.body);
   });
 
   describe("behavior.on()", () => {
@@ -33,6 +140,17 @@ describe("behavior", () => {
       behavior({}, { init }).on(el);
       assert(init.calledWithExactly(el));
     });
+
+    it("binds events", () => {
+      const sandbox = sinon.createSandbox();
+      sandbox.spy(document.body, "addEventListener");
+      const component = behavior({ click() {} });
+      component.on();
+
+      assert(document.body.addEventListener.calledOnceWith("click"));
+
+      sandbox.restore();
+    });
   });
 
   describe("behavior.off()", () => {
@@ -53,6 +171,18 @@ describe("behavior", () => {
       const el = document.createElement("div");
       behavior({}, { teardown }).off(el);
       assert(teardown.calledWithExactly(el));
+    });
+
+    it("removes event bindings", () => {
+      const sandbox = sinon.createSandbox();
+      sandbox.spy(document.body, "removeEventListener");
+      const component = behavior({ click() {} });
+      component.on();
+      component.off();
+
+      assert(document.body.removeEventListener.calledOnceWith("click"));
+
+      sandbox.restore();
     });
   });
 });
