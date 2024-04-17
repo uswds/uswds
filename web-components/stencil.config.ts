3869d6ea16c8323772d650dfd2c76b1afc8a5195
James Mejia <james.mejia@gsa.gov>
2024-04-17 16:18:58 -0500

    Add style plugins & config

--- web-components/stencil.config.ts
+++ web-components/stencil.config.ts
@@ -1,7 +1,24 @@
 import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
+import { postcss } from '@stencil-community/postcss';
+import autoprefixer from 'autoprefixer';
+
 
 export const config: Config = {
   namespace: 'web-components',
+  plugins: [
+    sass({
+      includePaths: [
+        '../packages/'
+      ]
+    }),
+    // TODO: Get all required USWDS postcss plugins.
+    //? Autoprefixer looks for browserslistrc in current or parent directories.
+    //? See: https://github.com/browserslist/browserslist?tab=readme-ov-file#queries
+    postcss({
+      plugins: [autoprefixer()]
+    })
+  ],
   outputTargets: [
     {
       type: 'dist',
