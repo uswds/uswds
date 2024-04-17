23bd7d4ce70b5abb6d2a016fdd76896e7fb252f7
James Mejia <james.mejia@gsa.gov>
2024-04-17 13:27:25 -0500

    Install stencilJS

---
+++ web-components/stencil.config.ts
@@ -0,0 +1,26 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'web-components',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+      customElementsExportBehavior: 'auto-define-custom-elements',
+      externalRuntime: false,
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+  testing: {
+    browserHeadless: "new",
+  },
+};
