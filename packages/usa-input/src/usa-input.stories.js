d62d43a55c316b0c910dbcb35ea3e60bfe33786d
Charlie Mahoney <charlie.mahoneyy@gmail.com>
2024-07-12 11:14:40 -0400

    Create aria-disabled + readonly test template

--- packages/usa-input/src/usa-input.stories.js
+++ packages/usa-input/src/usa-input.stories.js
@@ -1,5 +1,6 @@
 import Component from "./usa-input.twig";
 import Showcase from "./usa-input--showcase.twig";
+import ReadonlyTest from "./test/test-patterns/test-readonly-input.twig"
 
 export default {
   title: "Components/Form Inputs/Text Input",
@@ -22,6 +23,7 @@ export default {
 
 const Template = (args) => Component(args);
 const ShowcaseTemplate = (args) => Showcase(args);
+const ReadonlyTestTemplate = (args) => ReadonlyTest(args);
 
 export const Input = Template.bind({});
 Input.args = {
@@ -44,3 +46,15 @@ StateShowcase.argTypes = {
     table: { disable: true },
   },
 };
+
+export const TestReadonly = ReadonlyTestTemplate.bind({});
+TestReadonly.argTypes = {
+  state: {
+    table: { disable: true },
+  },
+  readonly: {
+    name: "Readonly",
+    control: "boolean",
+    defaultValue: true
+  }
+}
