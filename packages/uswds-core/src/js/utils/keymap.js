54a569b1fccda195d3bfcc29396861f6b7be499c
Andrew Duthie <andrew.duthie@gsa.gov>
2024-11-01 09:16:20 -0400

    Add inline code comments

--- packages/uswds-core/src/js/utils/keymap.js
+++ packages/uswds-core/src/js/utils/keymap.js
@@ -1,19 +1,32 @@
 /**
+ * Callback to be called when key is pressed.
+ *
  * @typedef {(event: KeyboardEvent) => any} KeyboardEventHandler
  */
 
 /**
+ * Object with keyboard key combination as object key, and corresponding callback handler as value.
+ *
  * @typedef {Record<string, KeyboardEventHandler>} KeymapConfig
  */
 
 /**
+ * Initialize keyboard events with an object of keyboard key combinations and corresponding event
+ * handlers.
+ *
  * @param {KeymapConfig} map
  * @return {KeyboardEventHandler}
  */
 module.exports = (map) => (event) =>
   Object.keys(map).forEach((combo) => {
+    // Each key combination can have one or more modifier, where each modifier is prefixed with the
+    // modifier name and "+".
     const parts = combo.split("+");
+
+    // The keyboard key name should come as the last part of a "+" delimited key. Popping the item
+    // will result in `parts` only containing the modifiers to be checked.
     const key = parts.pop();
+
     const isModifierMatch = ["Shift", "Alt", "Ctrl", "Meta"]
       .filter((mod) => event.getModifierState(mod) || parts.includes(mod))
       .every((mod) => event.getModifierState(mod) && parts.includes(mod));
