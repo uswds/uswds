96bfdfc2af0619da69be4003910dab5884026fbc
Rachel Corsino <rachel.corsino@bixal.com>
2025-01-29 11:56:37 -0600

    updated focus-trap bug fix

--- packages/uswds-core/src/js/utils/focus-trap.js
+++ packages/uswds-core/src/js/utils/focus-trap.js
@@ -3,61 +3,42 @@ const behavior = require("./behavior");
 const select = require("./select");
 const activeElement = require("./active-element");
 
-console.log("hello");
-
 const FOCUSABLE =
   'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
 
+const getFocusableElements = (context) => Array.from(select(FOCUSABLE, context));
+const firstTabStop = (context) => getFocusableElements(context)[0];
+const lastTabStop = (context) => getFocusableElements(context).at(-1);
+
 const tabHandler = (context) => {
-  let focusableElements = [];
-  
-  // Creates a new mutationObserver to watch for changes
-  // Observers callback function that updates the focusable elements when change is detected
-  const observer = new MutationObserver(() => {
-    updateFocusableElements();
-  });
-
-  function updateFocusableElements() {
-    focusableElements = Array.from(select(FOCUSABLE, context));
-  }
-  
-  const firstTabStop = () => focusableElements[0];
-  const lastTabStop = () => focusableElements[focusableElements.length - 1];
-
   // Special rules for when the user is tabbing forward from the last focusable element,
   // or when tabbing backwards from the first focusable element
   function tabAhead(event) {
-    if (activeElement() === lastTabStop()) {
+    if (activeElement() === lastTabStop(context)) {
       event.preventDefault();
-      firstTabStop().focus();
+      firstTabStop(context).focus();
     }
   }
 
   function tabBack(event) {
-    if (activeElement() === firstTabStop()) {
+    if (activeElement() === firstTabStop(context)) {
       event.preventDefault();
-      lastTabStop().focus();
+      lastTabStop(context).focus();
     }
     // This checks if you want to set the initial focus to a container
     // instead of an element within, and the user tabs back.
     // Then we set the focus to the first
-    else if (!focusableElements.includes(activeElement())) {
+    else if (!getFocusableElements(context).includes(activeElement())) {
       event.preventDefault();
-      firstTabStop().focus();
+      firstTabStop(context).focus();
     }
   }
-
-  observer.observe(context, {
-    childList: true,  // Watch for changes to the child elements
-    subtree: true,    // Watch for changes to the entire subtree
-  })
 
   return {
     firstTabStop,
     lastTabStop,
     tabAhead,
     tabBack,
-    disconnect: () => observer.disconnect(), // function to disconnect the observer
   };
 };
 
@@ -85,8 +66,8 @@ module.exports = (context, additionalKeyBindings = {}) => {
       init() {
         // TODO: is this desireable behavior? Should the trap always do this by default or should
         // the component getting decorated handle this?
-        if (tabEventHandler.firstTabStop()) {
-          tabEventHandler.firstTabStop().focus();
+        if (tabEventHandler.firstTabStop(context)) {
+          tabEventHandler.firstTabStop(context).focus();
         }
       },
       update(isActive) {
@@ -94,7 +75,6 @@ module.exports = (context, additionalKeyBindings = {}) => {
           this.on();
         } else {
           this.off();
-          tabEventHandler.disconnect();
         }
       },
     },
