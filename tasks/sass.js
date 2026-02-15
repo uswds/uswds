377667bf802bf0a751a1ea95f14da219acf99048
JB Hutch <jbhutch01@gmail.com>
2026-02-15 09:49:52 -0500

    Replace postcss, autoprefixer with lightningcss

--- tasks/sass.js
+++ tasks/sass.js
@@ -1,42 +1,57 @@
-const autoprefixer = require("autoprefixer");
-const csso = require("postcss-csso");
-const discardComments = require("postcss-discard-comments");
-const { src, dest } = require("gulp");
-const postcss = require("gulp-postcss");
-const replace = require("gulp-replace");
-const rename = require("gulp-rename");
-const sass = require("gulp-sass")(require("sass-embedded"));
-const sourcemaps = require("gulp-sourcemaps");
+const sass = require("sass-embedded");
+const lightningcss = require("lightningcss");
+const fs = require("fs");
+const path = require("path");
+const browserslist = require("browserslist");
 const dutil = require("./utils/doc-util");
 const pkg = require("../package.json");
 
-module.exports = {
-  compileSass() {
-    dutil.logMessage("sass", "Compiling Sass");
-    const pluginsProcess = [discardComments(), autoprefixer()];
-    const pluginsMinify = [csso({ forceMediaMerge: false })];
+const targets = lightningcss.browserslistToTargets(browserslist(pkg.browserslist));
+const versionStamp = `uswds v${pkg.version}`;
 
-    return src("src/stylesheets/uswds.scss")
-      .pipe(sourcemaps.init({ largeFile: true }))
-      .pipe(
-        sass({
-          loadPaths: ["./packages"],
-          style: "expanded",
-        }).on("error", function handleError(error) {
-          dutil.logError(error);
-          this.emit("end");
-        }),
-      )
-      .pipe(postcss(pluginsProcess))
-      .pipe(replace(/\buswds @version\b/g, `uswds v${pkg.version}`))
-      .pipe(dest("dist/css"))
-      .pipe(postcss(pluginsMinify))
-      .pipe(
-        rename({
-          suffix: ".min",
-        }),
-      )
-      .pipe(sourcemaps.write("."))
-      .pipe(dest("dist/css"));
+module.exports = {
+  async compileSass() {
+    dutil.logMessage("sass", "Compiling Sass");
+
+    const distDir = path.resolve("dist/css");
+    fs.mkdirSync(distDir, { recursive: true });
+
+    // Compile Sass
+    const result = await sass.compileAsync(
+      "src/stylesheets/uswds.scss",
+      {
+        loadPaths: ["./packages"],
+        style: "expanded",
+        sourceMap: true,
+        sourceMapIncludeSources: true,
+      },
+    );
+
+    const cssBuffer = Buffer.from(result.css);
+
+    const expanded = lightningcss.transform({
+      filename: "uswds.css",
+      code: cssBuffer,
+      minify: false,
+      targets,
+    });
+
+    const minified = lightningcss.transform({
+      filename: "uswds.min.css",
+      code: cssBuffer,
+      minify: true,
+      sourceMap: true,
+      targets,
+    });
+
+    // Write all outputs in parallel
+    await Promise.all([
+      fs.promises.writeFile(path.join(distDir, "uswds.css"), `/* ${versionStamp} */\n\n${expanded.code}`),
+      fs.promises.writeFile(path.join(distDir, "uswds.min.css"), `/* ${versionStamp} */\n\n${minified.code}`),
+      fs.promises.writeFile(
+        path.join(distDir, "uswds.min.css.map"),
+        JSON.stringify(minified.map),
+      ),
+    ]);
   },
 };
