4de589461f2977c200cde6fd7b908449de2dd605
Ethan Gardner <edgincvg@gmail.com>
2026-08-05 20:30:40 -0400

    simplify streaming

--- tasks/javascript.js
+++ tasks/javascript.js
@@ -1,6 +1,6 @@
 /* eslint-disable arrow-body-style */
 
-const { dest, src } = require("gulp");
+const { dest, src, parallel, series } = require("gulp");
 const buffer = require("vinyl-buffer");
 const browserify = require("browserify");
 const childProcess = require("child_process");
@@ -10,69 +10,71 @@ const sourcemaps = require("gulp-sourcemaps");
 const uglify = require("gulp-uglify");
 const dutil = require("./utils/doc-util");
 
-// Helper: resolve when a stream emits 'finish' or 'end', reject on 'error'.
-// Replaces merge-stream, which uses Node-streams PassThrough that is
-// incompatible with gulp 5's streamx-based vinyl streams: the fast
-// uswds-init stream would end the PassThrough before the slower browserify
-// bundle finished, causing a "write after end" error.
-function streamFinished(stream) {
-  return new Promise((resolve, reject) => {
-    stream.once("finish", resolve).once("end", resolve).once("error", reject);
-  });
+/**
+ * Shared JS processing pipeline.
+ * @param {Stream} stream - The input stream.
+ * @param {string} basename - The base name for the output file.
+ * @returns {Stream} The processed stream.
+ */
+function jsPipeline(stream, basename) {
+  return stream
+    .pipe(rename({ basename }))
+    .pipe(dest("dist/js"))
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .on("error", function handleError(error) {
+      dutil.logError(error);
+      this.emit("end");
+    })
+    .pipe(uglify())
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("dist/js"));
+}
+
+function bundleMain() {
+  const packageName = dutil.pkg.name.replace("@uswds/", "");
+  return jsPipeline(
+    browserify({
+      entries: ["packages/uswds-core/src/js/start.js"],
+      debug: true,
+    })
+      .transform("babelify", {
+        global: true,
+        presets: ["@babel/preset-env"],
+      })
+      .bundle()
+      .pipe(source(`${packageName}.js`))
+      .pipe(buffer()),
+    packageName
+  );
+}
+
+function copyInit() {
+  return jsPipeline(
+    src("packages/uswds-core/src/js/uswds-init.js"),
+    "uswds-init"
+  );
 }
 
 module.exports = {
-  compileJS() {
-    dutil.logMessage("javascript", "Compiling JavaScript");
-    let packageName = dutil.pkg.name.replace("@uswds/", "");
-    const promises = Object.entries({
-      [packageName]: browserify({
-        entries: ["packages/uswds-core/src/js/start.js"],
-        debug: true,
-      })
-        .transform("babelify", {
-          global: true,
-          presets: ["@babel/preset-env"],
-        })
-        .bundle()
-        .pipe(source(`${packageName}.js`))
-        .pipe(buffer()),
-      "uswds-init": src("packages/uswds-core/src/js/uswds-init.js"),
-    }).map(([basename, stream]) => {
-      const out = stream
-        .pipe(rename({ basename }))
-        .pipe(dest("dist/js"))
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .on("error", function handleError(error) {
-          dutil.logError(error);
-          this.emit('end');
-        })
-        .pipe(uglify())
-        .pipe(
-          rename({
-            suffix: ".min",
-          })
-        )
-        .pipe(sourcemaps.write("."))
-        .pipe(dest("dist/js"));
-      return streamFinished(out);
-    });
-
-    return Promise.all(promises);
-  },
-  typeCheck() {
-    return new Promise((resolve, reject) => {
-      childProcess
-        .spawn("./node_modules/.bin/tsc", { stdio: "inherit" })
-        .on("error", reject)
-        .on("exit", (code) => {
-          if (code === 0) {
-            dutil.logMessage("typecheck", "TypeScript likes our code!");
-            resolve();
-          } else {
-            reject(new Error("TypeScript failed, see output for details!"));
-          }
-        });
-    });
-  },
+  compileJS: series(
+    function logCompileJS(done) {
+      dutil.logMessage("javascript", "Compiling JavaScript");
+      done();
+    },
+    parallel(bundleMain, copyInit)
+  ),
+  typeCheck: series(
+    function spawnTsc() {
+      return childProcess.spawn("./node_modules/.bin/tsc", { stdio: "inherit" });
+    },
+    function logTscSuccess(done) {
+      dutil.logMessage("typecheck", "TypeScript likes our code!");
+      done();
+    }
+  ),
 };
