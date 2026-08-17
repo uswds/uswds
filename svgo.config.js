aa76f4a2a6fd0f436a8b0089bacda4e9b9d97b08
Ethan Gardner <ethan.gardner@gsa.gov>
2026-08-17 13:48:14 -0400

    add comment for clarity

--- svgo.config.js
+++ svgo.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   plugins: [
     {
+      // removeViewBox is intentionally excluded. In svgo v4, it is not part
+      // of preset-default and must be explicitly added to run. Omitting it
+      // ensures viewBox attributes are preserved on all optimized SVGs.
       name: 'preset-default',
     },
   ],
 }
+
