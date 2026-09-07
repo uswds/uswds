84ab6ad43d73b734c8494cf1d57597db8a0a5d81
Raviteja Doppalapudi <ravitejapioneerblaze@gmail.com>
2026-09-07 16:24:50 -0500

    Add behavioral characterization test for built JS bundle
    Apply prettier formatting to dist-bundle spec

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const { series, parallel } = require("gulp");
 const { noCleanup, noTest } = require("./tasks/flags");
 const { buildSprite, buildSpriteStandalone } = require("./tasks/svg-sprite");
 const { compileJS, typeCheck } = require("./tasks/javascript");
-const { unitTests, sassTests, tasksTests } = require("./tasks/test");
+const { unitTests, sassTests, tasksTests, distTests } = require("./tasks/test");
 const { lintSass } = require("./tasks/lint");
 const { build } = require("./tasks/build");
 const { release } = require("./tasks/release");
@@ -46,6 +46,8 @@ exports.sassTests = sassTests;
 exports.unitTests = unitTests;
 exports.tasksTests = tasksTests;
 exports.test = series(typeCheck, lintSass, sassTests, unitTests, tasksTests);
+exports.distTests = distTests;
+exports.testDist = series(compileJS, distTests);
 
 /**
  * *Build tasks*
